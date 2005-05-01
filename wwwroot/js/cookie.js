b9695441c5a600c00092f9008f122c7503c53669
Sven Klemm <sven@c3d2.de>
2005-05-01 11:39:30 +0000

    merged javascript changes from branch

--- wwwroot/js/cookie.js
+++ wwwroot/js/cookie.js
@@ -1,24 +1,21 @@
 // creates/saves a cookie 
 function createCookie(name, value) {
-  date = new Date();
+  var date = new Date();
   date.setTime(date.getTime()+365*60*60*24*1000); //cookie valid for 1 year from today
   document.cookie = name + "=" + value + "; expires="+date.toGMTString()+"; path=/";
 }
 
 //reads data stored in the cookie
 function readCookie(name) {
-  start = document.cookie.indexOf(name+"=");
-  ende = document.cookie.indexOf(";") == -1 ? document.cookie.length : document.cookie.indexOf(";");
+  var start = document.cookie.indexOf(name+"=");
+  var ende = document.cookie.indexOf(";") == -1 ? document.cookie.length : document.cookie.indexOf(";");
   return document.cookie.substring(start+name.length+1, ende);
-  //pattern = eval("/"+name+"=([a-zA-Z0-9-_:=]+);*/");
-  //pattern.exec(document.cookie);
-  //return RegExp.$1;
 }
 
 //checks if a variable name exists in the data area
 function issetCookieVar(name) {
-  data = readCookie("Pentabarf");
-  pattern = eval("/:" + name + "=[a-z0-9-_]+:/i");
+  var data = readCookie("Pentabarf");
+  var pattern = eval("/:" + name + "=[a-z0-9-_]+:/i");
   return pattern.test(data);
 }
 
@@ -32,16 +29,15 @@ function readCookieVar(name) {
       if (varArray[0] == name)
         return varArray[1];
     } 
-  } else {
-    return "";
-  }
+  } 
+  return "";
 }
 
 //saves a value under name "name" to the cookie
 function setCookieVar(name, value) {
-  data = readCookie("Pentabarf");
-  pattern = eval("/:" + name + "=[a-z0-9-_]+:/i");
-  repl = ":" + name + "=" + value + ":";
+  var data = readCookie("Pentabarf");
+  var pattern = eval("/:" + name + "=[a-z0-9-_]+:/i");
+  var repl = ":" + name + "=" + value + ":";
   if (pattern.test(data) == true) {
     data = data.replace(eval("/:" + name + "=[a-z0-9-_]+:/i"), repl);
   } else {
