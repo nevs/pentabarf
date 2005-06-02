0fe9ed580d125c50a7d5874070227f656f79ad72
Sven Klemm <sven@c3d2.de>
2005-06-02 16:44:09 +0000

    script for generating csv output of resources added

---
+++ wwwroot/resources.php
@@ -0,0 +1,30 @@
+<?php
+
+  require_once('../functions/error_handler.php');
+  require_once('../functions/exception_handler.php');
+  require_once('../db/auth_person.php');
+  require_once('../db/view_event.php');
+  require_once('../db/view_event_person_person.php');
+
+  // authenticate Person
+  $auth_person = new Auth_Person;
+  $auth_person->set_language_id(120);
+
+  $events = new View_Event;
+  $person = new View_Event_Person_Person;
+
+  if ( $events->select(array('conference_id' => 2) ) ) {
+     foreach( $events as $key ) {
+       echo $events->get('event_id').'|';
+       echo $events->get('title').'|';
+       $person->select(array('event_id' => $events->get('event_id')));
+       $persons = array();
+       foreach( $person as $key ) {
+         $persons[] = $person->get('name');
+       }
+       echo implode(', ', $persons).'|';
+       echo $events->get('event_state').'|';
+       echo $events->get('resources')."\r\n";
+     }
+  }
+?>
