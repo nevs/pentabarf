7b8c7efc7999b5b45387c4e9273c9392251f4fc0
Sven Klemm <sven@c3d2.de>
2005-06-04 18:23:56 +0000

    bugfix for report expenses

--- db/view_person_travel_with_expenses.php
+++ db/view_person_travel_with_expenses.php
@@ -29,7 +29,7 @@ class View_Person_Travel_with_Expenses extends View
   public function get_expenses_sum($conference_id)
   {
     $conference_id = $this->check_integer($conference_id);
-    return $this->select_single("sum", "SELECT sum(p.travel_cost / c_travel.exchange_rate)+ sum (p.accommodation_cost / c_acco.exchange_rate) AS sum from person_travel AS p,currency AS c_travel, currency AS c_acco WHERE (p.travel_cost is not null OR p.accommodation_cost IS NOT NULL) AND c_travel.currency_id = p.travel_currency_id AND c_acco.currency_id = p.accommodation_currency_id AND p.conference_id = $conference_id;");
+    return $this->select_single("sum", "SELECT sum( coalesce(p.travel_cost, 0) / c_travel.exchange_rate)+ sum( coalesce( p.accommodation_cost, 0 ) / c_acco.exchange_rate) AS sum from person_travel AS p,currency AS c_travel, currency AS c_acco WHERE (p.travel_cost is not null OR p.accommodation_cost IS NOT NULL) AND c_travel.currency_id = p.travel_currency_id AND c_acco.currency_id = p.accommodation_currency_id AND p.conference_id = $conference_id;");
   }
 
 }
