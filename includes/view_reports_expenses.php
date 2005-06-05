f5810559cb876cd11fc81b640a4ed1289078a22c
Tim Pritlove <tim@ccc.de>
2005-06-05 03:59:58 +0000

    calculate intermediate sums for columns

--- includes/view_reports_expenses.php
+++ includes/view_reports_expenses.php
@@ -16,11 +16,20 @@
   $currency = new Currency;
   $currency->select(array('f_default' => 't'));
   $curr_sign = $currency->get('iso_4217_code');
-  $template->addVar("report-table", "TOTAL_SUM", sprintf("%.2f",round($p_t->get_expenses_sum($preferences['conference']),2))." ".$curr_sign);
 
   $person = new View_Person;
+  
+  $sum_travel_cost = 0;
+  $sum_accommodation_cost = 0;
+  $sum_fee         = 0;
+  
   foreach($p_t as $value) {
     $person->select(array('person_id' => $p_t->get('person_id')));
+    
+    $sum_travel_cost += $p_t->get('travel_cost');
+    $sum_accommodation_cost += $p_t->get('accommodation_cost');
+    $sum_fee += $p_t->get('fee');
+    
     array_push($result, array("IMAGE_URL"          => get_person_image_url($person),
                               "URL"                => get_person_url($person, "#travel"),
                               "NAME"               => $person->get('name'),
@@ -30,5 +39,12 @@
                               "SUM"                => $p_t->get('fee') || $p_t->get('travel_cost') || $p_t->get('accommodation_cost') ? sprintf("%.2f",round($p_t->get('fee') + $p_t->get('travel_cost') + $p_t->get('accommodation_cost'),2))." $curr_sign" : "" ));
   }
   $template->addRows("report-list", $result);
+
+
+  $template->addVar("report-table", "TOTAL_SUM", sprintf("%.2f",round($p_t->get_expenses_sum($preferences['conference']),2))." ".$curr_sign);
+  $template->addVar("report-table", "SUM_TRAVEL_COST", sprintf("%.2f",round($sum_travel_cost,2))." ".$curr_sign);
+  $template->addVar("report-table", "SUM_ACCOMMODATION_COST", sprintf("%.2f",round($sum_accommodation_cost,2))." ".$curr_sign);
+  $template->addVar("report-table", "SUM_FEE", sprintf("%.2f",round($sum_fee,2))." ".$curr_sign);
+
 
 ?>
