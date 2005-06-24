6ae8314b23e18e65e3e974184fd232e18a2baf76
Sven Klemm <sven@c3d2.de>
2005-06-24 15:10:07 +0000

    minor bugfix for new offering export

--- wwwroot/offerings.php
+++ wwwroot/offerings.php
@@ -17,17 +17,30 @@
      foreach( $events as $key ) {
         $line = $events->get('event_id').'|';
         $line .= $events->get('title').'|';
+        $line .= $events->get('abstract').'|';
         $person->select(array('event_id' => $events->get('event_id'), 'event_role_tag' => array( 'moderator', 'speaker' ) ) );
         $persons = array();
+        $persons[0]['name'] = "";
+        $persons[0]['description'] = "";
         foreach( $person as $key ) {
-           $persons[] = $person->get('name');
+           $persons[$key]['name'] = $person->get('name');
+           $persons[$key]['description'] = $person->get('description');
         }
-        $line .= implode(', ', $persons).'|';
+        $line .= $persons[0]['name'].'|';
+        $line .= $persons[0]['description'].'|';
         $line .= $events->get('remark');
 
         // remove bad character
         $line = str_replace( array("\r", "\n"), " ", $line );
         echo $line."\r\n";
+
+        // print additional speaker on new lines
+        foreach( $persons as $key => $value ) {
+           if ( $key == 0 ) continue;
+           $line = "|||".$value['name']."|".$value['description']."|"
+           $line = str_replace( array("\r", "\n"), " ", $line );
+           echo $line."\r\n";
+        }
      }
   }
 ?>
