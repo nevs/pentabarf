b9bd359c4d0ebbdc59b127cbc2a5ecfda8babc28
Sven Klemm <sven@c3d2.de>
2005-08-12 18:36:44 +0000

    enhanced view_find_event

--- sql/views.sql
+++ sql/views.sql
@@ -163,7 +163,23 @@ SELECT conference.conference_id,
   LEFT OUTER JOIN mime_type USING (mime_type_id);
 
 CREATE OR REPLACE VIEW view_find_event AS
-SELECT event.event_id, event.title, event.subtitle FROM event;
+SELECT event.event_id, 
+       event.title, 
+       event.subtitle,
+       event.event_state_id,
+       event.room_id,
+       event.day,
+       (event.start_time + conference.day_change)::interval AS start_time,
+       view_event_state.language_id,
+       view_event_state.tag AS event_state_tag,
+       view_event_state.name AS event_state,
+       view_room.tag AS room_tag,
+       view_room.name AS room
+  FROM event
+  INNER JOIN conference USING (conference_id)
+  INNER JOIN view_event_state USING (event_state_id)
+  INNER JOIN view_room ON (view_event_state.language_id = view_room.language_id AND event.room_id = view_room.room_id)
+;
 
 CREATE OR REPLACE VIEW view_conference_image AS
 SELECT conference_image.conference_id, conference_image.mime_type_id, mime_type.mime_type, conference_image.image FROM conference_image INNER JOIN mime_type USING (mime_type_id);
