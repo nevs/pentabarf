a5f9c4a88070332ded4c70e55eb90c962571089f
Sven Klemm <sven@c3d2.de>
2005-09-21 18:34:28 +0000

    r2655@pulsar:  sven | 2005-09-21 20:34:20 +0200  update initial values for event_state and event_state_localized

--- sql/data/event_state.sql
+++ sql/data/event_state.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
-
 SET client_encoding = 'UNICODE';
+SET check_function_bodies = false;
 
 SET search_path = public, pg_catalog;
 
@@ -12,15 +12,9 @@ SET search_path = public, pg_catalog;
 -- Name: event_state; Type: TABLE DATA; Schema: public; Owner: pentabarf
 --
 
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (1, 'idea', 1);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (2, 'offering', 2);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (3, 'canceled', 8);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (4, 'declined', 6);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (5, 'confirmed', 4);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (6, 'unclear', 5);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (7, 'accepted', 3);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (8, 'rejected', 7);
-INSERT INTO event_state (event_state_id, tag, rank) VALUES (9, 'onhold', NULL);
+INSERT INTO event_state (event_state_id, tag, rank) VALUES (1, 'undecided', 1);
+INSERT INTO event_state (event_state_id, tag, rank) VALUES (2, 'accepted', 2);
+INSERT INTO event_state (event_state_id, tag, rank) VALUES (3, 'rejected', 3);
 
 
 --
@@ -28,6 +22,6 @@ INSERT INTO event_state (event_state_id, tag, rank) VALUES (9, 'onhold', NULL);
 -- Name: event_state_event_state_id_seq; Type: SEQUENCE SET; Schema: public; Owner: pentabarf
 --
 
-SELECT pg_catalog.setval('event_state_event_state_id_seq', 9, true);
+SELECT pg_catalog.setval('event_state_event_state_id_seq', 10, true);
 
 
--- sql/data/event_state_localized.sql
+++ sql/data/event_state_localized.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
-
 SET client_encoding = 'UNICODE';
+SET check_function_bodies = false;
 
 SET search_path = public, pg_catalog;
 
@@ -12,23 +12,8 @@ SET search_path = public, pg_catalog;
 -- Name: event_state_localized; Type: TABLE DATA; Schema: public; Owner: pentabarf
 --
 
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (1, 144, 'Idee');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (2, 144, 'Eingabe');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (3, 144, 'Abgesagt');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (4, 144, 'Abgelehnt');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (5, 144, 'Akzeptiert (bestätigt)');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (6, 144, 'Unklar');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (7, 144, 'Akzeptiert');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (8, 144, 'Abgelehnt (bestätigt)');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (9, 144, 'Zurückgestellt');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (1, 120, 'Idea');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (2, 120, 'Offering');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (3, 120, 'Canceled');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (4, 120, 'Decline');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (5, 120, 'Accepted (confirmed)');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (6, 120, 'Uncleared');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (7, 120, 'Accepted');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (8, 120, 'Declined (confirmed)');
-INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (9, 120, 'On Hold');
+INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (1, 120, 'undecided');
+INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (2, 120, 'accepted');
+INSERT INTO event_state_localized (event_state_id, language_id, name) VALUES (3, 120, 'rejected');
 
 
