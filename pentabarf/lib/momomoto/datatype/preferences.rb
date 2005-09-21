83df0684c90a365598851d9f0bb92fb88beb7840
Sven Klemm <sven@c3d2.de>
2005-09-21 01:09:21 +0000

    r2648@pulsar:  sven | 2005-09-21 03:09:14 +0200  changed preferences to use yaml

--- pentabarf/lib/momomoto/datatype/preferences.rb
+++ pentabarf/lib/momomoto/datatype/preferences.rb
@@ -16,21 +16,25 @@ module Momomoto
 
       def filter_read( value )
         if value.to_s != ''
-          value = Marshal.load(value)
+          value = YAML.load(value)
         else
           value = Hash.new
         end
         # make sure certain values are set
         value[:current_conference_id] = 7 unless value[:current_conference_id]
         value[:current_language_id] = 120 unless value[:current_language_id]
-        value[:search_event] = '' unless value[:search_event]
+        value[:search_event] = "" unless value[:search_event]
         value[:search_event_advanced] = {} unless value[:search_event_advanced]
+        value[:search_person] = "" unless value[:search_event]
+        value[:search_person_advanced] = {} unless value[:search_event_advanced]
+        value[:search_conference] = "" unless value[:search_event]
+        value[:search_conference_advanced] = {} unless value[:search_event_advanced]
         value
       end
 
       def filter_write( value )
         if value != nil
-          value = Marshal.dump( value )
+          value = YAML.dump( value )
           value = value.gsub(/'/, "''").gsub(/\\/, "\\\\")
           return "'#{value}'"
         end
