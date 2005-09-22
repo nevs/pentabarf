06083301966b98b18d9f4bed1c53ebb9e7e6496a
Sven Klemm <sven@c3d2.de>
2005-09-22 02:39:04 +0000

    r2684@pulsar:  sven | 2005-09-22 04:33:22 +0200  added command to move language out of table conference into table conference_language

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -17,3 +17,23 @@ INSERT INTO conference_person( conference_id,
          INNER JOIN person USING(person_id) 
          INNER JOIN event_role USING(event_role_id) 
    WHERE event_role.tag IN ('speaker', 'moderator');
+
+-- move language out of table conference into table conference_language
+INSERT INTO conference_language(conference_id, 
+                                language_id, 
+                                rank) 
+  SELECT conference_id, 
+         primary_language_id, 
+         1 
+    FROM conference 
+   WHERE primary_language_id IS NOT NULL;
+
+INSERT INTO conference_language(conference_id, 
+                                language_id, 
+                                rank) 
+  SELECT conference_id, 
+         secondary_language_id, 
+         2 
+    FROM conference 
+   WHERE secondary_language_id IS NOT NULL;
+
