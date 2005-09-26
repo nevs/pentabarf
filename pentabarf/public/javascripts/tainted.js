a4ace1f2efd5f5f158e1c4d8ce63cea839cae52f
Sven Klemm <sven@c3d2.de>
2005-09-26 21:12:01 +0000

    r2817@pulsar:  sven | 2005-09-26 23:10:27 +0200  added enumerator function

--- pentabarf/public/javascripts/tainted.js
+++ pentabarf/public/javascripts/tainted.js
@@ -1,4 +1,6 @@
 
+var init_done = false;
+
 function clear_tainting()
 {
   window.onbeforeunload = null;
@@ -15,3 +17,28 @@ function unloadMessage()
   var message = "If you leave this page, your changes will be lost.";
   return message;
 }
+
+var tabindex_counter;
+
+function enumerator()
+{
+  if (!init_done) return;
+  tabindex_counter = 1;
+  enumerate(document);
+}
+
+function enumerate(node)
+{
+   if (node.hasChildNodes) {
+    var node_child = node.firstChild;
+    while (node_child!=null) {
+      if(node_child.nodeType == 1 && node_child.getAttribute("tabindex") != null) {
+        node_child.setAttribute("tabindex", tabindex_counter);
+        tabindex_counter++;  
+      }
+      enumerate(node_child);
+      node_child = node_child.nextSibling;
+    }
+  }
+}
+
