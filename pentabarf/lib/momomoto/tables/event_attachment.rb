a8568a59cc18af64af54c041677db4351980be23
Sven Klemm <sven@c3d2.de>
2005-09-28 19:44:54 +0000

    r2835@pulsar:  sven | 2005-09-27 06:06:00 +0200  added class for table event_attachment

---
+++ pentabarf/lib/momomoto/tables/event_attachment.rb
@@ -0,0 +1,19 @@
+module Momomoto
+  class Event_attachment < Base
+    def initialize
+      super
+      @domain = 'event'
+      @fields = {
+        :event_attachment_id => Datatype::Integer.new( {:not_null=>true, :default=>true, :primary_key=>true, :serial=>true} ),
+        :attachment_type_id => Datatype::Integer.new( {:not_null=>true} ),
+        :event_id => Datatype::Integer.new( {:not_null=>true} ),
+        :mime_type_id => Datatype::Integer.new( {:not_null=>true} ),
+        :filename => Datatype::Varchar.new( {:length=>256} ),
+        :title => Datatype::Varchar.new( {:length=>64} ),
+        :data => Datatype::Bytea.new( {:not_null=>true} ),
+        :f_public => Datatype::Bool.new( {:not_null=>true, :default=>true} ),
+        :last_changed => Datatype::Timestamp.new( {:not_null=>true, :default=>true} )
+      }
+    end
+  end
+end
