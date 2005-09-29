fc264c66f4815cc9ea3d1c33e1fb2352d08e2b46
Sven Klemm <sven@c3d2.de>
2005-09-29 02:01:52 +0000

    r2847@pulsar:  sven | 2005-09-29 03:20:49 +0200  added class for view_event_attachment

---
+++ pentabarf/lib/momomoto/views/view_event_attachment.rb
@@ -0,0 +1,23 @@
+module Momomoto
+  class View_event_attachment < Base
+    def initialize
+      super
+      @domain = 'event'
+      @fields = {
+        :event_attachment_id => Datatype::Integer.new( {} ),
+        :attachment_type_id => Datatype::Integer.new( {} ),
+        :attachment_type => Datatype::Varchar.new( {} ),
+        :attachment_type_tag => Datatype::Varchar.new( {:length=>32} ),
+        :event_id => Datatype::Integer.new( {} ),
+        :mime_type_id => Datatype::Integer.new( {} ),
+        :mime_type_tag => Datatype::Varchar.new( {:length=>128} ),
+        :mime_type => Datatype::Varchar.new( {:length=>128} ),
+        :filename => Datatype::Varchar.new( {:length=>256} ),
+        :title => Datatype::Varchar.new( {:length=>64} ),
+        :f_public => Datatype::Bool.new( {} ),
+        :last_changed => Datatype::Timestamp.new( {} ),
+        :filesize => Datatype::Integer.new( {} )
+      }
+    end
+  end
+end
