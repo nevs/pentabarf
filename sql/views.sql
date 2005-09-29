f8feb8dc0ffca9d1f4516fab8539551f8aac3594
Sven Klemm <sven@c3d2.de>
2005-09-29 02:01:48 +0000

    r2846@pulsar:  sven | 2005-09-29 03:19:33 +0200  added view view_event_attachment

--- sql/views.sql
+++ sql/views.sql
@@ -92,6 +92,25 @@ CREATE OR REPLACE VIEW view_event AS
        LEFT OUTER JOIN mime_type USING (mime_type_id)
 ;
 
+CREATE OR REPLACE VIEW view_event_attachment AS
+  SELECT event_attachment_id,
+         attachment_type_id,
+         view_attachment_type.name AS attachment_type,
+         view_attachment_type.tag AS attachment_type_tag,
+         event_id,
+         mime_type_id,
+         mime_type AS mime_type_tag,
+         view_mime_type.name AS mime_type,
+         filename,
+         title,
+         f_public,
+         last_changed,
+         octet_length( data ) AS filesize
+    FROM event_attachment
+         INNER JOIN view_attachment_type USING (attachment_type_id)
+         INNER JOIN view_mime_type USING (mime_type_id)
+;
+
 -- view for last active user
 CREATE OR REPLACE VIEW view_last_active AS 
   SELECT person_id, 
