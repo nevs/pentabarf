6caaff22d33d22002ca563c554b1eb762f6b626a
Sven Klemm <sven@c3d2.de>
2005-10-01 01:26:18 +0000

    r2867@pulsar:  sven | 2005-10-01 02:57:47 +0200  added javascript function for adding related events

--- pentabarf/public/javascripts/add_functions.js
+++ pentabarf/public/javascripts/add_functions.js
@@ -442,3 +442,25 @@ function add_attachment()
 
   enumerator();
 }
+
+var related_event_counter = 0;
+
+function add_related_event(related_event_id)
+{
+  var row;
+
+  var row_id = related_event_counter;
+  related_event_counter++;
+
+  document.getElementById('related_event_table').style.display = "block";
+  
+  row = document.createElement("tr");
+  row.setAttribute("id","row_"+row_id);
+  
+  row.appendChild(create_element("select", 0, "related_event["+row_id+"][related_event_id]", event_names, related_event_id));
+  row.appendChild(create_element("input", "checkbox", "related_event["+row_id+"][delete]"));
+  row.appendChild(create_element("hidden", 0, "").firstChild);
+  document.getElementById('related_event_table_body').appendChild(row);
+  enumerator();
+}
+
