c90f2790731f42445cfc742de0cf0b176de670f8
Sven Klemm <sven@c3d2.de>
2005-10-04 00:22:37 +0000

    r2898@pulsar:  sven | 2005-10-04 02:22:32 +0200  nil represents false for datatype bool if field is not null

--- pentabarf/lib/momomoto/datatype/bool.rb
+++ pentabarf/lib/momomoto/datatype/bool.rb
@@ -7,10 +7,10 @@ module Momomoto
     class Bool < Base 
       def filter_set( value )
         case value
-          when 't', '1', true 
+          when 't', '1', 1, true 
             then 't'
           when nil
-            then nil
+            then property(:not_null) ? 'f' : nil
           else 
             'f'
         end
