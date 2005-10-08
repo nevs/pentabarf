9d05205a90653ba89b2232754d9f7e652aef675b
Tim Pritlove <tim@ccc.de>
2005-10-08 04:11:42 +0000

    re-enable custom icon display for person events

--- pentabarf/public/javascripts/add_functions.js
+++ pentabarf/public/javascripts/add_functions.js
@@ -285,8 +285,8 @@ function add_person_event(event_person_id, event_id, event_role_id, event_role_s
   if (event_id) {
     element = create_element("a", 0, "event_person[" + row_id + "][link]", p_base + "pentabarf/event/" + event_id, null, false );
     element.setAttribute("title", "Go to \""+event_names[event_id]+"\"");
-    element.appendChild(create_element("img", 0, "event_person["+row_id+"][image]", p_base + "images/icon-event-32x32.png", null, false ));
-    //element.appendChild(create_element("img", 0, "event_person["+row_id+"][image]", p_base + "images/event/" + event_id, null, false ));
+    //element.appendChild(create_element("img", 0, "event_person["+row_id+"][image]", p_base + "images/icon-event-32x32.png", null, false ));
+    element.appendChild(create_element("img", 0, "event_person["+row_id+"][image]", p_base + "images/event/" + event_id, null, false ));
     table_data = document.createElement("td");
     table_data.appendChild(element);
     table_row.appendChild(table_data);
