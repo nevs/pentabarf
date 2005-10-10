2924cda190fd9b11cc8441a1526c5dac1aec606c
Sven Klemm <sven@c3d2.de>
2005-10-10 19:36:55 +0000

    r3071@pulsar:  sven | 2005-10-10 21:36:46 +0200  added class for bot-login

---
+++ pentabarf/lib/momomoto/bot_login.rb
@@ -0,0 +1,31 @@
+module Momomoto
+
+  class Bot_login < Base
+
+    def initialize
+      super
+      @table = 'person'
+      @domain = 'own_person'
+      @fields = { :person_id    => Datatype::Integer.new( {:primary_key => true, :not_null => true, :serial => true} ),
+                  :login_name   => Datatype::Varchar.new( {} ),
+                  :preferences  => Datatype::Preferences.new( {} )
+      }
+    end
+
+    def self.authorize( login_name )
+      bot = self.new
+      bot.authorize( login_name )
+      bot
+    end
+
+    def authorize( login_name )
+      if login_name.to_s != '' && select( { :login_name => login_name } ) == 1
+        @@permissions = execute("SELECT get_permissions from get_permissions(#{self[:person_id]});").to_a.flatten
+        self.ui_language_id= self[:preferences][:current_language_id]
+        return true
+      end
+      false
+    end
+
+  end
+end
