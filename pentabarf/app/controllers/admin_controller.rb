fe41c633e66e4f3614c75a62bf27d01bbb23f487
Sven Klemm <sven@c3d2.de>
2005-10-11 21:38:30 +0000

    r3109@pulsar:  sven | 2005-10-11 23:37:16 +0200  added conflict setup to admin controller

--- pentabarf/app/controllers/admin_controller.rb
+++ pentabarf/app/controllers/admin_controller.rb
@@ -1,2 +1,37 @@
 class AdminController < ApplicationController
+  before_filter :authorize, :check_permission
+  after_filter :compress
+
+
+  def index
+  end
+
+  def conflict
+    @content_title = 'Conflict Setup'
+  end
+
+
+  protected
+
+    def check_permission
+      if @user.permission?('login_allowed') || params[:action] == 'meditation'
+        @preferences = @user.preferences
+        if params[:current_conference_id]
+          conf = Momomoto::Conference.find({:conference_id => params[:current_conference_id]})
+          if conf.length == 1
+            @preferences[:current_conference_id] = params[:current_conference_id].to_i
+            @user.preferences = @preferences
+            @user.write
+            redirect_to
+            return false
+          end
+        end
+        @current_conference_id = @preferences[:current_conference_id]
+        @current_language_id = @preferences[:current_language_id]
+      else
+        redirect_to( :action => :meditation )
+        false
+      end
+    end
+
 end
