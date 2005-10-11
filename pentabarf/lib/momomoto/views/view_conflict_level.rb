11fd4e4b40a86cc93c12c29f9b2534f0495da533
Sven Klemm <sven@c3d2.de>
2005-10-11 04:41:37 +0000

    r3095@pulsar:  sven | 2005-10-11 06:38:13 +0200  added class for view_conflict_level and added rank to view_conference_phase

--- pentabarf/lib/momomoto/views/view_conflict_level.rb
+++ pentabarf/lib/momomoto/views/view_conflict_level.rb
@@ -1,13 +1,13 @@
 module Momomoto
-  class View_conference_phase < Base
+  class View_conflict_level < Base
     def initialize
       super
       @domain = 'valuelist'
       @fields = {
-        :conference_phase_id => Datatype::Integer.new( {} ),
+        :conflict_level_id => Datatype::Integer.new( {} ),
         :language_id => Datatype::Integer.new( {} ),
-        :tag => Datatype::Varchar.new( {:length=>32} ),
-        :name => Datatype::Varchar.new( {} ),
+        :tag => Datatype::Varchar.new( {:length=>64} ),
+        :name => Datatype::Varchar.new( {:length=>64} ),
         :rank => Datatype::Integer.new( {} ),
         :language_tag => Datatype::Varchar.new( {:length=>32} )
       }
