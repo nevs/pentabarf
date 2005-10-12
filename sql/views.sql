698f37c67b619515c34ead5a7b9d11eeddc78410
Sven Klemm <sven@c3d2.de>
2005-10-12 20:30:43 +0000

    r3131@pulsar:  sven | 2005-10-12 22:30:35 +0200  corrected order for view_event_state_combined

--- sql/views.sql
+++ sql/views.sql
@@ -380,9 +380,11 @@ CREATE OR REPLACE VIEW view_event_state_combined AS
          view_event_state_progress.tag AS event_state_progress_tag,
          view_event_state_progress.language_id,
          view_event_state.tag AS event_state_tag,
-         view_event_state.name || ' ' || view_event_state_progress.name AS name
+         view_event_state.name || ' ' || view_event_state_progress.name AS name,
+         view_event_state.rank AS event_state_rank,
+         view_event_state_progress.rank AS event_state_progress_rank
     FROM view_event_state_progress
-         INNER JOIN view_event_state USING (event_state_id, language_id);
+         INNER JOIN view_event_state USING (event_state_id, language_id) ORDER BY event_state_rank,event_state_progress_rank;
 
 CREATE OR REPLACE VIEW view_jid_login AS
   SELECT person_im_id,
