992a66eaaf0d394edaa5bea63b8219e72ab0ecb7
Sven Klemm <sven@c3d2.de>
2005-10-12 23:41:02 +0000

    r3135@pulsar:  sven | 2005-10-12 23:10:22 +0200  added table conference_phase_conflict

--- sql/tables.sql
+++ sql/tables.sql
@@ -931,5 +931,17 @@ CREATE TABLE conflict_level_localized (
   PRIMARY KEY (conflict_level_id, language_id)
 ) WITHOUT OIDS;
 
+CREATE TABLE conference_phase_conflict(
+  conference_phase_conflict_id SERIAL NOT NULL,
+  conference_phase_id INTEGER NOT NULL,
+  conflict_id INTEGER NOT NULL,
+  conflict_level_id INTEGER NOT NULL,
+  FOREIGN KEY (conference_phase_id) REFERENCES conference_phase (conference_phase_id),
+  FOREIGN KEY (conflict_id) REFERENCES conflict (conflict_id),
+  FOREIGN KEY (conflict_level_id) REFERENCES conflict_level (conflict_level_id),
+  UNIQUE (conference_phase_id, conflict_id),
+  PRIMARY KEY (conference_phase_conflict_id)
+) WITHOUT OIDS;
+
 COMMIT;
 
