f1fcf43d6cffce3a8c87e187e9d992180843126f
Sven Klemm <sven@c3d2.de>
2005-10-14 23:14:26 +0000

    r3214@pulsar:  sven | 2005-10-15 01:14:15 +0200  added generic functions for retrieving all conflicts related to 2 events

--- sql/functions-conflict_event_event.sql
+++ sql/functions-conflict_event_event.sql
@@ -2,6 +2,34 @@
  * Conflicts concerning two events
 */
 
+-- returns all conclicts related to events
+CREATE OR REPLACE FUNCTION conflict_event_event(integer) RETURNS SETOF conflict_event_event_conflict AS '
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_conflict_event_event RECORD;
+    cur_conflict RECORD;
+
+  BEGIN
+
+    FOR cur_conflict IN
+      SELECT conflict.conflict_id, 
+             conflict.conflict_type_id, 
+             conflict.tag 
+        FROM conflict 
+             INNER JOIN conflict_type USING (conflict_type_id)
+       WHERE conflict_type.tag = ''event_event''
+    LOOP
+      FOR cur_conflict_event_event IN
+        EXECUTE ''SELECT conflict_id, event_id1, event_id2 FROM conflict_'' || cur_conflict.tag || ''('' || cur_conference_id || ''), ( SELECT '' || cur_conflict.conflict_id || '' AS conflict_id) AS conflict_id; ''
+      LOOP
+        RETURN NEXT cur_conflict_event_event;
+      END LOOP;
+    END LOOP;
+
+    RETURN;
+  END;
+' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
 -- returns events with conflicting timeslots
 CREATE OR REPLACE FUNCTION conflict_event_event_time(integer) RETURNS SETOF conflict_event_event AS '
   DECLARE 
@@ -14,7 +42,7 @@ CREATE OR REPLACE FUNCTION conflict_event_event_time(integer) RETURNS SETOF conf
     FOR cur_event IN
       SELECT event_id, start_time, duration, day, room_id FROM event INNER JOIN event_state USING (event_state_id) 
         WHERE event.conference_id = cur_conference_id AND
-              event_state.tag = ''confirmed'' AND
+              event_state.tag = ''accepted'' AND
               event.day IS NOT NULL AND
               event.start_time IS NOT NULL
     LOOP
@@ -23,14 +51,14 @@ CREATE OR REPLACE FUNCTION conflict_event_event_time(integer) RETURNS SETOF conf
           WHERE event.day IS NOT NULL AND
                 event.start_time IS NOT NULL AND
                 event.day = cur_event.day AND 
-                event_state.tag = ''confirmed'' AND
+                event_state.tag = ''accepted'' AND
                 event.event_id <> cur_event.event_id AND
                 event.conference_id = cur_conference_id AND
                 event.room_id = cur_event.room_id AND
                 (( event.start_time >= cur_event.start_time AND
                    event.start_time < cur_event.start_time + cur_event.duration ) OR
                  ( event.start_time + event.duration > cur_event.start_time AND
-                   event.start_time + event.duratION <= cur_event.start_time + cur_event.duration ))
+                   event.start_time + event.duration <= cur_event.start_time + cur_event.duration ))
       LOOP
         conflicting_event.event_id1 := cur_event.event_id;
         conflicting_event.event_id2 := cur_event2.event_id;
