e0834910d4166876970ce42c399197779f8a655d
Sven Klemm <sven@c3d2.de>
2005-10-14 17:30:27 +0000

    r3189@pulsar:  sven | 2005-10-14 19:30:19 +0200  added further types for generic conflict functions

--- sql/types.sql
+++ sql/types.sql
@@ -5,31 +5,55 @@
 */
 
 CREATE TYPE conflict_person AS (
-  person_id INTEGER  
+  person_id   INTEGER  
 );
 
 CREATE TYPE conflict_person_conflict AS (
   conflict_id INTEGER,
-  person_id INTEGER  
+  person_id   INTEGER  
 );
 
 CREATE TYPE conflict_event_person AS (
-  event_id  INTEGER,
-  person_id INTEGER
+  event_id    INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict_event_person_conflict AS (
+  conflict_id INTEGER,
+  event_id    INTEGER,
+  person_id   INTEGER
 );
 
 CREATE TYPE conflict_event_person_event AS (
-  event_id1 INTEGER,
-  event_id2 INTEGER,
-  person_id INTEGER
+  event_id1   INTEGER,
+  event_id2   INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict_event_person_event_conflict AS (
+  conflict_id INTEGER,
+  event_id1   INTEGER,
+  event_id2   INTEGER,
+  person_id   INTEGER
 );
 
 CREATE TYPE conflict_event AS (
-  event_id  INTEGER
+  event_id    INTEGER
+);
+
+CREATE TYPE conflict_event_conflict AS (
+  conflict_id INTEGER,
+  event_id    INTEGER
 );
 
 CREATE TYPE conflict_event_event AS (
-  event_id1 INTEGER,
-  event_id2 INTEGER
+  event_id1   INTEGER,
+  event_id2   INTEGER
+);
+
+CREATE TYPE conflict_event_event_conflict AS (
+  conflict_id INTEGER,
+  event_id1   INTEGER,
+  event_id2   INTEGER
 );
 
