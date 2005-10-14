b4ec1f9709bc83fa9e3d0f1594a1d2a8269ce393
Sven Klemm <sven@c3d2.de>
2005-10-14 17:17:57 +0000

    r3184@pulsar:  sven | 2005-10-14 19:17:25 +0200  added type for function returning all conflict_person conflicts

--- sql/types.sql
+++ sql/types.sql
@@ -8,6 +8,11 @@ CREATE TYPE conflict_person AS (
   person_id INTEGER  
 );
 
+CREATE TYPE conflict_person_conflict AS (
+  conflict_id INTEGER,
+  person_id INTEGER  
+);
+
 CREATE TYPE conflict_event_person AS (
   event_id  INTEGER,
   person_id INTEGER
