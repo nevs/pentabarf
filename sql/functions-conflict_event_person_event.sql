bdf2e707b07dfd3bad2c02adac1b6fcdb0420b88
Sven Klemm <sven@c3d2.de>
2005-10-15 02:06:34 +0000

    r3220@pulsar:  sven | 2005-10-15 04:06:30 +0200  added generic function to retrieve conflicts concerning 2 event and a person

--- sql/functions-conflict_event_person_event.sql
+++ sql/functions-conflict_event_person_event.sql
@@ -1,4 +1,32 @@
 
+-- returns all conclicts related to events
+CREATE OR REPLACE FUNCTION conflict_event_person_event(integer) RETURNS SETOF conflict_event_person_event_conflict AS '
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_conflict_event_person_event RECORD;
+    cur_conflict RECORD;
+
+  BEGIN
+
+    FOR cur_conflict IN
+      SELECT conflict.conflict_id, 
+             conflict.conflict_type_id, 
+             conflict.tag 
+        FROM conflict 
+             INNER JOIN conflict_type USING (conflict_type_id)
+       WHERE conflict_type.tag = ''event_person_event''
+    LOOP
+      FOR cur_conflict_event_person_event IN
+        EXECUTE ''SELECT conflict_id, event_id1, event_id2, person_id FROM conflict_'' || cur_conflict.tag || ''('' || cur_conference_id || ''), ( SELECT '' || cur_conflict.conflict_id || '' AS conflict_id) AS conflict_id; ''
+      LOOP
+        RETURN NEXT cur_conflict_event_person_event;
+      END LOOP;
+    END LOOP;
+
+    RETURN;
+  END;
+' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
 -- returns event_persons with conflicting events
 CREATE OR REPLACE FUNCTION conflict_event_person_event_time(integer) RETURNS SETOF conflict_event_person_event AS '
   DECLARE 
@@ -19,7 +47,7 @@ CREATE OR REPLACE FUNCTION conflict_event_person_event_time(integer) RETURNS SET
         WHERE event_role.tag IN (''speaker'', ''moderator'') AND
               event_role_state.tag = ''confirmed'' AND
               event.conference_id = cur_conference_id AND
-              event_state.tag = ''confirmed'' AND
+              event_state.tag = ''accepted'' AND
               event.day IS NOT NULL AND
               event.start_time IS NOT NULL
     LOOP
@@ -42,7 +70,7 @@ CREATE OR REPLACE FUNCTION conflict_event_person_event_time(integer) RETURNS SET
                    event.start_time < cur_speaker.start_time + cur_speaker.duration ) OR
                  ( event.start_time + event.duration > cur_speaker.start_time AND
                    event.start_time + event.duration <= cur_speaker.start_time + cur_speaker.duration )) AND
-                event_state.tag = ''confirmed'' AND
+                event_state.tag = ''accepted'' AND
                 event_role.tag IN (''speaker'', ''moderator'') AND
                 event_role_state.tag = ''confirmed'' AND
                 event_person.person_id = cur_speaker.person_id
