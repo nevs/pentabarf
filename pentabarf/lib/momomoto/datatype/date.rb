cb66e40b8d35ab1b7a4ee411bd743eabce653acd
Sven Klemm <sven@c3d2.de>
2005-10-18 23:38:09 +0000

    r3373@pulsar:  sven | 2005-10-19 01:38:00 +0200  changed internal value for Datatype::Date to ::Date

--- pentabarf/lib/momomoto/datatype/date.rb
+++ pentabarf/lib/momomoto/datatype/date.rb
@@ -6,6 +6,26 @@ module Momomoto
 
     class Date < Base 
 
+      def filter_write( data )
+        return 'NULL' if data == nil
+        "'" + data.strftime('%Y-%m-%d') + "'"
+      end
+
+      def filter_read( value )
+        return nil if value.nil?
+        ::Date.parse( value )
+      end
+
+      def filter_set( value )
+        if value.kind_of?( String ) && value.length > 0
+          return ::Date.parse( value )
+        elsif value.kind_of?( ::Date )
+          return value
+        else
+          return nil
+        end
+      end
+
     end
     
   end
