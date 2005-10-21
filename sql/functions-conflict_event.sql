284b4d5b88d9c10ad7d92d1a65dffa4f73e49513
Sven Klemm <sven@c3d2.de>
2005-10-21 21:49:08 +0000

    r3453@pulsar:  sven | 2005-10-21 23:29:45 +0200  added conflict event with inconsistent public link

--- sql/functions-conflict_event.sql
+++ sql/functions-conflict_event.sql
@@ -156,7 +156,7 @@ CREATE OR REPLACE FUNCTION conflict_event_inconsistent_tag(INTEGER) RETURNS SETO
   END;
 ' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
--- returns all accepted events with inconsistent tag
+-- returns all accepted events with no paper but the f_paper flag set
 CREATE OR REPLACE FUNCTION conflict_event_no_paper(INTEGER) RETURNS SETOF conflict_event AS '
   DECLARE
     cur_conference_id ALIAS FOR $1;
@@ -173,6 +173,26 @@ CREATE OR REPLACE FUNCTION conflict_event_no_paper(INTEGER) RETURNS SETOF confli
                                        INNER JOIN attachment_type USING (attachment_type_id)
                                  WHERE event_id = event.event_id AND
                                        attachment_type.tag = ''paper'')
+    LOOP
+      RETURN NEXT cur_event;
+    END LOOP;
+    RETURN;
+  END;
+' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
+-- returns all events with inconsistent public links
+CREATE OR REPLACE FUNCTION conflict_event_inconsistent_public_link(INTEGER) RETURNS SETOF conflict_event AS '
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_event conflict_event%ROWTYPE;
+  BEGIN
+    FOR cur_event IN
+      SELECT event_id
+        FROM event
+       WHERE conference_id = cur_conference_id AND
+             EXISTS (SELECT 1 FROM event_link
+                             WHERE event_id = event.event_id AND
+                                   url NOT SIMILAR TO ''[a-z]+:%'')
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
