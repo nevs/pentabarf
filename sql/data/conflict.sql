8cdd60caf3cb1a7dceaa5c0d06a1356b165902aa
Sven Klemm <sven@c3d2.de>
2005-10-21 13:42:48 +0000

    r3416@pulsar:  sven | 2005-10-21 15:40:41 +0200  added conflict data

---
+++ sql/data/conflict.sql
@@ -0,0 +1,34 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UNICODE';
+SET check_function_bodies = false;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for TOC entry 3 (OID 89152)
+-- Name: conflict; Type: TABLE DATA; Schema: public; Owner: pentabarf
+--
+
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (1, 1, 'person_no_email');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (2, 2, 'event_person_language');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (3, 3, 'event_person_event_time');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (4, 4, 'event_no_speaker');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (5, 4, 'event_no_coordinator');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (6, 4, 'event_incomplete');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (7, 5, 'event_event_time');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (8, 4, 'event_missing_tag');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (9, 5, 'event_event_duplicate_tag');
+INSERT INTO conflict (conflict_id, conflict_type_id, tag) VALUES (10, 4, 'event_inconsistent_tag');
+
+
+--
+-- TOC entry 2 (OID 89150)
+-- Name: conflict_conflict_id_seq; Type: SEQUENCE SET; Schema: public; Owner: pentabarf
+--
+
+SELECT pg_catalog.setval('conflict_conflict_id_seq', 10, true);
+
+
