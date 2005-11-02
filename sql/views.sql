12f2c1645ee474785ec1645afc12b6b4f165e7d2
Sven Klemm <sven@c3d2.de>
2005-11-02 00:54:03 +0000

    r3659@pulsar:  sven | 2005-11-02 01:53:55 +0100  added event_state_progress to view_event_person

--- sql/views.sql
+++ sql/views.sql
@@ -184,6 +184,8 @@ CREATE OR REPLACE VIEW view_event_person AS
          view_event_state.language_tag, 
          view_event_state.tag AS event_state_tag, 
          view_event_state.name AS event_state, 
+         view_event_state_progress.tag AS event_state_progress_tag, 
+         view_event_state_progress.name AS event_state_progress, 
          view_event_role.tag AS event_role_tag, 
          view_event_role.name AS event_role, 
          view_event_role_state.tag AS event_role_state_tag, 
@@ -193,6 +195,9 @@ CREATE OR REPLACE VIEW view_event_person AS
          INNER JOIN conference USING (conference_id)
          INNER JOIN view_person USING (person_id) 
          INNER JOIN view_event_state USING (event_state_id)
+         INNER JOIN view_event_state_progress ON (
+               view_event_state_progress.event_state_progress_id = event.event_state_progress_id AND
+               view_event_state_progress.language_id = view_event_state.language_id)
          INNER JOIN view_event_role ON (
                view_event_role.event_role_id = event_person.event_role_id AND 
                view_event_state.language_id = view_event_role.language_id)
