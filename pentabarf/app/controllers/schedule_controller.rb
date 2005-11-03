1cc7577e5316250a0e86cd050a85d37ef5e702b8
Sven Klemm <sven@c3d2.de>
2005-11-03 21:51:23 +0000

    r3690@pulsar:  sven | 2005-11-03 22:51:19 +0100  implemented schedule table

--- pentabarf/app/controllers/schedule_controller.rb
+++ pentabarf/app/controllers/schedule_controller.rb
@@ -6,26 +6,36 @@ class ScheduleController < ApplicationController
 
   end
   
+  def day
+    @day = params[:id].to_i
+    @content_title = "Day #{@day}"
+    @rooms = Momomoto::View_room.find({:conference_id=>@conference.conference_id, :language_id=>120}, nil, 'rank')
+    @events = Momomoto::View_schedule_event.find({:conference_id=>@conference.conference_id,:translated_id=>120}, nil, 'lower(title),lower(subtitle)' )
+  end
+  
   def speaker 
     @content_title = "Speaker and moderators"
-    @speaker = Momomoto::View_schedule_person.find({:conference_id=>params[:conference_id]}, nil, 'lower(name)')
+    @speaker = Momomoto::View_schedule_person.find({:conference_id=>@conference.conference_id}, nil, 'lower(name)')
   end
 
   def events
     @content_title = "Lectures and workshops"
-    @events = Momomoto::View_schedule_event.find({:conference_id=>params[:conference_id],:translated_id=>120}, nil, 'lower(title),lower(subtitle)' )
+    @events = Momomoto::View_schedule_event.find({:conference_id=>@conference.conference_id,:translated_id=>120}, nil, 'lower(title),lower(subtitle)' )
   end
 
   def css
-    conference = Momomoto::Conference.find({:conference_id=>params[:conference_id]})
     @response.headers['Content-Type'] = 'text/css'
-    render_text(conference.nil? ? "" : conference.css)
+    render_text(@conference.css.nil? ? "" : @conference.css)
   end
 
   def check_permission
     @conference = Momomoto::Conference.new
-    return false unless @conference.select({:conference_id => params[:conference_id]}) == 1
-    true
+    if params[:conference_id].to_s.match(/^\d+$/)
+      @conference.select({:conference_id => params[:conference_id]})
+    else
+      @conference.select({:acronym => params[:conference_id]})
+    end
+    return @conference.length == 1 ? true : false
   end
 
 end
