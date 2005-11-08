aa27347059cb3896addc96ccbca9726ae6f13365
Sven Klemm <sven@c3d2.de>
2005-11-08 23:00:40 +0000

    r3784@pulsar:  sven | 2005-11-09 00:00:35 +0100  renamed description field to title

--- pentabarf/lib/momomoto/tables/event_link.rb
+++ pentabarf/lib/momomoto/tables/event_link.rb
@@ -7,7 +7,7 @@ module Momomoto
         :event_link_id => Datatype::Integer.new( {:not_null=>true, :default=>true, :primary_key=>true, :serial=>true} ),
         :event_id => Datatype::Integer.new( {:not_null=>true} ),
         :url => Datatype::Varchar.new( {:not_null=>true, :length=>1024} ),
-        :description => Datatype::Varchar.new( {:length=>256} ),
+        :title => Datatype::Varchar.new( {:length=>256} ),
         :rank => Datatype::Integer.new( {} )
       }
     end
