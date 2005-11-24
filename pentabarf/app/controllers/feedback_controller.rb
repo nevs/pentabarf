714ea6f05bca5fbf0441f8c315c5392ef0104c24
Sven Klemm <sven@c3d2.de>
2005-11-24 00:01:14 +0000

    r3923@pulsar:  sven | 2005-11-24 01:01:10 +0100  added event page to feedback controller

--- pentabarf/app/controllers/feedback_controller.rb
+++ pentabarf/app/controllers/feedback_controller.rb
@@ -1,2 +1,31 @@
 class FeedbackController < ApplicationController
+  before_filter :check_permission
+  after_filter :compress
+
+  def css
+    @conference = Momomoto::Conference.find({:conference_id => 7})
+    @response.headers['Content-Type'] = 'text/css'
+    render_text(@conference.css.nil? ? "" : @conference.css)
+  end
+
+  def event
+    @conference = Momomoto::Conference.find({:conference_id => @conference_id})
+    @event = Momomoto::View_event.find({:event_id => @event_id, :language_id => @language_id})
+    @content_title = "Feedback"
+  end
+
+  def save_event
+    
+  end
+
+  protected
+
+  def check_permission
+    @conference_id = 7
+    @event_id = 561
+    @language_id = 120
+    Momomoto::Base.ui_language_id = @language_id
+    true
+  end
+
 end
