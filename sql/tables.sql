03fbab1c77ff08c55f480e46de95f56c98dcceed
Sven Klemm <sven@c3d2.de>
2005-12-22 02:58:24 +0000

    r4169@pulsar:  sven | 2005-12-22 03:58:16 +0100  order fix for table creation

--- sql/tables.sql
+++ sql/tables.sql
@@ -245,6 +245,42 @@ CREATE TABLE role_authorisation (
   PRIMARY KEY (role_id, authorisation_id)
 ) WITHOUT OIDS;
   
+CREATE TABLE person (
+  person_id SERIAL NOT NULL,
+  login_name VARCHAR(32) UNIQUE,
+  password CHAR(48),
+  title VARCHAR(32),
+  gender BOOL,
+  first_name VARCHAR(64),
+  middle_name VARCHAR(64),
+  last_name VARCHAR(64),
+  public_name VARCHAR(64),
+  nickname VARCHAR(64),
+  address VARCHAR(256),
+  street VARCHAR(64),
+  street_postcode VARCHAR(10),
+  po_box VARCHAR(10),
+  po_box_postcode VARCHAR(10),
+  city VARCHAR(64),
+  country_id INTEGER,
+  email_contact VARCHAR(64),
+  iban VARCHAR(128),
+  bic VARCHAR(32),
+  bank_name VARCHAR(128),
+  account_owner VARCHAR(128),
+  gpg_key TEXT,
+  preferences TEXT,
+  f_conflict BOOL NOT NULL DEFAULT FALSE,
+  f_deleted BOOL NOT NULL DEFAULT FALSE,
+  f_spam BOOL NOT NULL DEFAULT FALSE,
+  last_login TIMESTAMP WITH TIME ZONE,
+  last_modified TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  last_modified_by INTEGER,
+  FOREIGN KEY (country_id) REFERENCES country (country_id),
+  FOREIGN KEY (last_modified_by) REFERENCES person (person_id),
+  PRIMARY KEY (person_id)
+) WITHOUT OIDS;
+
 CREATE TABLE conference_phase (
   conference_phase_id SERIAL NOT NULL,
   tag VARCHAR(32) NOT NULL UNIQUE,
@@ -377,42 +413,6 @@ CREATE TABLE room_localized (
   FOREIGN KEY (room_id) REFERENCES room (room_id),
   FOREIGN KEY (language_id) REFERENCES language (language_id),
   PRIMARY KEY (room_id, language_id)
-) WITHOUT OIDS;
-
-CREATE TABLE person (
-  person_id SERIAL NOT NULL,
-  login_name VARCHAR(32) UNIQUE,
-  password CHAR(48),
-  title VARCHAR(32),
-  gender BOOL,
-  first_name VARCHAR(64),
-  middle_name VARCHAR(64),
-  last_name VARCHAR(64),
-  public_name VARCHAR(64),
-  nickname VARCHAR(64),
-  address VARCHAR(256),
-  street VARCHAR(64),
-  street_postcode VARCHAR(10),
-  po_box VARCHAR(10),
-  po_box_postcode VARCHAR(10),
-  city VARCHAR(64),
-  country_id INTEGER,
-  email_contact VARCHAR(64),
-  iban VARCHAR(128),
-  bic VARCHAR(32),
-  bank_name VARCHAR(128),
-  account_owner VARCHAR(128),
-  gpg_key TEXT,
-  preferences TEXT,
-  f_conflict BOOL NOT NULL DEFAULT FALSE,
-  f_deleted BOOL NOT NULL DEFAULT FALSE,
-  f_spam BOOL NOT NULL DEFAULT FALSE,
-  last_login TIMESTAMP WITH TIME ZONE,
-  last_modified TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  last_modified_by INTEGER,
-  FOREIGN KEY (country_id) REFERENCES country (country_id),
-  FOREIGN KEY (last_modified_by) REFERENCES person (person_id),
-  PRIMARY KEY (person_id)
 ) WITHOUT OIDS;
 
 CREATE TABLE conference_person (
