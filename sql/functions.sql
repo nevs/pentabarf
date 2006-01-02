7994b01281642038c4497f8308826a7ab8c40b4f
Sven Klemm <sven@c3d2.de>
2006-01-02 23:48:35 +0000

    r4233@pulsar:  sven | 2006-01-03 00:48:31 +0100  added function for copying events

--- sql/functions.sql
+++ sql/functions.sql
@@ -73,3 +73,107 @@ CREATE OR REPLACE FUNCTION person_phone_by_type(integer, text) RETURNS text AS '
   END;
 ' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
+CREATE OR REPLACE FUNCTION copy_event(integer, integer, integer) RETURNS INTEGER AS '
+  DECLARE
+    cur_event_id ALIAS FOR $1;
+    cur_conference_id ALIAS FOR $2;
+    cur_person_id ALIAS FOR $3;
+    new_event_id INTEGER;
+
+  BEGIN
+    SELECT INTO new_event_id nextval(''event_event_id_seq''::regclass);
+    INSERT INTO event( event_id, 
+                       conference_id, 
+                       title, 
+                       subtitle, 
+                       tag,
+                       duration, 
+                       event_type_id, 
+                       event_origin_id, 
+                       event_state_id, 
+                       event_state_progress_id, 
+                       language_id, 
+                       abstract, 
+                       description, 
+                       resources, 
+                       f_public, 
+                       f_paper, 
+                       f_slides, 
+                       f_unmoderated,
+                       last_modified_by )
+                SELECT new_event_id, 
+                       cur_conference_id, 
+                       title, 
+                       subtitle, 
+                       tag,
+                       duration, 
+                       event_type_id, 
+                       (SELECT event_origin_id FROM event_origin WHERE tag = ''idea''), 
+                       (SELECT event_state_id FROM event_state WHERE tag = ''undecided''), 
+                       (SELECT event_state_progress_id 
+                          FROM event_state_progress 
+                               INNER JOIN event_state 
+                                  ON (event_state.tag = ''undecided'' AND 
+                                      event_state.event_state_id = event_state_progress.event_state_id) 
+                         WHERE event_state_progress.tag = ''new''), 
+                       language_id, 
+                       abstract, 
+                       description, 
+                       resources, 
+                       f_public, 
+                       f_paper, 
+                       f_slides, 
+                       f_unmoderated,
+                       cur_person_id
+                  FROM event WHERE event_id = cur_event_id;
+
+    INSERT INTO event_image( event_id,
+                             mime_type_id,
+                             image,
+                             last_modified_by )
+                      SELECT new_event_id,
+                             mime_type_id,
+                             image,
+                             cur_person_id
+                        FROM event_image 
+                       WHERE event_id = cur_event_id;
+
+    INSERT INTO event_person( event_id,
+                              person_id,
+                              event_role_id,
+                              event_role_state_id,
+                              rank,
+                              last_modified_by )
+                       SELECT new_event_id,
+                              person_id,
+                              event_person.event_role_id,
+                              event_person.event_role_state_id,
+                              event_person.rank,
+                              cur_person_id
+                         FROM event_person
+                              INNER JOIN event_role ON
+                                  (event_role.event_role_id = event_person.event_role_id AND
+                                   event_role.tag IN (''speaker'', ''moderator''))
+                              INNER JOIN event_role_state ON 
+                                  (event_person.event_role_id = event_role_state.event_role_id AND 
+                                   event_role_state.tag = ''unclear'')
+                        WHERE event_person.event_id = cur_event_id;
+
+    INSERT INTO event_link( event_id,
+                            url,
+                            rank,
+                            title,
+                            last_modified_by )
+                     SELECT new_event_id,
+                            url,
+                            rank,
+                            title,
+                            cur_person_id
+                       FROM event_link
+                      WHERE event_id = cur_event_id;
+
+    RETURN new_event_id;
+
+  END;
+' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
