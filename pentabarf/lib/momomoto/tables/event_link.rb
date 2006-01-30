f94452c9a97affb93f3428fa24dfcc31c0f83749
Sven Klemm <sven@c3d2.de>
2006-01-30 14:00:26 +0000

    r4530@pulsar:  sven | 2006-01-30 15:00:19 +0100  added missing columns to event_link

--- pentabarf/lib/momomoto/tables/event_link.rb
+++ pentabarf/lib/momomoto/tables/event_link.rb
@@ -7,9 +7,11 @@ module Momomoto
       @fields = {
         :event_link_id => Datatype::Integer.new( {:not_null=>true, :default=>true, :primary_key=>true, :serial=>true} ),
         :event_id => Datatype::Integer.new( {:not_null=>true} ),
-        :url => Datatype::Varchar.new( {:not_null=>true, :length=>1024} ),
+        :url => Datatype::Varchar.new( {:length=>1024, :not_null=>true} ),
+        :rank => Datatype::Integer.new( {} ),
         :title => Datatype::Varchar.new( {:length=>256} ),
-        :rank => Datatype::Integer.new( {} )
+        :last_modified => Datatype::Timestamp.new( {:with_timezone=>true, :not_null=>true, :default=>true} ),
+        :last_modified_by => Datatype::Integer.new( {} )
       }
     end
   end
