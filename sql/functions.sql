4ee1cf2bfd7903aa82b89b60bd51dbfea8894d69
Sven Klemm <sven@c3d2.de>
2006-04-30 21:54:40 +0000

    r4842@pulsar:  sven | 2006-04-30 23:53:53 +0200  bugfix for stored procedure to reset password

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,4 +1,5 @@
 \i functions/functions.sql
+\i functions/functions-account.sql
 \i functions/functions-related.sql
 \i functions/functions-conflict_person.sql
 \i functions/functions-conflict_event.sql
--- sql/functions/functions-account.sql
+++ sql/functions/functions-account.sql
@@ -65,14 +65,16 @@ CREATE OR REPLACE FUNCTION account_forgot_password(integer, char(64)) RETURNS IN
   DECLARE
     cur_person_id ALIAS FOR $1;
     cur_activation_string ALIAS FOR $2;
+    cur_person RECORD;
   BEGIN
     DELETE FROM account_password_reset WHERE password_reset < now() + '-1 day';   
     
-    SELECT INTO cur_person person_id FROM password_reset WHERE person_id = cur_person_id;
+    SELECT INTO cur_person person_id FROM account_password_reset WHERE person_id = cur_person_id;
     IF FOUND THEN
       RAISE EXCEPTION 'This account has already been reset in the last 24 hours.';
     END IF;
-    INSERT INTO password_reset(person_id, activation_string, password_reset) VALUES (cur_person_id, cur_activation_string, now());
+    INSERT INTO account_password_reset(person_id, activation_string, password_reset) VALUES (cur_person_id, cur_activation_string, now());
 
+    RETURN cur_person_id;
   END;
 $$ LANGUAGE plpgsql RETURNS NULL ON NULL INPUT;
