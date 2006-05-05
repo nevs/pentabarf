66ce9c1b177f0135288ef5ba6f61b194352ce98f
Sven Klemm <sven@c3d2.de>
2006-05-05 09:31:16 +0000

    r4868@pulsar:  sven | 2006-05-05 11:31:11 +0200  removed all account functions from submission controller

--- pentabarf/app/controllers/submission_controller.rb
+++ pentabarf/app/controllers/submission_controller.rb
@@ -1,7 +1,7 @@
 class SubmissionController < ApplicationController
   before_filter :check_conference
-  before_filter :authorize, :except => [:index, :create_account, :new_account, :activate_account, :account_done, :logout, :reset_password]
-  before_filter :check_permission, :except => [:index, :create_account, :new_account, :activate_account, :account_done, :logout, :reset_password]
+  before_filter :authorize, :except => [:index, :logout]
+  before_filter :check_permission, :except => [:index, :logout]
   before_filter :transparent_authorize
   after_filter :compress
 
@@ -12,32 +12,6 @@ class SubmissionController < ApplicationController
 
   def login
     redirect_to({:action=>:events,:conference=>@conference.acronym})
-  end
-
-  def new_account
-    @content_title = 'Create account'
-  end
-
-  def create_account
-    return redirect_to(:action=>:index,:conference=>@conference.acronym) unless params[:person]
-    raise "Passwords do not match" if params[:person][:password] != params[:password]
-    raise "Invalid email address" unless params[:person][:email_contact].match(/[\w_.+-]+@([\w.+_-]+\.)+\w{2,3}$/)
-    raise "This login name is already in use." unless Momomoto::Person.find({:login_name=>params[:person][:login_name]}).nil?
-    raise "This email address is already in use." unless Momomoto::Person.find({:email_contact=>params[:person][:email_contact]}).nil?
-    Notifier::deliver_activate_account( account.login_name, account.email_contact, url_for({:action=>:activate_account,:conference=>@conference.acronym,:id=>account.activation_string}) )
-    account = Momomoto::Create_account.find({:login_name=>params[:person][:login_name],:password=>params[:person][:password],:email_contact=>params[:person][:email_contact],:activation_string=>random_string})
-
-    redirect_to({:action=>:account_done,:conference=>@conference.acronym})
-  end
-
-  def account_done
-
-  end
-
-  def activate_account
-    raise "Invalid activation sequence." unless params[:id].length == 64
-    Momomoto::Activate_account.find({:activation_string=>params[:id]})
-    redirect_to({:action=>:login,:conference=>@conference.acronym})
   end
 
   def person
--- pentabarf/app/controllers/user_controller.rb
+++ pentabarf/app/controllers/user_controller.rb
@@ -3,6 +3,32 @@ class UserController < ApplicationController
   before_filter :authorize, :except => [:forgot_password]
   after_filter :compress
 
+  def new_account
+    @content_title = 'Create account'
+  end
+
+  def create_account
+    return redirect_to(:action=>:index,:conference=>@conference.acronym) unless params[:person]
+    raise "Passwords do not match" if params[:person][:password] != params[:password]
+    raise "Invalid email address" unless params[:person][:email_contact].match(/[\w_.+-]+@([\w.+_-]+\.)+\w{2,3}$/)
+    raise "This login name is already in use." unless Momomoto::Person.find({:login_name=>params[:person][:login_name]}).nil?
+    raise "This email address is already in use." unless Momomoto::Person.find({:email_contact=>params[:person][:email_contact]}).nil?
+    Notifier::deliver_activate_account( account.login_name, account.email_contact, url_for({:action=>:activate_account,:conference=>@conference.acronym,:id=>account.activation_string}) )
+    account = Momomoto::Create_account.find({:login_name=>params[:person][:login_name],:password=>params[:person][:password],:email_contact=>params[:person][:email_contact],:activation_string=>random_string})
+
+    redirect_to({:action=>:account_done,:conference=>@conference.acronym})
+  end
+
+  def account_done
+
+  end
+
+  def activate_account
+    raise "Invalid activation sequence." unless params[:id].length == 64
+    Momomoto::Activate_account.find({:activation_string=>params[:id]})
+    redirect_to({:action=>:login,:conference=>@conference.acronym})
+  end
+
   def forgot_password
     if params[:commit]
       person = Momomoto::Person.find({:login_name=>params[:person][:login_name], :email_contact=>params[:person][:email_contact]})
