fb25f6acc21e5fd2d3e49c911f8276d38384fc45
Sven Klemm <sven@c3d2.de>
2006-05-15 12:29:58 +0000

    r4193@pinealgland:  sven | 2006-05-15 14:24:31 +0200  added conflict functions for checking length and description of events

--- sql/functions/functions-conflict_event.sql
+++ sql/functions/functions-conflict_event.sql
@@ -3,7 +3,7 @@
 */
 
 -- returns all conclicts related to events
-CREATE OR REPLACE FUNCTION conflict_event(integer) RETURNS SETOF conflict_event_conflict AS '
+CREATE OR REPLACE FUNCTION conflict_event(integer) RETURNS SETOF conflict_event_conflict AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_conflict_event RECORD;
@@ -19,12 +19,12 @@ CREATE OR REPLACE FUNCTION conflict_event(integer) RETURNS SETOF conflict_event_
              INNER JOIN conference_phase_conflict USING (conflict_id)
              INNER JOIN conference USING (conference_phase_id)
              INNER JOIN conflict_level USING (conflict_level_id)
-       WHERE conflict_type.tag = ''event'' AND
-             conflict_level.tag <> ''silent'' AND
+       WHERE conflict_type.tag = 'event' AND
+             conflict_level.tag <> 'silent' AND
              conference.conference_id = cur_conference_id
     LOOP
       FOR cur_conflict_event IN
-        EXECUTE ''SELECT conflict_id, event_id FROM conflict_'' || cur_conflict.tag || ''('' || cur_conference_id || ''), ( SELECT '' || cur_conflict.conflict_id || '' AS conflict_id) AS conflict_id; ''
+        EXECUTE 'SELECT conflict_id, event_id FROM conflict_' || cur_conflict.tag || '(' || cur_conference_id || '), ( SELECT ' || cur_conflict.conflict_id || ' AS conflict_id) AS conflict_id; '
       LOOP
         RETURN NEXT cur_conflict_event;
       END LOOP;
@@ -32,10 +32,10 @@ CREATE OR REPLACE FUNCTION conflict_event(integer) RETURNS SETOF conflict_event_
 
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all confirmed events without confirmed speaker/moderator
-CREATE OR REPLACE FUNCTION conflict_event_no_speaker(integer) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_speaker(integer) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -47,26 +47,26 @@ CREATE OR REPLACE FUNCTION conflict_event_no_speaker(integer) RETURNS SETOF conf
              INNER JOIN event_state USING (event_state_id)
              INNER JOIN event_state_progress USING (event_state_progress_id)
         WHERE event.conference_id = cur_conference_id AND
-              event_state.tag = ''accepted'' AND
-              event_state_progress.tag = ''confirmed''
+              event_state.tag = 'accepted' AND
+              event_state_progress.tag = 'confirmed'
     LOOP
       IF NOT EXISTS (SELECT 1 FROM event_person 
                               INNER JOIN event_role USING (event_role_id) 
                               INNER JOIN event_role_state USING (event_role_state_id) 
                        WHERE event_person.event_id = cur_event.event_id AND
-                             event_role.tag IN (''speaker'', ''moderator'') AND
-                             event_role_state.tag = ''confirmed'')
+                             event_role.tag IN ('speaker', 'moderator') AND
+                             event_role_state.tag = 'confirmed')
       THEN
         RETURN NEXT cur_event;
       END IF;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 
 -- returns all events without coordinator
-CREATE OR REPLACE FUNCTION conflict_event_no_coordinator(integer) RETURNS setof conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_coordinator(integer) RETURNS setof conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -79,17 +79,17 @@ CREATE OR REPLACE FUNCTION conflict_event_no_coordinator(integer) RETURNS setof 
       IF NOT EXISTS (SELECT 1 FROM event_person 
                                    INNER JOIN event_role USING (event_role_id)
                        WHERE event_person.event_id = cur_event.event_id AND
-                             event_role.tag = ''coordinator'')
+                             event_role.tag = 'coordinator')
       THEN
         RETURN NEXT cur_event;
       END IF;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with incomplete day/time/room
-CREATE OR REPLACE FUNCTION conflict_event_incomplete(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_incomplete(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -100,8 +100,8 @@ CREATE OR REPLACE FUNCTION conflict_event_incomplete(INTEGER) RETURNS SETOF conf
              INNER JOIN event_state USING (event_state_id)
              INNER JOIN event_state_progress USING (event_state_progress_id)
         WHERE conference_id = cur_conference_id AND
-              event_state.tag = ''accepted'' AND
-              event_state_progress.tag = ''confirmed'' AND
+              event_state.tag = 'accepted' AND
+              event_state_progress.tag = 'confirmed' AND
               (day IS NULL OR 
                room_id IS NULL OR 
                start_time IS NULL) AND
@@ -113,10 +113,10 @@ CREATE OR REPLACE FUNCTION conflict_event_incomplete(INTEGER) RETURNS SETOF conf
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with missing tag
-CREATE OR REPLACE FUNCTION conflict_event_missing_tag(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_missing_tag(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -126,14 +126,14 @@ CREATE OR REPLACE FUNCTION conflict_event_missing_tag(INTEGER) RETURNS SETOF con
         FROM event 
              INNER JOIN event_state USING (event_state_id)
         WHERE conference_id = cur_conference_id AND
-              event_state.tag = ''accepted'' AND
+              event_state.tag = 'accepted' AND
               event.tag IS NULL
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with inconsistent tag
 CREATE OR REPLACE FUNCTION conflict_event_inconsistent_tag(INTEGER) RETURNS SETOF conflict_event AS $$
@@ -157,7 +157,7 @@ CREATE OR REPLACE FUNCTION conflict_event_inconsistent_tag(INTEGER) RETURNS SETO
 $$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with no paper but the f_paper flag set
-CREATE OR REPLACE FUNCTION conflict_event_no_paper(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_paper(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -169,25 +169,25 @@ CREATE OR REPLACE FUNCTION conflict_event_no_paper(INTEGER) RETURNS SETOF confli
              INNER JOIN event_state_progress ON (
                 event.event_state_progress_id = event_state_progress.event_state_progress_id AND
                 event.event_state_id = event_state_progress.event_state_id AND
-                event_state_progress.tag = ''confirmed''
+                event_state_progress.tag = 'confirmed'
               )
        WHERE conference_id = cur_conference_id AND
-             event_state.tag = ''accepted'' AND
-             f_paper = ''t'' AND
+             event_state.tag = 'accepted' AND
+             f_paper = 't' AND
              NOT EXISTS (SELECT 1 FROM event_attachment
                                        INNER JOIN attachment_type USING (attachment_type_id)
                                  WHERE event_attachment.event_id = event.event_id AND
-                                       attachment_type.tag = ''paper'' AND
-                                       event_attachment.f_public = ''t'')
+                                       attachment_type.tag = 'paper' AND
+                                       event_attachment.f_public = 't')
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with no paper but the f_slides flag set
-CREATE OR REPLACE FUNCTION conflict_event_no_slides(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_slides(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -199,25 +199,25 @@ CREATE OR REPLACE FUNCTION conflict_event_no_slides(INTEGER) RETURNS SETOF confl
              INNER JOIN event_state_progress ON (
                 event.event_state_progress_id = event_state_progress.event_state_progress_id AND
                 event.event_state_id = event_state_progress.event_state_id AND
-                event_state_progress.tag = ''confirmed''
+                event_state_progress.tag = 'confirmed'
               )
        WHERE conference_id = cur_conference_id AND
-             event_state.tag = ''accepted'' AND
-             f_slides = ''t'' AND
+             event_state.tag = 'accepted' AND
+             f_slides = 't' AND
              NOT EXISTS (SELECT 1 FROM event_attachment
                                        INNER JOIN attachment_type USING (attachment_type_id)
                                  WHERE event_id = event.event_id AND
-                                       attachment_type.tag = ''slides'' AND
-                                       event_attachment.f_public = ''t'')
+                                       attachment_type.tag = 'slides' AND
+                                       event_attachment.f_public = 't')
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all events with inconsistent public links
-CREATE OR REPLACE FUNCTION conflict_event_inconsistent_public_link(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_inconsistent_public_link(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -228,16 +228,16 @@ CREATE OR REPLACE FUNCTION conflict_event_inconsistent_public_link(INTEGER) RETU
        WHERE conference_id = cur_conference_id AND
              EXISTS (SELECT 1 FROM event_link
                              WHERE event_id = event.event_id AND
-                                   url NOT SIMILAR TO ''[a-z]+:%'')
+                                   url NOT SIMILAR TO '[a-z]+:%')
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with no language set
-CREATE OR REPLACE FUNCTION conflict_event_no_language(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_language(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -247,17 +247,17 @@ CREATE OR REPLACE FUNCTION conflict_event_no_language(INTEGER) RETURNS SETOF con
         FROM event
              INNER JOIN event_state USING (event_state_id)
        WHERE conference_id = cur_conference_id AND
-             event_state.tag = ''accepted'' AND
+             event_state.tag = 'accepted' AND
              language_id IS NULL
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all accepted events with no conference track set
-CREATE OR REPLACE FUNCTION conflict_event_no_track(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_track(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -267,17 +267,17 @@ CREATE OR REPLACE FUNCTION conflict_event_no_track(INTEGER) RETURNS SETOF confli
         FROM event
              INNER JOIN event_state USING (event_state_id)
        WHERE conference_id = cur_conference_id AND
-             event_state.tag = ''accepted'' AND
+             event_state.tag = 'accepted' AND
              conference_track_id IS NULL
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
 -- returns all events with a language not in conference_language 
-CREATE OR REPLACE FUNCTION conflict_event_conference_language(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_conference_language(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -296,9 +296,9 @@ CREATE OR REPLACE FUNCTION conflict_event_conference_language(INTEGER) RETURNS S
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
-CREATE OR REPLACE FUNCTION conflict_event_no_abstract(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_abstract(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -313,9 +313,9 @@ CREATE OR REPLACE FUNCTION conflict_event_no_abstract(INTEGER) RETURNS SETOF con
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
-CREATE OR REPLACE FUNCTION conflict_event_no_description(INTEGER) RETURNS SETOF conflict_event AS '
+CREATE OR REPLACE FUNCTION conflict_event_no_description(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
     cur_event conflict_event%ROWTYPE;
@@ -330,5 +330,41 @@ CREATE OR REPLACE FUNCTION conflict_event_no_description(INTEGER) RETURNS SETOF 
     END LOOP;
     RETURN;
   END;
-' LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION conflict_event_abstract_length(INTEGER) RETURNS SETOF conflict_event AS $$
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_conference RECORD;
+    cur_event RECORD;
+  BEGIN
+    SELECT INTO cur_conference abstract_length, description_length FROM conference WHERE conference_id = cur_conference_id;
+    FOR cur_event IN
+      SELECT event_id
+        FROM event
+       WHERE conference_id = cur_conference_id AND 
+             length(abstract) < cur_conference.abstract_length
+    LOOP
+      RETURN NEXT cur_event;
+    END LOOP;
+  END;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION conflict_event_description_length(INTEGER) RETURNS SETOF conflict_event AS $$
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_conference RECORD;
+    cur_event RECORD;
+  BEGIN
+    SELECT INTO cur_conference abstract_length, description_length FROM conference WHERE conference_id = cur_conference_id;
+    FOR cur_event IN
+      SELECT event_id
+        FROM event
+       WHERE conference_id = cur_conference_id AND 
+             length(description) < cur_conference.description_length
+    LOOP
+      RETURN NEXT cur_event;
+    END LOOP;
+  END;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
 
