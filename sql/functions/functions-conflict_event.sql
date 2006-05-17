bb17d982e9cbacfbe2ca9ca6b471da11cfed1fc9
Sven Klemm <sven@c3d2.de>
2006-05-17 11:40:49 +0000

    r4985@pulsar:  sven | 2006-05-17 13:40:41 +0200  added footer row to conflict admin page  implemented #169: conflict for acceptend and confirmed events without timeslot and  unconfirmed event without timeslot

--- sql/functions/functions-conflict_event.sql
+++ sql/functions/functions-conflict_event.sql
@@ -313,8 +313,7 @@ CREATE OR REPLACE FUNCTION conflict_event_no_abstract(INTEGER) RETURNS SETOF con
     END LOOP;
     RETURN;
   END;
-$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
-
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT; 
 CREATE OR REPLACE FUNCTION conflict_event_no_description(INTEGER) RETURNS SETOF conflict_event AS $$
   DECLARE
     cur_conference_id ALIAS FOR $1;
@@ -362,6 +361,52 @@ CREATE OR REPLACE FUNCTION conflict_event_description_length(INTEGER) RETURNS SE
         FROM event
        WHERE conference_id = cur_conference_id AND 
              length(description) > cur_conference.description_length
+    LOOP
+      RETURN NEXT cur_event;
+    END LOOP;
+  END;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION conflict_event_accepted_without_timeslot(INTEGER) RETURNS SETOF conflict_event AS $$
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_event RECORD;
+  BEGIN
+    FOR cur_event IN
+      SELECT event_id
+        FROM event
+             INNER JOIN event_state ON (
+               event_state.event_state_id = event.event_state_id AND
+               event_state.tag = 'accepted' )
+             INNER JOIN event_state_progress ON (
+               event_state_progress.event_state_progress_id = event.event_state_progress_id AND
+               event_state_progress.tag = 'confirmed' )
+       WHERE event.conference_id = cur_conference_id AND
+             ( event.start_time IS NULL OR 
+               event.room_id IS NULL )
+    LOOP
+      RETURN NEXT cur_event;
+    END LOOP;
+  END;
+$$ LANGUAGE 'plpgsql' RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION conflict_event_unconfirmed_with_timeslot(INTEGER) RETURNS SETOF conflict_event AS $$
+  DECLARE
+    cur_conference_id ALIAS FOR $1;
+    cur_event RECORD;
+  BEGIN
+    FOR cur_event IN
+      SELECT event_id
+        FROM event
+             INNER JOIN event_state ON (
+               event_state.event_state_id = event.event_state_id AND
+               event_state.tag = 'accepted' )
+             INNER JOIN event_state_progress ON (
+               event_state_progress.event_state_progress_id = event.event_state_progress_id AND
+               event_state_progress.tag != 'confirmed' )
+       WHERE event.conference_id = cur_conference_id AND
+             event.start_time IS NOT NULL AND
+             event.room_id IS NOT NULL 
     LOOP
       RETURN NEXT cur_event;
     END LOOP;
