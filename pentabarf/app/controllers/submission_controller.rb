0062604f1de679d70b580456a56596691e624c63
Sven Klemm <sven@c3d2.de>
2006-06-14 13:37:00 +0000

    r5067@pulsar:  sven | 2006-06-14 15:36:15 +0200  added list of conferences to submission index page of no conference is selected

--- pentabarf/app/controllers/submission_controller.rb
+++ pentabarf/app/controllers/submission_controller.rb
@@ -7,6 +7,7 @@ class SubmissionController < ApplicationController
 
   def index
     @content_title = "#{@conference.nil? ? '' : @conference.title + ' ' }Paper Submission"
+    @conferences = Momomoto::Conference.select(:f_submission_enabled=>'t') if @conference.nil?
     Momomoto::Base.ui_language_id = 120;
   end
 
