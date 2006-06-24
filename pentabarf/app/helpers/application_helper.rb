e610f0cda89548615b2e9f3cd3e6210ff5abe496
Sven Klemm <sven@c3d2.de>
2006-06-24 17:14:33 +0000

    fixing versions

--- pentabarf/app/helpers/application_helper.rb
+++ pentabarf/app/helpers/application_helper.rb
@@ -240,7 +240,7 @@ module ApplicationHelper
 
   # returns the current version of pentabarf
   def get_version()
-    "0.2.4"
+    "0.2.5"
   end
 
   # tries to read the current revision of pentabarf from a file named revision.txt otherwise returns 2342
