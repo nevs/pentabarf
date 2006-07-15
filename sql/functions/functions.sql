aed2b6b1ad9fb927154e05c04a19f71b4d42b052
Sven Klemm <sven@c3d2.de>
2006-07-15 15:52:52 +0000

    r5096@pulsar:  sven | 2006-07-15 17:52:43 +0200  added procedure for removing events

--- sql/functions/functions.sql
+++ sql/functions/functions.sql
@@ -271,3 +271,35 @@ CREATE OR REPLACE FUNCTION remove_attendee(person_id INTEGER, event_id INTEGER) 
   SELECT $1;
 $$ LANGUAGE SQL RETURNS NULL ON NULL INPUT;
 
+CREATE OR REPLACE FUNCTION remove_event( event_id INTEGER ) RETURNS INTEGER AS $$
+  DECLARE
+  BEGIN
+    DELETE FROM event_transaction WHERE event_transaction.event_id = event_id;
+    DELETE FROM event_related WHERE event_related.event_id1 = event_id;
+    DELETE FROM event_image WHERE event_image.event_id = event_id;
+    DELETE FROM event_person WHERE event_person.event_id = event_id;
+    DELETE FROM event_link WHERE event_link.event_id = event_id;
+    DELETE FROM event_link_internal WHERE event_link_internal.event_id = event_id;
+    DELETE FROM event_attachment WHERE event_attachment.event_id = event_id;
+    DELETE FROM event_rating WHERE event_rating.event_id = event_id;
+    DELETE FROM event_rating_public WHERE event_rating_public.event_id = event_id; 
+    DELETE FROM event WHERE event.event_id = event_id;
+    RETURN event_id;
+  END;
+$$ LANGUAGE PLPGSQL RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION remove_conference( conference_id INTEGER ) RETURNS INTEGER AS $$
+  DECLARE
+    cur_event RECORD;
+  BEGIN
+    DELETE FROM conference_transaction WHERE conference_transaction.conference_id = conference_id;
+    FOR cur_event IN
+      SELECT event_id FROM event WHERE event.conference_id = conference_id
+    LOOP
+      PERFORM remove_event( cur_event.event_id );
+    END LOOP;
+    DELETE FROM conference WHERE conference.conference_id = conference_id;
+    RETURN conference_id;
+  END;
+$$ LANGUAGE PLPGSQL RETURNS NULL ON NULL INPUT;
+
