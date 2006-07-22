ec5604cf107ceaa3559708f52f7dc72fa34296aa
Sven Klemm <sven@c3d2.de>
2006-07-22 20:29:26 +0000

    r5111@pulsar:  sven | 2006-07-22 22:29:22 +0200  added view for mail addresses of all speakes and moderators

--- sql/views.sql
+++ sql/views.sql
@@ -917,6 +917,25 @@ CREATE OR REPLACE VIEW view_report_feedback AS
    GROUP BY event_id, conference_id, title, subtitle
 ;
 
+-- returns all speakers of all conferences
+CREATE OR REPLACE VIEW view_mail_all_speaker AS
+  SELECT DISTINCT ON ( person.person_id )
+         person.person_id,
+         view_person.name,
+         view_person.email_contact
+    FROM event_person
+         INNER JOIN event_role ON (
+             event_role.event_role_id = event_person.event_role_id AND
+             event_role.tag IN ('speaker', 'moderator') )
+         INNER JOIN person ON (
+             person.person_id = event_person.person_id AND 
+             person.email_contact IS NOT NULL AND
+             person.f_spam = true )
+         INNER JOIN view_person ON (
+             view_person.person_id = event_person.person_id AND
+             view_person.email_contact IS NOT NULL )
+;
+
 -- returns all accepted speakers of accepted and confirmed events
 CREATE OR REPLACE VIEW view_mail_accepted_speaker AS
   SELECT view_person.person_id,
