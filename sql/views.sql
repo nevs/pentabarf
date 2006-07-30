72dccc82ffca8753255610471435d8aa9142b573
Sven Klemm <sven@c3d2.de>
2006-07-30 04:17:51 +0000

    r5159@pulsar:  sven | 2006-07-30 06:17:36 +0200  added view view_event_search_persons  find event shows speakers and moderators now

--- sql/views.sql
+++ sql/views.sql
@@ -1034,4 +1034,17 @@ CREATE OR REPLACE VIEW view_conference_persons AS
                view_event_role.tag IN ('speaker', 'moderator', 'coordinator') )
 ORDER BY lower( view_person.name ), view_event_role.tag; 
          
+CREATE OR REPLACE VIEW view_event_search_persons AS
+  SELECT 
+         view_person.person_id,
+         view_person.name,
+         event_person.event_id,
+         event_role.tag
+    FROM event_person 
+         INNER JOIN event_role ON (
+             event_role.event_role_id = event_person.event_role_id AND
+             event_role.tag IN ('speaker','moderator') )
+         INNER JOIN view_person ON (
+             view_person.person_id = event_person.person_id )
+ORDER BY lower( view_person.name );
 
