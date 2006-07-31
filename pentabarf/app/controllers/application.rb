3df3d70b7faccdfd37ddbd97e91e84b8e4164239
Sven Klemm <sven@c3d2.de>
2006-07-31 20:54:04 +0000

    r5181@pulsar:  sven | 2006-07-31 22:54:00 +0200  send errors via jabber

--- pentabarf/app/controllers/application.rb
+++ pentabarf/app/controllers/application.rb
@@ -2,4 +2,22 @@
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
 
+  def log_error( e )
+    super( e )
+
+    message = ''
+    message += "Time: #{Time.now.to_s}\n"
+    message += "UA: #{request.env['HTTP_USER_AGENT']}\n"
+    message += "IP: #{request.remote_ip}\n"
+    message += "URL: https://#{request.host + request.request_uri}\n"
+    message += "Exception: #{e.message}\n"
+    message += "Exception Class: #{e.class}\n"
+    message += "Backtrace:\n"
+    message += clean_backtrace(e).join("\n")
+    message += "\n"
+    message += "Request: #{params.inspect}\n"
+
+    JabberLogger.log( message )
+  end
+
 end
