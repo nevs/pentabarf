5da36fe7bd78b8609ec1a554aff632b8d54b4851
Sven Klemm <sven@c3d2.de>
2006-08-05 12:14:36 +0000

    r5094@pinealgland:  sven | 2006-08-05 14:00:40 +0200  added script to create all tables

---
+++ sql/tables.sql
@@ -0,0 +1,43 @@
+-- script to create  all tables
+
+\i tables/language.sql
+\i tables/language_localized.sql
+
+\i tables/ui_message.sql
+\i tables/ui_message_localized.sql
+
+\i tables/country.sql
+\i tables/country_localized.sql
+
+\i tables/currency.sql
+\i tables/currency_localized.sql
+
+\i tables/im_type.sql
+\i tables/im_type_localized.sql
+
+\i tables/link_type.sql
+\i tables/link_type_localized.sql
+
+\i tables/mime_type.sql
+\i tables/mime_type_localized.sql
+
+\i tables/phone_type.sql
+\i tables/phone_type_localized.sql
+
+\i tables/transport.sql
+\i tables/transport_localized.sql
+
+\i tables/conference_phase.sql
+\i tables/conference_phase_localized.sql
+
+\i tables/conference.sql
+\i tables/conference_image.sql
+\i tables/conference_language.sql
+\i tables/conference_track.sql
+
+\i tables/room.sql
+
+\i tables/person.sql
+
+\i tables/event.sql
+
