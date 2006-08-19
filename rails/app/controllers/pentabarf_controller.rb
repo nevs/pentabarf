ec28dda46583d3a06cdd3d42436f8cd259850044
Sven Klemm <sven@c3d2.de>
2006-08-19 04:43:12 +0000

    r5435@pinealgland:  sven | 2006-08-19 06:40:21 +0200  adding model for conference_phase

--- rails/app/controllers/pentabarf_controller.rb
+++ rails/app/controllers/pentabarf_controller.rb
@@ -3,7 +3,22 @@ class PentabarfController < ApplicationController
   session(:off)
 
   def index
-    @conference = Conference.select_or_new(:conference_id => 1)
+    @current_conference = Conference.select_or_new(:conference_id => 1)
+  end
+
+  def person
+    @current_conference = Conference.select_or_new(:conference_id => 1)
+    @person = Person.select_or_new( :person_id => params[:id] )
+  end
+
+  def event
+    @current_conference = Conference.select_or_new(:conference_id => 1)
+    @event = Event.select_or_new( :event_id => params[:id] )
+  end
+
+  def conference
+    @current_conference = Conference.select_or_new(:conference_id => 1)
+    @conference = Conference.select_or_new( :conference_id => params[:id] )
   end
 
 end
