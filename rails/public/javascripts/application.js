6289931a04c7e75cae5bf6eb2238c9b617b08e40
Sven Klemm <sven@c3d2.de>
2006-08-22 21:56:25 +0000

    r5468@pinealgland:  sven | 2006-08-22 23:47:16 +0200  added jscalendar to the repository  r5469@pinealgland:  sven | 2006-08-22 23:51:24 +0200  replacing tabs with spaces  r5470@pinealgland:  sven | 2006-08-22 23:53:21 +0200  tab fix

--- rails/public/javascripts/application.js
+++ rails/public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function clear_tainting()
+{
+  window.onbeforeunload = null;
+}
+
+function enable_save_button()
+{
+  $('buttons').style.display = 'block';
+  window.onbeforeunload = unloadMessage;
+}
+
+function unloadMessage()
+{
+  return "If you leave this page, your changes will be lost.";
+}
+
