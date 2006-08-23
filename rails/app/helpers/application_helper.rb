4e5f3bfa9ac4eac4053d76a8c083e51960760cee
Sven Klemm <sven@c3d2.de>
2006-08-23 22:58:35 +0000

    r5325@pulsar:  sven | 2006-08-24 00:53:50 +0200  added models for country and currency  adjusted select_tag to work with simple arrays

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -9,13 +9,16 @@ module ApplicationHelper
     h("<[#{text}]>")
   end
 
-  def select_tag( name, collection, options, html_options )
-    xml = Builder::XmlMarkup.new(:indent=>1) 
+  def select_tag( name, collection, options = {}, html_options = {} )
+    xml = Builder::XmlMarkup.new(:indent=>1)
     html_options[:name] = name
     html_options[:id] = name
+    html_options[:tabindex] = 0 if not html_options[:tabindex]
     xml.select( html_options ) do
       collection.each do | element |
-        xml.option( element.send( options[:value] ), {:value=>element.send( options[:key] )})
+        value = options.has_key?(:value) ? element.send( options[:value] ) : element
+        key = options.has_key?(:key) ? element.send( options[:key] ) : element
+        xml.option( value, { :value=> key })
       end
     end
   end
