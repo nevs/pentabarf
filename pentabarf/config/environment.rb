f385621a2b74fc98ef862a565198b440aa707a7f
Sven Klemm <sven@c3d2.de>
2006-09-02 16:04:55 +0000

    r5416@pulsar:  sven | 2006-09-02 18:04:39 +0200  moved pentabarf-specific installation out of boot.rb into environment.rb to work under environments when the rails stuff is not yet loaded in boot.rb

--- pentabarf/config/environment.rb
+++ pentabarf/config/environment.rb
@@ -14,7 +14,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/app/services )
 
-  # Force all environments to use the same logger level 
+  # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
@@ -31,7 +31,7 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-  
+
   # Use Active Record's schema dumper instead of SQL when creating the test database
   # (enables use of different database adapters for development and test environments)
   # config.active_record.schema_format = :ruby
@@ -39,7 +39,7 @@ Rails::Initializer.run do |config|
   # See Rails::Configuration for more options
 end
 
-# Add new inflection rules using the following format 
+# Add new inflection rules using the following format
 # (all these examples are active by default):
 # Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
@@ -49,6 +49,75 @@ end
 # end
 
 # Include your application configuration below
+
+require 'time'
+require 'date'
+require 'momomoto/momomoto'
+require 'momomoto/login'
+require 'momomoto/tables'
+require 'momomoto/functions'
+require 'momomoto/views'
+require 'momomoto/views-conflict'
+require 'momomoto/views-procedures'
+require 'builder'
+require 'builder_escape_attributes'
+
+Momomoto::Base.connect(YAML::load_file("#{RAILS_ROOT}/config/database.yml")[RAILS_ENV])
+
+# we want localization in our rhtml renderer
+module ActionView
+  class Base
+    private
+
+      def localize( template )
+        # lets do some localization
+        tags = template.to_s.scan(/<\[[a-z:_]+\]>/)
+        tags.collect do | tag |
+          tag.delete!("<[]>")
+        end
+        if tags.length > 0
+          Momomoto::View_ui_message.find({:language_id => Momomoto::Base.ui_language_id, :tag => tags}).each do | msg |
+            next if msg.name.match(/[{}<>]/)
+            template.gsub!( "<[" + msg.tag + "]>", h(msg.name) )
+          end
+        end
+        template
+      end
+
+      # overwrite render function for rails 0.14.x
+      alias compile_template_default compile_template
+      alias compile_template_default? compile_template?
+
+      def compile_template( extension, template, file_name, local_assigns)
+        compile_template_default( extension, localize( template ), file_name, local_assigns )
+      end
+
+      def compile_template?( template, file_name, local_assigns )
+        compile_template_default?( template, nil, local_assigns )
+      end
+
+  end
+
+  module Helpers
+    module FormTagHelper
+      alias form_tag_default form_tag
+      undef start_form_tag
+      def form_tag(url_for_options = {}, options = {}, *parameters_for_url)
+        form_tag_default(url_for_options, options, *parameters_for_url) + hidden_field_tag('edittoken', @edittoken)
+      end
+      alias start_form_tag form_tag
+    end
+  end
+
+end
+
+module Momomoto
+  class Base
+    def self.log_error( text )
+      ApplicationController.jabber_message( text )
+    end
+  end
+end
 
 # read mail configuration if available
 if File.exists?("#{RAILS_ROOT}/config/mail.yml")
