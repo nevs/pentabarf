37753cac8a68e0aaad1e1eeffc0fee484d2923f9
Sven Klemm <sven@c3d2.de>
2006-09-16 18:51:57 +0000

    r5485@pulsar:  sven | 2006-09-16 20:51:52 +0200  minor changes to activate_logging

--- sql/functions/logging/activate_logging.sql
+++ sql/functions/logging/activate_logging.sql
@@ -1,6 +1,6 @@
 
--- this function creates a log function as trigger for every table found in the logging schema
-
+-- this function creates a log function for every table found in the
+-- logging schema and registers the function as trigger
 CREATE OR REPLACE FUNCTION logging.activate_logging() RETURNS VOID AS $$
   DECLARE
     logtable    RECORD;
@@ -14,8 +14,8 @@ CREATE OR REPLACE FUNCTION logging.activate_logging() RETURNS VOID AS $$
       SELECT table_name FROM information_schema.tables WHERE table_schema = 'logging'
     LOOP
       tablename = logtable.table_name;
-      RAISE NOTICE 'Creating log function for %', tablename;
-      -- creating trigger function
+      RAISE NOTICE 'Creating log function for table %', tablename;
+      -- (re)creating trigger function
       procname = tablename || '_log_function';
       fundef = $f$CREATE OR REPLACE FUNCTION logging.$f$ || quote_ident( procname ) || $f$() RETURNS TRIGGER AS $i$
         BEGIN
@@ -32,12 +32,8 @@ CREATE OR REPLACE FUNCTION logging.activate_logging() RETURNS VOID AS $$
       EXECUTE fundef;
 
       trigname = tablename || '_log_trigger';
-      PERFORM * FROM information_schema.triggers
-        WHERE trigger_name = trigname AND
-              event_object_schema = 'public' AND
-              event_object_table = tablename;
-      -- activating trigger function
-      IF ( NOT FOUND ) THEN
+      -- enable function as trigger if it is not yet enables
+      IF ( NOT EXISTS( SELECT 1 FROM information_schema.triggers WHERE trigger_name = trigname AND event_object_schema = 'public' AND event_object_table = tablename ) ) THEN
         RAISE NOTICE 'Creating trigger for table %', tablename;
         trigdef = $t$CREATE TRIGGER $t$ || quote_ident( trigname ) || $t$ AFTER INSERT OR UPDATE OR DELETE ON public.$t$ || quote_ident( tablename ) || $t$ FOR EACH ROW EXECUTE PROCEDURE logging.$t$ || quote_ident( procname ) || $t$();$t$;
         EXECUTE trigdef;
