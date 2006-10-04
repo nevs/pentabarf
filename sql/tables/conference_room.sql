aecfd926f904d16b4710adbc66b6b31a90ad02d8
Sven Klemm <sven@c3d2.de>
2006-10-04 13:35:24 +0000

    r5553@pulsar:  sven | 2006-10-04 15:35:21 +0200  really renaming room to conference_room

--- sql/tables/conference_room.sql
+++ sql/tables/conference_room.sql
@@ -1,18 +1,18 @@
 
-CREATE TABLE master.room(
+CREATE TABLE master.conference_room(
   conference_room_id INTEGER NOT NULL,
   conference_room TEXT NOT NULL,
   conference_id INTEGER NOT NULL,
   public BOOL NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE room(
-  PRIMARY KEY(conference_id, room),
+CREATE TABLE conference_room(
+  PRIMARY KEY(conference_id, conference_room),
   FOREIGN KEY(conference_id) REFERENCES conference(conference_id) ON UPDATE CASCADE
-) INHERITS(master.room);
+) INHERITS(master.conference_room);
 
 CREATE SEQUENCE conference_room_id_sequence;
 ALTER TABLE conference_room ALTER COLUMN conference_room_id SET DEFAULT nextval('conference_room_id_sequence');
 
-CREATE TABLE logging.room() INHERITS(master.logging, master.room);
+CREATE TABLE logging.conference_room() INHERITS(master.logging, master.conference_room);
 
