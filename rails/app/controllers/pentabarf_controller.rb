206aa41fbd47b813a12547ddf34cff466b4c9910
Sven Klemm <sven@c3d2.de>
2007-03-25 01:36:14 +0000

    r6272@pulsar:  sven | 2007-03-25 03:36:11 +0200  added pentabarf controller stub to trunk

---
+++ rails/app/controllers/pentabarf_controller.rb
@@ -0,0 +1,2 @@
+class PentabarfController < ApplicationController
+end
---
+++ rails/test/functional/pentabarf_controller_test.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'pentabarf_controller'
+
+# Re-raise errors caught by the controller.
+class PentabarfController; def rescue_action(e) raise e end; end
+
+class PentabarfControllerTest < Test::Unit::TestCase
+  def setup
+    @controller = PentabarfController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
