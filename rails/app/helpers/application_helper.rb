2cb7d0afc5adcbe88efb9614a186eb6edbeeba80
Sven Klemm <sven@c3d2.de>
2007-03-25 14:07:55 +0000

    r6280@pulsar:  sven | 2007-03-25 16:07:51 +0200  added pentabarf_version and pentabarf_revision helper functions to trunk  minor adjustments to routes  ignore everything in the sessions directory

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def pentabarf_version
+    "0.3.0"
+  end
+
+  # tries to read the current revision of pentabarf from subversion meta data
+  def pentabarf_revision
+    if not self.class.class_variables.member?( '@@revision' )
+      revision_file = File.join( RAILS_ROOT, '..', '.svn', 'entries' )
+      begin
+        content = File.open( revision_file, 'r').read
+        if content[0..1] == '<?'    # new svn xml file
+          d = REXML::Document.new( content )
+          rev = d.elements['//entry[@name=""]/@revision'].to_s
+        else                        # simple txt file
+          rev = content.split("\n")[3]
+        end
+        rev = rev.to_i
+      rescue
+        rev = 0
+      end
+      @@revision = rev == 0 ? 2342 : rev
+    end
+    @@revision.to_s
+  end
+
 end
