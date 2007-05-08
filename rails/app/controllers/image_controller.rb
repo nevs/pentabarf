cd742befcc792c66e91e9fe107143e86f10fa00d
Sven Klemm <sven@c3d2.de>
2007-05-08 17:29:31 +0000

    added static image showing to image controller

--- rails/app/controllers/image_controller.rb
+++ rails/app/controllers/image_controller.rb
@@ -1,8 +1,11 @@
 class ImageController < ApplicationController
 
   [:conference, :event, :person].each do | o |
-    define_method( o ) do
-      render(:text=>'')
+    define_method( action ) do
+      img = File.join( RAILS_ROOT, "public/images/icon-#{action}-128x128.png" ).read
+
+      response.headers['Content-Type'] = 'image/png'
+      render( :text => img )
     end
   end
 
