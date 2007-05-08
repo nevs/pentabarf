a01f281af4ed0a8eb110df622a3a054efb353f3f
Sven Klemm <sven@c3d2.de>
2007-05-08 17:32:13 +0000

    bugfix for static image rendering

--- rails/app/controllers/image_controller.rb
+++ rails/app/controllers/image_controller.rb
@@ -1,11 +1,11 @@
 class ImageController < ApplicationController
 
-  [:conference, :event, :person].each do | o |
+  [:conference, :event, :person].each do | action |
     define_method( action ) do
-      img = File.join( RAILS_ROOT, "public/images/icon-#{action}-128x128.png" ).read
+      img = File.join( RAILS_ROOT, "public/images/icon-#{action}-128x128.png" )
 
       response.headers['Content-Type'] = 'image/png'
-      render( :text => img )
+      render( :text => File.open( img ).read )
     end
   end
 
