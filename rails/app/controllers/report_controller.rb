2a3fb313fab7c1e902e4385d16086cdc2130a907
Sven Klemm <sven@c3d2.de>
2007-06-19 22:56:01 +0000

    adding expenses report to report controller

--- rails/app/controllers/report_controller.rb
+++ rails/app/controllers/report_controller.rb
@@ -1,2 +1,30 @@
 class ReportController < ApplicationController
+  before_filter :init
+
+  REPORTS = [:expenses]
+
+  def index
+  end
+  
+  def expenses
+    @report_class = View_report_expenses
+  end
+
+  REPORTS.each do | report |
+    next if true
+    next if ReportController.instance_methods(false).member?(:expenses)
+    define_method( report ) do 
+      @report_class = Object.const_get("View_report_#{report}")
+      render(:action => 'report')
+    end
+  end
+
+  protected
+
+  def init
+    # XXX FIXME remove hardcoded conference and language
+    @current_conference = Conference.select_single(:conference_id => 1)
+    @current_language_id = 120
+  end
+
 end
---
+++ rails/app/models/view_report_expenses.rb
@@ -0,0 +1,4 @@
+class View_report_expenses < Momomoto::Table
+  Reportfields = [:name,:fee,:fee_currency_name,:travel_cost,:travel_currency_name,:accommodation_cost,:accommodation_currency_name]
+end
+
