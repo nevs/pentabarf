61cb1a72debb306400fedce8b4c63769b01a000d
Sven Klemm <sven@c3d2.de>
2007-06-19 16:25:38 +0000

    make image controller deliver images from database

--- rails/app/controllers/image_controller.rb
+++ rails/app/controllers/image_controller.rb
@@ -2,10 +2,18 @@ class ImageController < ApplicationController
 
   [:conference, :event, :person].each do | action |
     define_method( action ) do
-      img = File.join( RAILS_ROOT, "public/images/icon-#{action}-128x128.png" )
+      begin
+        img = const_get("View_#{action}_image").select({"#{action}_id".to_sym=>params[:id]})
+        data = img.image
+        mimetype = img.mime_type
+      rescue
+        img = File.join( RAILS_ROOT, "public/images/icon-#{action}-128x128.png" )
+        data = File.open( img ).read
+        mimetype = 'image/png'
+      end
 
-      response.headers['Content-Type'] = 'image/png'
-      render( :text => File.open( img ).read )
+      response.headers['Content-Type'] = mimetype
+      render( :text => data )
     end
   end
 
