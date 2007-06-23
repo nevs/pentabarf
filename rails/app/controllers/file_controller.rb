13f6d2ab3078bcdbe12ab9b47dd98255c8329234
Sven Klemm <sven@c3d2.de>
2007-06-23 21:31:03 +0000

    adding file controller

---
+++ rails/app/controllers/file_controller.rb
@@ -0,0 +1,27 @@
+class FileController < ApplicationController
+  before_filter :authorize
+  before_filter :check_permission
+
+  def initialize
+    @current_language_id = 120
+  end
+
+  def event_attachment
+    data = View_event_attachment.select_single({:event_attachment_id => params[:id], :language_id => @current_language_id})
+    file = Event_attachment.select_single({:event_attachment_id => params[:id]})
+    response.headers['Content-Disposition'] = "attachment; filename=\"#{file.filename}\""
+    response.headers['Content-Type'] = data.mime_type
+    response.headers['Content-Length'] = data.filesize
+    response.headers['Last-Modified'] = file.last_modified
+    render_text(file.data)
+   rescue
+    render_text("File not found", 404)
+  end
+
+  protected
+
+  def check_permission
+    return POPE.permission?('pentabarf_login')
+  end
+
+end
