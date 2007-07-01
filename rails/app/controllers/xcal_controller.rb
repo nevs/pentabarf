815d3d0267d91062a3e38241bcd910509cfaa3e2
Sven Klemm <sven@c3d2.de>
2007-07-01 15:35:27 +0000

    adding xcal export for conference schedule

--- rails/app/controllers/xcal_controller.rb
+++ rails/app/controllers/xcal_controller.rb
@@ -1,2 +1,22 @@
 class XcalController < ApplicationController
+  before_filter :check_permission
+  after_filter :content_type
+
+  def conference
+    @conference = Conference.find({:conference_id => params[:id]})
+    @rooms = View_room.find({:conference_id=>@current_conference_id, :language_id=>@current_language_id})
+    @events = View_schedule_event.find({:conference_id => @conference.conference_id, :translated_id => @current_language_id})
+  end
+
+  protected
+
+  def check_permission
+    POPE.permission?('pentabarf_login')
+  end
+
+  def content_type
+    response.headers['Content-Type'] = 'application/calendar+xml'
+    response.headers['Content-Disposition'] = "attachment; filename=\"#{@conference.acronym}.xcs\""
+  end
+
 end
