a2bcd884f0817b338be2d65b8843384fc30183c6
Sven Klemm <sven@c3d2.de>
2007-07-07 17:19:36 +0000

    modified javascript to use more prototype functions added functions for advanced search

--- rails/public/javascripts/application.js
+++ rails/public/javascripts/application.js
@@ -4,17 +4,14 @@
 // replace pattern with substitute in attributes
 // recursively on all child nodes of element
 function attribute_replace( element, pattern, substitute, attributes ) {
-  var nodes = element.childNodes;
   if ( ! attributes ) attributes = ['id','name','onchange'];
+  var nodes = $(element).descendants();
   for( var i = 0; i < nodes.length; i++ ) {
     var node = nodes[i];
-    if ( node instanceof Element ) {
-      for( var k = 0; k < attributes.length; k++ ) {
-        if ( node.getAttribute( attributes[k] ) ) {
-          node.setAttribute( attributes[k], node.getAttribute( attributes[k] ).replace( pattern, substitute ) );
-        }
+    for( var k = 0; k < attributes.length; k++ ) {
+      if ( node.getAttribute( attributes[k] ) ) {
+        node.setAttribute( attributes[k], node.getAttribute( attributes[k] ).replace( pattern, substitute ) );
       }
-      attribute_replace( node, pattern, substitute, attributes );
     }
   }
 }
@@ -28,7 +25,7 @@ var tabs = new Array();
 
 // find all tabs
 function find_tabs() {
-  var nodes = $('tabs').childNodes;
+  var nodes = $('tabs').descendants();
   var outer;
   for( var i = 0; i < nodes.length; i++){
     outer = nodes[i];
@@ -89,7 +86,7 @@ function table_add_row( table_name ) {
         field.value = arguments[i];
       }
       if ( field.onchange ) {
-        field.onchange();  
+        field.onchange();
       }
     }
   } else {
@@ -113,7 +110,7 @@ function table_find_fields( table_name ) {
   var cells = $(table_name + '_template').cells;
   var fields = new Array();
   for (var i = 0; i < cells.length; i++) {
-    var children = cells[i].childNodes;
+    var children = cells[i].descendants();
     for (var j = 0; j < children.length; j++) {
       var node = children[j].nodeName;
       if ( node == "INPUT" || node == "SELECT" ) {
@@ -155,10 +152,52 @@ function master_change( select, slave_column ) {
   }
 }
 
-function add_search_column( select ) {
-  var template_rows = $('search_template_table').rows;
-  for ( var i = 0; i < template_rows.length; i++ ) {
-    
+function init_search_list( list ) {
+  var target = $( list );
+  var template_row = $('search_template').getElementsBySelector('li')[0];
+  var selector = template_row.getElementsBySelector('select')[0];
+  var content = $('search_content').getElementsBySelector('li');
+  for (var i = 0; i < content.length; i++ ) {
+    var new_entry = document.createElement('option');
+    new_entry.text = content[i].title;
+    new_entry.value = content[i].className;
+    selector.add( new_entry, null );
   }
 }
+
+var search_row_counter = 0;
+function search_row_add( list ) {
+  var template_row = $('search_template').getElementsBySelector('li')[0];
+  var new_row = template_row.cloneNode(true);
+  var row_id = search_row_counter;
+  search_row_counter++;
+
+  attribute_replace( new_row, /\[row_id\]/g, '[' + row_id + ']' );
+  new_row.className = row_id;
+  $('advanced_search_list').appendChild( new_row );
+  $( new_row ).getElementsBySelector('select')[0].onchange();
+}
+
+function search_row_remove( list ) {
+  $(list).up().remove();
+}
+
+function search_row_change( list ) {
+  var row = $(list).up();
+  var row_id = row.className;
+  var value = row.getElementsBySelector('select')[0].getValue();
+  var target = row.getElementsBySelector('span.dynamic')[0];
+
+  // remove old content
+  var old_stuff = target.immediateDescendants();
+  for ( var i = 0; i < old_stuff.length; i++ ) { Element.remove( old_stuff[i] ); }
+
+  // add new content
+  var content = $('search_content').getElementsBySelector('li.' + value)[0].immediateDescendants();
+  for ( var i = 0; i < content.length; i++ ) {
+    target.appendChild( content[i].cloneNode( true ) );
+  }
+  attribute_replace( target, /\[row_id\]/, '[' + row_id + ']' );
+}
+
 
