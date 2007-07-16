a5f039bf2403f96223cafa049ac3ee4a684b3546
Sven Klemm <sven@c3d2.de>
2007-07-16 06:02:55 +0000

    fix after-filter of xcal controller to only set header if @filename is set

--- rails/app/controllers/xcal_controller.rb
+++ rails/app/controllers/xcal_controller.rb
@@ -6,13 +6,16 @@ class XcalController < ApplicationController
     @conference = Conference.find({:conference_id => params[:id]})
     @rooms = View_room.find({:conference_id=>@current_conference_id, :language_id=>@current_language_id})
     @events = View_schedule_event.find({:conference_id => @conference.conference_id, :translated_id => @current_language_id})
+    @filename = "#{@conference.acronym}.xcs"
   end
 
   protected
 
   def content_type
-    response.headers['Content-Type'] = 'application/calendar+xml'
-    response.headers['Content-Disposition'] = "attachment; filename=\"#{@conference.acronym}.xcs\""
+    if @filename
+      response.headers['Content-Type'] = 'application/calendar+xml'
+      response.headers['Content-Disposition'] = "attachment; filename=\"#{@filename}\""
+    end
   end
 
 end
