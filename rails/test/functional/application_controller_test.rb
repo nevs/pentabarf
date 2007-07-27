802f42d4af7b352786a0a7d5e68a54df8ac62e82
Sven Klemm <sven@c3d2.de>
2007-07-27 09:50:57 +0000

    testing auth for all actions in all controllers

---
+++ rails/test/functional/application_controller_test.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'application'
+
+class ApplicationControllerTest < Test::Unit::TestCase
+  def setup
+    @controller = nil
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  Dir.new( File.join( File.dirname(__FILE__), '/../../app/controllers')).each do | entry |
+    if match = entry.match(/(([a-z_0-]+)_controller)\.rb/)
+      require( match[1] )
+      klass = const_get("#{match[2].capitalize}Controller")
+      klass.action_methods.each do | method |
+        define_method "test_auth_#{match[2]}_#{method}" do
+          @controller = klass.new
+          get method
+          assert_response 401
+        end
+
+      end
+    end
+  end
+
+end
