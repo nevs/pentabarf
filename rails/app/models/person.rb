ea8f7c6246686416622d2199df537fb280f6703d
Sven Klemm <sven@c3d2.de>
2007-07-30 21:38:09 +0000

    overwrite password setter for person to properly hash passwords

--- rails/app/models/person.rb
+++ rails/app/models/person.rb
@@ -1,2 +1,20 @@
 class Person < Momomoto::Table
+  class Row < Momomoto::Row
+
+    def password=( value )
+      srand()
+      salt = []
+      8.times do salt << rand(256) end
+
+      salt_bin, salt_hex = '', ''
+      for number in salt
+        salt_bin += sprintf('%c', number)
+        salt_hex += sprintf('%02x', number)
+      end
+
+      hash = Digest::MD5.hexdigest( salt_bin + value.to_s )
+      set_column( :password, salt_hex + hash )
+    end
+
+  end
 end
