21f9f159d858ae24fde5bc2e776bbb207cdc0cf9
Sven Klemm <sven@c3d2.de>
2007-07-31 23:53:07 +0000

    remove auth test from image controller test

--- rails/test/functional/image_controller_test.rb
+++ rails/test/functional/image_controller_test.rb
@@ -9,30 +9,21 @@ class ImageControllerTest < Test::Unit::TestCase
     @controller = ImageController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
-  end
-
-  # test auth for all public methods
-  ImageController.action_methods.each do | method |
-    define_method "test_auth_#{method}" do
-      get method
-      assert_response 401
-    end
+    # hijack auth
+    @controller.send( :instance_eval ) { class << self; self; end }.send(:define_method, :auth ) do true end
   end
 
   def test_person
-    @controller.send( :instance_eval ) { class << self; self; end }.send(:define_method, :auth ) do true end
     get :person, {:id => 1}
     assert_response :success
   end
 
   def test_event
-    @controller.send( :instance_eval ) { class << self; self; end }.send(:define_method, :auth ) do true end
     get :event, {:id => 1}
     assert_response :success
   end
 
   def test_conference
-    @controller.send( :instance_eval ) { class << self; self; end }.send(:define_method, :auth ) do true end
     get :conference, {:id => 1}
     assert_response :success
   end
