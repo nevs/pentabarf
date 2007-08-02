fc4c0d49bc9a9c9b652342aa77184dad90a78087
Sven Klemm <sven@c3d2.de>
2007-08-02 19:40:33 +0000

    adding index page to admin controller

--- rails/app/controllers/admin_controller.rb
+++ rails/app/controllers/admin_controller.rb
@@ -1,2 +1,17 @@
 class AdminController < ApplicationController
+
+  before_filter :init
+
+  def index
+  end
+
+  protected
+
+  def init
+    @current_conference = Conference.select_single(:conference_id => POPE.user.current_conference_id)
+    # FIXME: remove hardcoded language
+    @current_language_id = 120
+    @token = generate_token( url_for() )
+  end
+
 end
