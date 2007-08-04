c701495ca2e407bed5421f7de0d7aeccc51f6286
Sven Klemm <sven@c3d2.de>
2007-08-04 14:45:48 +0000

    adding conflict setup page to admin controller

--- rails/app/controllers/admin_controller.rb
+++ rails/app/controllers/admin_controller.rb
@@ -5,6 +5,22 @@ class AdminController < ApplicationController
   def index
   end
 
+  def conflict_setup
+    @phases = View_conference_phase.select(:language_id=>@current_language_id)
+    @conflicts = View_conflict.select({:language_id=>@current_language_id})
+    @level = View_conflict_level.select(:language_id=>@current_language_id).map{|l|[l.conflict_level_id,l.name]}
+    @phase_conflicts = Conference_phase_conflict.select
+  end
+
+  def save_conflict_setup
+    params[:conflict].each do | conflict_id, outer |
+      outer.each do | conference_phase_id, value |
+        write_row( Conference_phase_conflict, value, {:preset=>{:conflict_id=>conflict_id,:conference_phase_id=>conference_phase_id}})
+      end
+    end
+    redirect_to( :action => :conflict_setup )
+  end
+
   protected
 
   def init
