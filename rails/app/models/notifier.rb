b73bf0551d0d147053057ef48031c19523a137fd
Sven Klemm <sven@c3d2.de>
2007-08-23 19:54:13 +0000

    copying Notifier model from branches/0.2

---
+++ rails/app/models/notifier.rb
@@ -0,0 +1,39 @@
+class Notifier < ActionMailer::Base
+
+  if File.exists?( File.join( RAILS_ROOT, 'config', 'mail.yml' ) )
+    @@config = YAML.load_file( File.join( RAILS_ROOT, 'config', 'mail.yml' ) )
+    @@config.each do | key, value | @@config[key.to_sym] = value end
+  else
+    @@config = {}
+  end
+  @@config[:from] = 'noreply@pentabarf.org' unless @@config[:from]
+
+  def general( recipients, subject, body, from = nil )
+    @recipients = recipients
+    @subject = subject
+    @from = from || @@config[:from] 
+    @body['text'] = body
+    @cc = @@config[:always_cc]
+  end
+
+  def activate_account( name, email, link, from = nil )
+    @recipients = email
+    @subject = "Activate your pentabarf account"
+    @from = from || @@config[:from] 
+
+    # variables for usage in mail
+    @body["name"] = name
+    @body["link"] = link
+  end
+
+  def forgot_password( name, email, link, from = nil )
+    @recipients = email
+    @subject = "Pentabarf password reset"
+    @from = from || @@config[:from] 
+
+    # variables for usage in mail
+    @body["name"] = name
+    @body["link"] = link
+  end
+
+end
