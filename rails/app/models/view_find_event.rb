baa451bb4088c3ede07660ec5e10852670a3b5dd
Sven Klemm <sven@c3d2.de>
2007-08-31 11:41:58 +0000

    setting default_order for view_find_event and view_find_person

--- rails/app/models/view_find_event.rb
+++ rails/app/models/view_find_event.rb
@@ -1,3 +1,4 @@
 class View_find_event < Momomoto::Table
+  default_order( Momomoto.lower([:title,:subtitle]))
 end
 
--- rails/app/models/view_find_person.rb
+++ rails/app/models/view_find_person.rb
@@ -1,3 +1,4 @@
 class View_find_person < Momomoto::Table
+  default_order( Momomoto.lower(:name) )
 end
 
