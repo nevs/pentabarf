68a0f173787d1cdcc1dd941e72f9fe0f558c6786
Sven Klemm <sven@c3d2.de>
2007-09-03 09:12:58 +0000

    use named arguments in activate account stored procedure

--- sql/functions/account/activate_account.sql
+++ sql/functions/account/activate_account.sql
@@ -1,7 +1,6 @@
 
-CREATE OR REPLACE FUNCTION activate_account(char(64)) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION activate_account( activation_string char(64)) RETURNS INTEGER AS $$
   DECLARE
-    cur_activation_string ALIAS FOR $1;
     cur_person_id INTEGER;
     cur_person RECORD;
     cur_activation RECORD;
@@ -14,11 +13,11 @@ CREATE OR REPLACE FUNCTION activate_account(char(64)) RETURNS INTEGER AS $$
       DELETE FROM person WHERE person_id = cur_person.person_id AND NOT EXISTS ( SELECT 1 FROM person_transaction WHERE person_id = cur_person.person_id);
     END LOOP;
 
-    SELECT INTO cur_activation person_id, conference_id FROM account_activation WHERE activation_string = cur_activation_string;
+    SELECT INTO cur_activation person_id, conference_id FROM account_activation WHERE account_activation.cactivation_string = activation_string;
     IF FOUND THEN
       INSERT INTO person_role(person_id, role_id) VALUES (cur_activation.person_id, (SELECT role_id FROM role WHERE tag = 'submitter'));
       INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_activation.person_id, now(), cur_activation.person_id, 't');
-      DELETE FROM account_activation WHERE activation_string = cur_activation_string;
+      DELETE FROM account_activation WHERE account_activation.activation_string = activation_string;
     ELSE
       RAISE EXCEPTION 'invalid activation string.';
     END IF;
