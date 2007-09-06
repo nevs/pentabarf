856eebb01107a207d1bc4c3ba3a7d026fbedbd3b
Sven Klemm <sven@c3d2.de>
2007-09-06 09:53:40 +0000

    remove obsolete email checking from create_account

--- sql/functions/account/create_account.sql
+++ sql/functions/account/create_account.sql
@@ -13,17 +13,9 @@ CREATE OR REPLACE FUNCTION create_account( p_login_name VARCHAR(32), p_email_con
       RAISE EXCEPTION 'login name already in use.';
     END IF;
 
-    SELECT INTO p_person person_id, login_name FROM person WHERE email_contact = p_email_contact;
-    IF FOUND AND p_person.login_name IS NOT NULL THEN
-      RAISE EXCEPTION 'email address already in use.';
-    ELSIF FOUND AND p_person.login_name IS NULL THEN
-      new_person_id = p_person.person_id;
-      UPDATE person SET login_name = p_login_name, password = p_password WHERE person_id = new_person_id;
-    ELSE
-      SELECT INTO new_person_id nextval(pg_get_serial_sequence('person', 'person_id'));
-      SELECT INTO binary_salt gen_random_bytes(8);
-      INSERT INTO person(person_id, login_name, email_contact, password) VALUES (new_person_id, p_login_name, p_email_contact, encode(binary_salt, 'hex'::text) || md5(binary_salt||textsend(p_password)));
-    END IF;
+    SELECT INTO new_person_id nextval(pg_get_serial_sequence('person', 'person_id'));
+    SELECT INTO binary_salt gen_random_bytes(8);
+    INSERT INTO person(person_id, login_name, email_contact, password) VALUES (new_person_id, p_login_name, p_email_contact, encode(binary_salt, 'hex'::text) || md5(binary_salt||textsend(p_password)));
 
     INSERT INTO account_activation(person_id, activation_string, conference_id) VALUES (new_person_id, p_activation_string, CASE WHEN p_conference_id = 0 THEN NULL ELSE p_conference_id END);
     RETURN new_person_id;
