73aee6734d4d54f959b9228b2f50b5766ba0a173
Sven Klemm <sven@c3d2.de>
2007-09-06 00:20:48 +0000

    bugfix for create_account function (requires pgcrypto)

--- sql/functions/account/create_account.sql
+++ sql/functions/account/create_account.sql
@@ -1,18 +1,12 @@
 
 -- create new account to be activated
 -- the parameter are login_name, email_contact, password, activation_string
-CREATE OR REPLACE FUNCTION create_account( p_login_name VARCHAR(32), p_email_contact VARCHAR(64), p_password CHAR(48), p_activation_string CHAR(64), p_conference_id INTEGER) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION create_account( p_login_name VARCHAR(32), p_email_contact VARCHAR(64), p_password TEXT, p_activation_string CHAR(64), p_conference_id INTEGER) RETURNS INTEGER AS $$
   DECLARE
     new_person_id INTEGER;
+    binary_salt BYTEA;
     p_person RECORD;
   BEGIN
-    -- cleanup obsolete activation stuff
-    FOR p_person IN
-      SELECT person_id FROM person WHERE person_id IN (SELECT person_id FROM account_activation WHERE account_creation < (now() + '-1 day')::timestamptz)
-    LOOP
-      DELETE FROM account_activation WHERE person_id = p_person.person_id;
-      DELETE FROM person WHERE person_id = p_person.person_id AND NOT EXISTS ( SELECT 1 FROM person_transaction WHERE person_id = p_person.person_id);
-    END LOOP;
 
     SELECT INTO p_person person_id FROM person WHERE login_name = p_login_name;
     IF FOUND THEN
@@ -27,7 +21,8 @@ CREATE OR REPLACE FUNCTION create_account( p_login_name VARCHAR(32), p_email_con
       UPDATE person SET login_name = p_login_name, password = p_password WHERE person_id = new_person_id;
     ELSE
       SELECT INTO new_person_id nextval(pg_get_serial_sequence('person', 'person_id'));
-      INSERT INTO person(person_id, login_name, email_contact, password) VALUES (new_person_id, p_login_name, p_email_contact, p_password);
+      SELECT INTO binary_salt gen_random_bytes(8);
+      INSERT INTO person(person_id, login_name, email_contact, password) VALUES (new_person_id, p_login_name, p_email_contact, encode(binary_salt, 'hex'::text) || md5(binary_salt||textsend(p_password)));
     END IF;
 
     INSERT INTO account_activation(person_id, activation_string, conference_id) VALUES (new_person_id, p_activation_string, CASE WHEN p_conference_id = 0 THEN NULL ELSE p_conference_id END);
