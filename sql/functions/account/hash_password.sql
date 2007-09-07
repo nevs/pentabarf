d482f6b61aa986d43d05d8f84281d289f1749fdf
Sven Klemm <sven@c3d2.de>
2007-09-07 08:26:34 +0000

    adding hash_password stored procedure

---
+++ sql/functions/account/hash_password.sql
@@ -0,0 +1,10 @@
+
+CREATE OR REPLACE FUNCTION hash_password( password TEXT ) RETURNS TEXT AS $$
+  DECLARE
+    binary_salt BYTEA;
+  BEGIN
+    SELECT INTO binary_salt gen_random_bytes(8);
+    RETURN encode(binary_salt, 'hex'::text) || md5(binary_salt||textsend(password));
+  END;
+$$ LANGUAGE plpgsql;
+
