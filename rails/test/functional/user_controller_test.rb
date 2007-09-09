5c511a5c8dc8d10fce62b53b72ff5350b72eeda8
Sven Klemm <sven@c3d2.de>
2007-09-09 17:07:11 +0000

    added for account creation and activation in submission controller

--- rails/test/functional/user_controller_test.rb
+++ rails/test/functional/user_controller_test.rb
@@ -15,4 +15,23 @@ class UserControllerTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
+
+  def test_account_creation_and_activation
+    get :new_account
+    assert_response :success
+    token = @response.body.match( /<input [^>]+value="([a-f0-9]{40,40})"/ )
+    assert_not_nil token
+    token = token[1]
+    post :save_account, {:token=>token,:person=>{:login_name=>'chunky',:email_contact=>'chunky@bacon.com',:password=>'bacon'},:password=>'bacon'}
+    assert_response :redirect
+    assert_redirected_to( :action => :account_done)
+    chunky = Person.select_single(:login_name=>'chunky')
+    active = Account_activation.select_single(:person_id=>chunky.person_id)
+    get :activate_account, :id=>active.activation_string
+    assert_response :redirect
+    assert_redirected_to( :controller => 'submission' )
+    assert_equal( 1, Person_role.select(:person_id=>chunky.person_id).length )
+    chunky.delete
+  end
+
 end
