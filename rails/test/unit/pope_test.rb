0d8196f4a7365ce67a4d518ebe287bee8f61307d
Sven Klemm <sven@c3d2.de>
2007-09-09 20:18:03 +0000

    adding tests for pope

---
+++ rails/test/unit/pope_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PopeTest < Test::Unit::TestCase
+
+  def test_truth
+    assert( true )
+  end
+
+  def test_permissions
+    chunky = Person.new
+    chunky.public_name = 'Chunky Bacon'
+    assert_raise( Pope::PermissionError ) { chunky.write }
+    POPE.instance_eval{ @permissions = [:create_person] }
+    assert_nothing_raised{ chunky.write }
+    assert_raise( Pope::PermissionError ) { chunky.delete }
+    POPE.instance_eval{ @permissions = [:delete_person] }
+    assert_nothing_raised{ chunky.delete }
+  end
+
+end
