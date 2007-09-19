882cbf0d3e5265cd979a8f74979acbc2aa24fd7f
Sven Klemm <sven@c3d2.de>
2007-09-19 09:36:29 +0000

    cleaning maintenance dir in trunk

--- sql/maintenance/upgrade_0.2.11_to_0.2.12.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE conference_person ADD CONSTRAINT conference_person_unique UNIQUE(conference_id,person_id);
-
-ALTER TABLE conference ALTER timeslot_duration SET DEFAULT '1:00:00'::interval;
-
