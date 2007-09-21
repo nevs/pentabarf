6ff111d8b30351805c20a6040245ef03447e7eb6
Sven Klemm <sven@c3d2.de>
2007-09-21 12:13:31 +0000

    remove surrogate primary keys from event state progress drop unused columns from event

--- sql/maintenance/upgrade_0.3.0_to_0.3.1.sql
+++ sql/maintenance/upgrade_0.3.0_to_0.3.1.sql
@@ -51,8 +51,40 @@ ALTER TABLE event DROP COLUMN event_state_id;
 ALTER TABLE event_state_progress DROP COLUMN event_state_id CASCADE;
 ALTER TABLE event_state_progress ADD CONSTRAINT event_state_progress_event_state_fkey FOREIGN KEY( event_state ) REFERENCES event_state( event_state ) ON UPDATE CASCADE ON DELETE CASCADE;
 
--- putting domain information into database
+CREATE INDEX event_event_state_index ON event(event_state);
 
+-- event state progress migration
+
+ALTER TABLE event_state_progress ADD COLUMN event_state_progress TEXT;
+UPDATE event_state_progress SET event_state_progress = tag;
+ALTER TABLE event_state_progress DROP COLUMN tag;
+
+ALTER TABLE event_state_progress_localized ADD COLUMN event_state TEXT;
+ALTER TABLE event_state_progress_localized ADD COLUMN event_state_progress TEXT;
+ALTER TABLE event_state_progress_localized ALTER name TYPE TEXT;
+UPDATE event_state_progress_localized SET event_state = ( SELECT event_state FROM event_state_progress WHERE event_state_progress.event_state_progress_id = event_state_progress_localized.event_state_progress_id );
+UPDATE event_state_progress_localized SET event_state_progress = ( SELECT event_state_progress FROM event_state_progress WHERE event_state_progress.event_state_progress_id = event_state_progress_localized.event_state_progress_id );
+
+ALTER TABLE event ADD COLUMN event_state_progress TEXT;
+UPDATE event SET event_state_progress = ( SELECT event_state_progress FROM event_state_progress WHERE event_state_progress.event_state_progress_id = event.event_state_progress_id );
+
+ALTER TABLE event_state_progress DROP COLUMN event_state_progress_id CASCADE;
+ALTER TABLE event_state_progress ADD CONSTRAINT event_state_progress_pkey PRIMARY KEY (event_state, event_state_progress);
+
+ALTER TABLE event_state_progress_localized DROP COLUMN event_state_progress_id CASCADE;
+ALTER TABLE event_state_progress_localized ADD CONSTRAINT event_state_progress_localized_pkey PRIMARY KEY ( event_state, event_state_progress, language_id );
+ALTER TABLE event_state_progress_localized ADD CONSTRAINT event_state_progress_localized_event_state_fkey FOREIGN KEY ( event_state, event_state_progress ) REFERENCES event_state_progress( event_state, event_state_progress ) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE event DROP COLUMN event_state_progress_id CASCADE;
+ALTER TABLE event ADD CONSTRAINT event_event_state_fkey FOREIGN KEY ( event_state, event_state_progress ) REFERENCES event_state_progress( event_state,event_state_progress);
+ALTER TABLE event ALTER event_state SET NOT NULL;
+ALTER TABLE event ALTER event_state_progress SET NOT NULL;
+
+-- drop unused columns from event
+
+ALTER TABLE event DROP COLUMN f_conflict;
+ALTER TABLE event DROP COLUMN f_deleted;
+ALTER TABLE event DROP COLUMN f_unmoderated;
 
 COMMIT;
 
