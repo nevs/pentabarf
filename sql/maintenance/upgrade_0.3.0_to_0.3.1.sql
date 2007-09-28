7d0e02c034c66e52058355a5813ae6791589f15f
Sven Klemm <sven@c3d2.de>
2007-09-28 13:06:30 +0000

    minor bugfixes to upgrade script

--- sql/maintenance/upgrade_0.3.0_to_0.3.1.sql
+++ sql/maintenance/upgrade_0.3.0_to_0.3.1.sql
@@ -10,8 +10,7 @@ ALTER TABLE ui_message_localized ADD COLUMN ui_message TEXT;
 UPDATE ui_message_localized SET ui_message = ( SELECT ui_message FROM ui_message WHERE ui_message.ui_message_id = ui_message_localized.ui_message_id );
 
 ALTER TABLE ui_message_localized DROP CONSTRAINT ui_message_localized_pkey;
-ALTER TABLE ui_message_localized DROP CONSTRAINT ui_message_localized_ui_message_id_fkey;
-ALTER TABLE ui_message DROP CONSTRAINT ui_message_pkey;
+ALTER TABLE ui_message DROP CONSTRAINT ui_message_pkey CASCADE;
 ALTER TABLE ui_message DROP COLUMN ui_message_id;
 ALTER TABLE ui_message_localized DROP COLUMN ui_message_id ;
 
@@ -28,16 +27,13 @@ ALTER TABLE event_state DROP COLUMN tag CASCADE;
 ALTER TABLE event_state_localized ALTER name TYPE TEXT;
 ALTER TABLE event_state_localized ADD COLUMN event_state TEXT;
 UPDATE event_state_localized SET event_state = ( SELECT event_state FROM event_state WHERE event_state.event_state_id = event_state_localized.event_state_id );
-ALTER TABLE event_state_localized DROP CONSTRAINT event_state_localized_event_state_id_fkey;
-ALTER TABLE event_state_localized DROP CONSTRAINT event_state_localized_pkey;
+ALTER TABLE event_state_localized DROP CONSTRAINT event_state_localized_pkey CASCADE;
 
 ALTER TABLE event_state_progress ADD COLUMN event_state TEXT;
 UPDATE event_state_progress SET event_state = ( SELECT event_state FROM event_state WHERE event_state.event_state_id = event_state_progress.event_state_id );
-ALTER TABLE event_state_progress DROP CONSTRAINT event_state_progress_event_state_id_fkey;
 
 ALTER TABLE event ADD COLUMN event_state TEXT;
 UPDATE event SET event_state = ( SELECT event_state FROM event_state WHERE event_state.event_state_id = event.event_state_id );
-ALTER TABLE event DROP CONSTRAINT event_event_state_id_fkey;
 
 ALTER TABLE event_state DROP CONSTRAINT event_state_pkey CASCADE;
 ALTER TABLE event_state ADD CONSTRAINT event_state_pkey PRIMARY KEY( event_state );
