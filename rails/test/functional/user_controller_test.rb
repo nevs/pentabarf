e51c10bda82c4364aac575b27e73eb3ade98826a
Sven Klemm <sven@c3d2.de>
2007-10-03 16:59:18 +0000

    added test for resetting password

--- rails/test/functional/user_controller_test.rb
+++ rails/test/functional/user_controller_test.rb
@@ -14,9 +14,8 @@ class UserControllerTest < Test::Unit::TestCase
   def test_account_creation_and_activation
     get :new_account
     assert_response :success
-    token = @response.body.match( /<input [^>]+value="([a-f0-9]{40,40})"/ )
+    token = extract_token( @response.body )
     assert_not_nil token
-    token = token[1]
     post :save_account, {:token=>token,:person=>{:login_name=>'chunky',:email_contact=>'chunky@bacon.com',:password=>'bacon'},:password=>'bacon'}
     assert_response :redirect
     assert_redirected_to( :action => :account_done)
@@ -27,6 +26,32 @@ class UserControllerTest < Test::Unit::TestCase
     assert_redirected_to( :controller => 'submission' )
     assert_equal( 1, Person_role.select(:person_id=>chunky.person_id).length )
     Person.__delete( chunky )
+  end
+
+  def test_resetting_password
+    user = Person.new(:login_name=>'test_password_reset',:email_contact=>'sven@pentabarf.org')
+    Person.__write( user )
+    get :forgot_password
+    assert_response :success
+    token = extract_token( @response.body )
+    assert_not_nil token
+    post :save_forgot_password, {:token=>token,:login_name=>'test_password_reset',:email=>'sven@pentabarf.org'}
+    assert_response :redirect
+    get :reset_link_sent
+    assert_response :success
+    id = Password_reset_string.select_single(:person_id=>user.person_id)
+    get :reset_password, :id=>id.activation_string
+    assert_response :success
+    token = extract_token( @response.body )
+    post :save_reset_password, :id=>id.activation_string,:password=>'newpass',:password2=>'newpass',:token=>token
+    assert_response :redirect
+    assert_not_equal( user.password, Person.select_single(:person_id=>user.person_id).password )
+    Person.__delete( user )
+  end
+
+  def extract_token( body )
+    token = body.match( /<input [^>]+value="([a-f0-9]{40,40})"/ )
+    token ? token[1] : nil
   end
 
 end
