00880545675025a35a8b5dba441331377b1fd771
Sven Klemm <sven@c3d2.de>
2007-10-04 07:33:09 +0000

    add cascading to foreign key constraints of table role_permission

--- sql/maintenance/upgrade_0.3.2_to_0.3.3.sql
+++ sql/maintenance/upgrade_0.3.2_to_0.3.3.sql
@@ -1,12 +1,17 @@
 
 BEGIN;
 
- ALTER TABLE auth.permission_localized DROP CONSTRAINT permission_localized_permission_fkey;
- ALTER TABLE auth.permission_localized ADD constraint permission_localized_permission_fkey FOREIGN KEY (permission) REFERENCES auth.permission(permission) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE auth.permission_localized DROP CONSTRAINT permission_localized_permission_fkey;
+ALTER TABLE auth.permission_localized ADD constraint permission_localized_permission_fkey FOREIGN KEY (permission) REFERENCES auth.permission(permission) ON UPDATE CASCADE ON DELETE CASCADE;
 
- ALTER TABLE auth.permission_localized DROP CONSTRAINT permission_localized_translated_id_fkey;
- ALTER TABLE auth.permission_localized ADD CONSTRAINT permission_localized_translated_id_fkey FOREIGN KEY (translated_id) REFERENCES "language"(language_id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE auth.permission_localized DROP CONSTRAINT permission_localized_translated_id_fkey;
+ALTER TABLE auth.permission_localized ADD CONSTRAINT permission_localized_translated_id_fkey FOREIGN KEY (translated_id) REFERENCES "language"(language_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
+ALTER TABLE auth.role_permission DROP CONSTRAINT role_permission_permission_fkey;
+ALTER TABLE auth.role_permission ADD CONSTRAINT role_permission_permission_fkey FOREIGN KEY (permission) REFERENCES auth.permission(permission) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE auth.role_permission DROP CONSTRAINT role_permission_role_fkey;
+ALTER TABLE auth.role_permission ADD CONSTRAINT role_permission_role_fkey FOREIGN KEY ("role") REFERENCES auth."role"("role") ON UPDATE CASCADE ON DELETE CASCADE;
 
 COMMIT;
 
