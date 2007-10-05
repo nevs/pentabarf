086483b5051fa8b557a1725b4ed3b9dde26e28c8
Sven Klemm <sven@c3d2.de>
2007-10-05 13:20:44 +0000

    remove surrogate key from table event_state

--- sql/maintenance/upgrade_0.3.2_to_0.3.3.sql
+++ sql/maintenance/upgrade_0.3.2_to_0.3.3.sql
@@ -17,5 +17,33 @@ UPDATE auth.permission SET permission = 'modify_account' WHERE permission = 'mod
 UPDATE auth.permission SET permission = 'create_account' WHERE permission = 'create_login';
 UPDATE auth.permission SET permission = 'delete_account' WHERE permission = 'delete_login';
 
+-- event_role surrogate key removal
+ALTER TABLE event_role ADD COLUMN event_role TEXT;
+UPDATE event_role SET event_role = tag;
+ALTER TABLE event_role DROP COLUMN tag CASCADE;
+
+ALTER TABLE event_role_localized ADD COLUMN event_role TEXT;
+UPDATE event_role_localized SET event_role = ( SELECT event_role FROM event_role WHERE event_role.event_role_id = event_role_localized.event_role_id );
+ALTER TABLE event_role_state ADD COLUMN event_role TEXT;
+UPDATE event_role_state SET event_role = ( SELECT event_role FROM event_role WHERE event_role.event_role_id = event_role_state.event_role_id );
+ALTER TABLE event_person ADD COLUMN event_role TEXT;
+UPDATE event_person SET event_role = ( SELECT event_role FROM event_role WHERE event_role.event_role_id = event_person.event_role_id );
+
+ALTER TABLE event_role DROP CONSTRAINT event_role_pkey CASCADE;
+ALTER TABLE event_role ADD CONSTRAINT event_role_pkey PRIMARY KEY( event_role );
+
+ALTER TABLE event_role_localized DROP CONSTRAINT event_role_localized_pkey;
+ALTER TABLE event_role_localized ADD CONSTRAINT event_role_localized_pkey PRIMARY KEY( event_role, language_id );
+ALTER TABLE event_role_localized ADD CONSTRAINT event_role_localized_event_role_fkey FOREIGN KEY( event_role ) REFERENCES event_role(event_role) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE event_role_localized DROP COLUMN event_role_id CASCADE;
+
+ALTER TABLE event_role_state DROP CONSTRAINT event_role_state_pkey CASCADE;
+ALTER TABLE event_role_state ADD CONSTRAINT event_role_state_event_role_fkey FOREIGN KEY( event_role ) REFERENCES event_role(event_role) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE event_role_state DROP COLUMN event_role_id CASCADE;
+
+ALTER TABLE event_person DROP COLUMN event_role_id CASCADE;
+ALTER TABLE event_person ADD CONSTRAINT event_person_event_id_key UNIQUE( event_id, person_id, event_role );
+ALTER TABLE event_person ADD CONSTRAINT event_person_event_role_fkey FOREIGN KEY( event_role ) REFERENCES event_role(event_role) ON UPDATE CASCADE ON DELETE CASCADE;
+
 COMMIT;
 
