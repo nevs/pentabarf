3f14a649ec93dadde4912295768690379fc795de
Sven Klemm <sven@c3d2.de>
2007-10-06 10:56:44 +0000

    remove surrogate key from table event_role

--- sql/tables/event_role.sql
+++ sql/tables/event_role.sql
@@ -1,9 +1,7 @@
 
 CREATE TABLE event_role (
-  event_role_id SERIAL NOT NULL,
-  tag VARCHAR(32) NOT NULL UNIQUE,
+  event_role TEXT NOT NULL,
   rank INTEGER,
-  f_public BOOL,
-  PRIMARY KEY (event_role_id)
+  PRIMARY KEY (event_role)
 );
 
