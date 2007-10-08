81bfd688d2e4c9a0840c9c6204f4ee0f95cca686
Sven Klemm <sven@c3d2.de>
2007-10-08 14:26:04 +0000

    bugfix for review showing unreviewed events as reviewed

--- rails/app/controllers/pentabarf_controller.rb
+++ rails/app/controllers/pentabarf_controller.rb
@@ -30,7 +30,7 @@ class PentabarfController < ApplicationController
 
   def review
     @events = View_review.select(:conference_id=>@current_conference.conference_id,:translated_id=>POPE.user.current_language_id)
-    @ratings = Event_rating.select({:person_id=>POPE.user.person_id}).map{|r| r.event_id}
+    @ratings = Event_rating.select({:person_id=>POPE.user.person_id}).select{|r| r.remark || r.relevance || r.acceptance || r.actuality }.map{|r| r.event_id}
   end
 
   def conference
