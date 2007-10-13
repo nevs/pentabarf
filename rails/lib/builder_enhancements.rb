911b65d7c288a407d26dfc582bea87d0187dd0bc
Sven Klemm <sven@c3d2.de>
2007-10-13 13:00:43 +0000

    minor bugfixes for event submission page and form helper add submit button to password reset form

--- rails/lib/builder_enhancements.rb
+++ rails/lib/builder_enhancements.rb
@@ -8,7 +8,7 @@ module Builder
     def form( options, &block )
       options[:method] ||= :post
       method_missing(:form, options) do | xml |
-        xml.input( nil, {:type=>:hidden,:name=>:token,:value=>Token.generate(options[:action])})
+        xml.input({:type=>:hidden,:name=>:token,:value=>Token.generate(options[:action])})
         yield( xml )
       end
     end
