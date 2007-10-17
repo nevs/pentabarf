416f47db00a82f5ce2100499c6492fc60fb4c36d
Sven Klemm <sven@c3d2.de>
2007-10-17 09:23:02 +0000

    setting pentabarf version to 0.3.5

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   include Builder_helper
 
   def pentabarf_version
-    "0.3.4"
+    "0.3.5"
   end
 
   # tries to read the current revision of pentabarf from subversion meta data
