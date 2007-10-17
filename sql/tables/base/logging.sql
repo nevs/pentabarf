68dfe083290bfa4864c978e91cecbcd3bd3728a7
Sven Klemm <sven@c3d2.de>
2007-10-17 18:31:02 +0000

    add transaction_id to master log table

--- sql/tables/base/logging.sql
+++ sql/tables/base/logging.sql
@@ -1,7 +1,11 @@
 
 CREATE TABLE base.logging (
+  log_transaction_id BIGINT,
   log_operation   char(1),
   log_timestamp   TIMESTAMP NOT NULL,
   log_person_id   INTEGER
 );
+
+CREATE SEQUENCE log.log_transaction_id_seq;
+
 
