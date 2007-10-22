78a94d529e3e95dd68cb5fc7d5ee63ab93c0092e
Sven Klemm <sven@c3d2.de>
2007-10-22 14:09:32 +0000

    remove timestamp and person_id from base.logging

--- sql/tables/base/logging.sql
+++ sql/tables/base/logging.sql
@@ -1,9 +1,7 @@
 
 CREATE TABLE base.logging (
   log_transaction_id BIGINT,
-  log_operation   char(1),
-  log_timestamp   TIMESTAMP NOT NULL,
-  log_person_id   INTEGER
+  log_operation   char(1)
 );
 
 CREATE SEQUENCE log.log_transaction_id_seq;
