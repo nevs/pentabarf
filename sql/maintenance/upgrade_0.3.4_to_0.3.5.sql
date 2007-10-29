d40456893c2300b920e293e41f7ff1b812810a4c
Sven Klemm <sven@c3d2.de>
2007-10-29 19:28:01 +0000

    adjust types to conflict changes

--- sql/maintenance/upgrade_0.3.4_to_0.3.5.sql
+++ sql/maintenance/upgrade_0.3.4_to_0.3.5.sql
@@ -171,6 +171,133 @@ ALTER TABLE conflict.conference_phase_conflict DROP COLUMN conflict_level_id;
 
 ALTER TABLE conflict.conflict_level DROP COLUMN conflict_level_id;
 
+DROP TYPE conflict_person CASCADE;
+DROP TYPE conflict_person_conflict CASCADE;
+DROP TYPE conflict_event_person CASCADE;
+DROP TYPE conflict_event_person_conflict CASCADE;
+DROP TYPE conflict_event_person_event CASCADE;
+DROP TYPE conflict_event_person_event_conflict CASCADE;
+DROP TYPE conflict_event CASCADE;
+DROP TYPE conflict_event_conflict CASCADE;
+DROP TYPE conflict_event_event CASCADE;
+DROP TYPE conflict_event_event_conflict CASCADE;
+DROP TYPE view_conflict_event CASCADE;
+DROP TYPE view_conflict_event_event CASCADE;
+DROP TYPE view_conflict_event_person CASCADE;
+DROP TYPE view_conflict_event_person_event CASCADE;
+DROP TYPE view_conflict_person CASCADE;
+
+CREATE TYPE conflict.conflict_person AS (
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_person_conflict AS (
+  conflict    TEXT,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_person AS (
+  event_id    INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_person_conflict AS (
+  conflict    TEXT,
+  event_id    INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_person_event AS (
+  event_id1   INTEGER,
+  event_id2   INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_person_event_conflict AS (
+  conflict    TEXT,
+  event_id1   INTEGER,
+  event_id2   INTEGER,
+  person_id   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event AS (
+  event_id    INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_conflict AS (
+  conflict    TEXT,
+  event_id    INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_event AS (
+  event_id1   INTEGER,
+  event_id2   INTEGER
+);
+
+CREATE TYPE conflict.conflict_event_event_conflict AS (
+  conflict    TEXT,
+  event_id1   INTEGER,
+  event_id2   INTEGER
+);
+
+CREATE TYPE conflict.view_conflict_event AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  event_id    INTEGER,
+  title TEXT
+);
+
+CREATE TYPE conflict.view_conflict_event_event AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  event_id1 INTEGER,
+  event_id2 INTEGER,
+  title1 TEXT,
+  title2 TEXT
+);
+
+CREATE TYPE conflict.view_conflict_event_person AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  event_id INTEGER,
+  person_id INTEGER,
+  title TEXT,
+  name TEXT
+);
+
+CREATE TYPE conflict.view_conflict_event_person_event AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  person_id INTEGER,
+  event_id1 INTEGER,
+  event_id2 INTEGER,
+  name TEXT,
+  title1 TEXT,
+  title2 TEXT
+);
+
+CREATE TYPE conflict.view_conflict_person AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  person_id INTEGER,
+  name TEXT
+);
+
 -- inheritance based logging stuff
 
 CREATE TABLE base.logging (
--- sql/types.sql
+++ sql/types.sql
@@ -4,124 +4,114 @@
  *
 */
 
-CREATE TYPE conflict_person AS (
+CREATE TYPE conflict.conflict_person AS (
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_person_conflict AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.conflict_person_conflict AS (
+  conflict    TEXT,
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_event_person AS (
+CREATE TYPE conflict.conflict_event_person AS (
   event_id    INTEGER,
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_event_person_conflict AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.conflict_event_person_conflict AS (
+  conflict    TEXT,
   event_id    INTEGER,
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_event_person_event AS (
+CREATE TYPE conflict.conflict_event_person_event AS (
   event_id1   INTEGER,
   event_id2   INTEGER,
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_event_person_event_conflict AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.conflict_event_person_event_conflict AS (
+  conflict    TEXT,
   event_id1   INTEGER,
   event_id2   INTEGER,
   person_id   INTEGER
 );
 
-CREATE TYPE conflict_event AS (
+CREATE TYPE conflict.conflict_event AS (
   event_id    INTEGER
 );
 
-CREATE TYPE conflict_event_conflict AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.conflict_event_conflict AS (
+  conflict    TEXT,
   event_id    INTEGER
 );
 
-CREATE TYPE conflict_event_event AS (
+CREATE TYPE conflict.conflict_event_event AS (
   event_id1   INTEGER,
   event_id2   INTEGER
 );
 
-CREATE TYPE conflict_event_event_conflict AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.conflict_event_event_conflict AS (
+  conflict    TEXT,
   event_id1   INTEGER,
   event_id2   INTEGER
 );
 
-CREATE TYPE view_conflict_event AS (
-  conflict_id INTEGER,
-  event_id INTEGER,
-  conflict_level_id INTEGER,
-  level_name TEXT,
-  level_tag TEXT,
-  language_id INTEGER,
-  conflict_tag TEXT,
+CREATE TYPE conflict.view_conflict_event AS (
+  conflict    TEXT,
   conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  event_id    INTEGER,
   title TEXT
 );
 
-CREATE TYPE view_conflict_event_event AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.view_conflict_event_event AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
   event_id1 INTEGER,
   event_id2 INTEGER,
-  conflict_level_id INTEGER,
-  level_name TEXT,
-  level_tag TEXT,
-  language_id INTEGER,
-  conflict_tag TEXT,
-  conflict_name TEXT,
   title1 TEXT,
   title2 TEXT
 );
 
-CREATE TYPE view_conflict_event_person AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.view_conflict_event_person AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
   event_id INTEGER,
   person_id INTEGER,
-  conflict_level_id INTEGER,
-  level_name TEXT,
-  level_tag TEXT,
-  language_id INTEGER,
-  conflict_tag TEXT,
-  conflict_name TEXT,
   title TEXT,
   name TEXT
 );
 
-CREATE TYPE view_conflict_event_person_event AS (
-  conflict_id INTEGER,
+CREATE TYPE conflict.view_conflict_event_person_event AS (
+  conflict    TEXT,
+  conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
   person_id INTEGER,
   event_id1 INTEGER,
   event_id2 INTEGER,
-  conflict_level_id INTEGER,
-  level_name TEXT,
-  level_tag TEXT,
-  language_id INTEGER,
-  conflict_tag TEXT,
-  conflict_name TEXT,
   name TEXT,
   title1 TEXT,
   title2 TEXT
 );
 
-CREATE TYPE view_conflict_person AS (
-  conflict_id INTEGER,
-  person_id INTEGER,
-  conflict_level_id INTEGER,
-  level_name TEXT,
-  level_tag TEXT,
-  language_id INTEGER,
-  conflict_tag TEXT,
+CREATE TYPE conflict.view_conflict_person AS (
+  conflict    TEXT,
   conflict_name TEXT,
+  conflict_level TEXT,
+  conflict_level_name TEXT,
+  language_id INTEGER,
+  person_id INTEGER,
   name TEXT
 );
 
