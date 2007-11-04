45fc746fa9edee0ea5d1edc1d1761f1e827d5c03
Sven Klemm <sven@c3d2.de>
2007-11-04 12:52:38 +0000

    adding table definition for table account

---
+++ sql/tables/auth/account.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE auth.account (
+  account_id SERIAL,
+  login_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  salt TEXT,
+  password TEXT,
+  edit_token TEXT,
+  current_language_id INTEGER,
+  current_conference_id INTEGER,
+  preferences TEXT,
+  last_login TIMESTAMP,
+  person_id INTEGER REFERENCES public.person(person_id),
+  CHECK (login_name <> 'logout'),
+  CHECK ( strpos( login_name, ':' ) = 0 ),
+  PRIMARY KEY( account_id )
+);
+
