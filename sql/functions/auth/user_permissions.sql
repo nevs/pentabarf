0d82ce780cfb854374d207e0f3fff5550a96451d
Sven Klemm <sven@c3d2.de>
2007-11-05 19:33:36 +0000

    adjust functions to recent database changes

--- sql/functions/auth/user_permissions.sql
+++ sql/functions/auth/user_permissions.sql
@@ -3,10 +3,10 @@
  * function for getting the permissions of a user
 */
 
-CREATE OR REPLACE FUNCTION auth.user_permissions( person_id INTEGER ) RETURNS SETOF TEXT AS '
+CREATE OR REPLACE FUNCTION auth.user_permissions( account_id INTEGER ) RETURNS SETOF TEXT AS '
   SELECT DISTINCT permission
-  FROM auth.person_role
+  FROM auth.account_role
     INNER JOIN auth.role_permission USING( role )
-  WHERE person_role.person_id = $1;
+  WHERE account_role.account_id = $1;
 ' LANGUAGE SQL;
 
--- sql/functions/conflict/person/no_mail.sql
+++ sql/functions/conflict/person/no_mail.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION conflict.conflict_person_no_email(integer) RETURNS SE
       SELECT person_id FROM person
                             INNER JOIN event_person USING (person_id)
                             INNER JOIN event USING (event_id)
-        WHERE person.email_contact IS NULL AND
+        WHERE person.email IS NULL AND
               event.conference_id = $1;
 $$ LANGUAGE SQL;
 
