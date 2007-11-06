d5f70cb07256e2be77597c13aad947465539d175
Sven Klemm <sven@c3d2.de>
2007-11-06 14:22:06 +0000

    adjust testcases to person/account separation

--- rails/test/functional/user_controller_test.rb
+++ rails/test/functional/user_controller_test.rb
@@ -16,46 +16,49 @@ class UserControllerTest < Test::Unit::TestCase
       get :new_account
       assert_response :success
       submit_form do | form |
-        form.person.login_name = 'chunky'
-        form.person.email_contact = 'chunky@bacon.com'
-        form.person.password = 'bacon'
+        form.account.login_name = 'chunky'
+        form.account.email = 'chunky@bacon.com'
+        form.account.password = 'bacon'
         form.password = 'bacon'
       end
       assert_response :redirect
       assert_redirected_to( :action => :account_done)
-      chunky = Person.select_single(:login_name=>'chunky')
-      active = Account_activation.select_single(:person_id=>chunky.person_id)
+      chunky = Account.select_single(:login_name=>'chunky')
+      active = Account_activation.select_single(:account_id=>chunky.account_id)
       get :activate_account, :id=>active.activation_string
       assert_response :redirect
       assert_redirected_to( :controller => 'submission' )
-      assert_equal( 1, Person_role.select(:person_id=>chunky.person_id).length )
+      assert_equal( 1, Account_role.select(:account_id=>chunky.account_id).length )
     ensure
-      Person.__delete( chunky )
+      Account.__delete( chunky )
     end
   end
 
   def test_resetting_password
-    user = Person.new(:login_name=>'test_password_reset',:email_contact=>'sven@pentabarf.org')
-    Person.__write( user )
-    get :forgot_password
-    assert_response :success
-    submit_form do | form |
-      form.login_name = 'test_password_reset'
-      form.email = 'sven@pentabarf.org'
+    begin
+      user = Account.new(:login_name=>'test_password_reset',:email=>'sven@pentabarf.org')
+      Account.__write( user )
+      get :forgot_password
+      assert_response :success
+      submit_form do | form |
+        form.login_name = 'test_password_reset'
+        form.email = 'sven@pentabarf.org'
+      end
+      assert_response :redirect
+      get :reset_link_sent
+      assert_response :success
+      id = Account_password_reset.select_single(:account_id=>user.account_id)
+      get :reset_password, :id=>id.activation_string
+      assert_response :success
+      submit_form do | form |
+        form.password = 'newpass'
+        form.password2 = 'newpass'
+      end
+      assert_response :redirect
+      assert_not_equal( user.password, Account.select_single(:account_id=>user.account_id).password )
+    ensure
+      Account.__delete( user )
     end
-    assert_response :redirect
-    get :reset_link_sent
-    assert_response :success
-    id = Password_reset_string.select_single(:person_id=>user.person_id)
-    get :reset_password, :id=>id.activation_string
-    assert_response :success
-    submit_form do | form |
-      form.password = 'newpass'
-      form.password2 = 'newpass'
-    end
-    assert_response :redirect
-    assert_not_equal( user.password, Person.select_single(:person_id=>user.person_id).password )
-    Person.__delete( user )
   end
 
 end
