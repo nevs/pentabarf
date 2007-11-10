ba0c93a533ec40c1f7f2d22673570b07b2b75ff2
Sven Klemm <sven@c3d2.de>
2007-11-10 22:15:39 +0000

    make table conference_person_link_internal use table  inheritance

--- sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
+++ sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
@@ -974,6 +974,18 @@ CREATE TABLE log.conference_person_link () INHERITS( base.logging, base.conferen
 INSERT INTO conference_person_link(conference_person_id,url,title,rank) SELECT conference_person_id,url,title,rank FROM old_conference_person_link;
 DROP TABLE old_conference_person_link;
 
+ALTER TABLE conference_person_link_internal RENAME TO old_conference_person_link_internal;
+CREATE TABLE base.conference_person_link_internal ( conference_person_link_internal_id SERIAL NOT NULL, conference_person_id INTEGER NOT NULL, link_type TEXT NOT NULL, url TEXT NOT NULL, description TEXT, rank INTEGER);
+CREATE TABLE conference_person_link_internal (
+  FOREIGN KEY (conference_person_id) REFERENCES conference_person (conference_person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (link_type) REFERENCES link_type (link_type) ON UPDATE CASCADE ON DELETE RESTRICT,
+  PRIMARY KEY (conference_person_link_internal_id)
+) INHERITS( base.conference_person_link_internal );
+CREATE TABLE log.conference_person_link_internal () INHERITS( base.logging, base.conference_person_link_internal );
+INSERT INTO conference_person_link_internal(conference_person_id,link_type,url,description,rank) SELECT conference_person_id,link_type,url,description,rank FROM old_conference_person_link_internal;
+DROP TABLE old_conference_person_link_internal;
+
+
 
 
 
--- sql/tables/conference_person_link_internal.sql
+++ sql/tables/conference_person_link_internal.sql
@@ -1,13 +1,19 @@
 
-CREATE TABLE conference_person_link_internal (
+CREATE TABLE base.conference_person_link_internal (
   conference_person_link_internal_id SERIAL NOT NULL,
   conference_person_id INTEGER NOT NULL,
   link_type TEXT NOT NULL,
   url TEXT NOT NULL,
   description TEXT,
-  rank INTEGER,
+  rank INTEGER
+);
+
+CREATE TABLE conference_person_link_internal (
   FOREIGN KEY (conference_person_id) REFERENCES conference_person (conference_person_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (link_type) REFERENCES link_type (link_type) ON UPDATE CASCADE ON DELETE RESTRICT,
   PRIMARY KEY (conference_person_link_internal_id)
-);
+) INHERITS( base.conference_person_link_internal );
+
+CREATE TABLE log.conference_person_link_internal (
+) INHERITS( base.logging, base.conference_person_link_internal );
 
