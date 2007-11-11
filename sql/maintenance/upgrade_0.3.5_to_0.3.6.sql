ce2ce516020ecc1c5afb71e76bc312f2bb274b28
Sven Klemm <sven@c3d2.de>
2007-11-11 23:06:40 +0000

    make table event use table inheritance

--- sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
+++ sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
@@ -1214,7 +1214,25 @@ UPDATE event SET conference_team = (SELECT tag FROM team where team.team_id = ev
 DROP TABLE team_localized CASCADE;
 DROP TABLE team CASCADE;
 
+ALTER TABLE event ADD COLUMN conference_room TEXT;
+UPDATE event SET conference_room = (SELECT short_name FROM old_room WHERE old_room.room_id = event.room_id);
+DROP TABLE old_room CASCADE;
 
+ALTER TABLE event RENAME TO old_event;
+CREATE TABLE base.event ( event_id SERIAL NOT NULL, conference_id INTEGER NOT NULL, tag TEXT, title TEXT NOT NULL, subtitle TEXT, conference_track TEXT, conference_team TEXT, event_type TEXT, duration INTERVAL NOT NULL DEFAULT '1:00:00', event_origin TEXT NOT NULL DEFAULT 'idea', event_state TEXT NOT NULL DEFAULT 'undecided', event_state_progress TEXT NOT NULL DEFAULT 'new', language TEXT, conference_room TEXT, day SMALLINT, start_time INTERVAL, abstract TEXT, description TEXT, resources TEXT, public BOOL NOT NULL DEFAULT FALSE, paper BOOL, slides BOOL, remark TEXT, submission_notes TEXT);
+CREATE TABLE event (
+  FOREIGN KEY (conference_id) REFERENCES conference (conference_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (conference_track,conference_id) REFERENCES conference_track (conference_track,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (conference_team,conference_id) REFERENCES conference_team(conference_team,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (event_type) REFERENCES event_type(event_type) ON UPDATE CASCADE ON DELETE SET NULL,
+  FOREIGN KEY (event_origin) REFERENCES event_origin (event_origin) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (event_state,event_state_progress) REFERENCES event_state_progress (event_state,event_state_progress) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (language) REFERENCES language (language) ON UPDATE CASCADE ON DELETE SET NULL,
+  FOREIGN KEY (conference_room,conference_id) REFERENCES conference_room (conference_room,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  PRIMARY KEY (event_id)
+) INHERITS( base.event );
+CREATE TABLE log.event () INHERITS( base.logging, base.event );
+INSERT INTO event( event_id,conference_id,tag,title,subtitle,conference_track,conference_team,event_type,duration,event_origin,event_state,event_state_progress,language,conference_room,day,start_time,abstract,description,resources,public,paper,slides,remark,submission_notes ) SELECT event_id,conference_id,tag,title,subtitle,(SELECT tag FROM old_conference_track WHERE old_conference_track.conference_track_id = old_event.conference_track_id),conference_team,event_type,duration,event_origin,event_state,event_state_progress,(SELECT language FROM old_language WHERE old_language.language_id = old_event.language_id),conference_room,day,start_time,abstract,description,resources,f_public,f_paper,f_slides,remark,submission_notes FROM old_event;
 
 
 
@@ -1286,10 +1304,6 @@ SELECT setval(pg_get_serial_sequence('conference_transaction','conference_transa
 
 DROP TABLE old_conference_transaction CASCADE;
 
-ALTER TABLE event ADD COLUMN conference_room TEXT;
-UPDATE event SET conference_room = (SELECT short_name FROM old_room WHERE old_room.room_id = event.room_id);
-
-DROP TABLE old_room CASCADE;
 
 COMMIT;
 
--- sql/tables/event.sql
+++ sql/tables/event.sql
@@ -1,43 +1,43 @@
 
-CREATE TABLE event (
+CREATE TABLE base.event (
   event_id SERIAL NOT NULL,
   conference_id INTEGER NOT NULL,
   tag TEXT,
   title TEXT NOT NULL,
   subtitle TEXT,
-  conference_track_id INTEGER,
+  conference_track TEXT,
   conference_team TEXT,
   event_type TEXT,
-  duration INTERVAL NOT NULL,
-  event_origin TEXT NOT NULL,
+  duration INTERVAL NOT NULL DEFAULT '1:00:00',
+  event_origin TEXT NOT NULL DEFAULT 'idea',
   event_state TEXT NOT NULL DEFAULT 'undecided',
   event_state_progress TEXT NOT NULL DEFAULT 'new',
-  language_id INTEGER,
+  language TEXT,
   conference_room TEXT,
   day SMALLINT,
   start_time INTERVAL,
   abstract TEXT,
   description TEXT,
   resources TEXT,
-  f_public BOOL NOT NULL DEFAULT FALSE,
-  f_paper BOOL,
-  f_slides BOOL,
+  public BOOL NOT NULL DEFAULT FALSE,
+  paper BOOL,
+  slides BOOL,
   remark TEXT,
-  submission_notes TEXT,
-  last_modified TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  last_modified_by INTEGER,
+  submission_notes TEXT
+);
+
+CREATE TABLE event (
   FOREIGN KEY (conference_id) REFERENCES conference (conference_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (conference_track_id) REFERENCES conference_track (conference_track_id) ON UPDATE CASCADE ON DELETE SET NULL,
+  FOREIGN KEY (conference_track,conference_id) REFERENCES conference_track (conference_track,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
   FOREIGN KEY (conference_team,conference_id) REFERENCES conference_team(conference_team,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
   FOREIGN KEY (event_type) REFERENCES event_type(event_type) ON UPDATE CASCADE ON DELETE SET NULL,
   FOREIGN KEY (event_origin) REFERENCES event_origin (event_origin) ON UPDATE CASCADE ON DELETE RESTRICT,
   FOREIGN KEY (event_state,event_state_progress) REFERENCES event_state_progress (event_state,event_state_progress) ON UPDATE CASCADE ON DELETE RESTRICT,
-  FOREIGN KEY (language_id) REFERENCES language (language_id) ON UPDATE CASCADE ON DELETE SET NULL,
+  FOREIGN KEY (language) REFERENCES language (language) ON UPDATE CASCADE ON DELETE SET NULL,
   FOREIGN KEY (conference_room,conference_id) REFERENCES conference_room (conference_room,conference_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-  FOREIGN KEY (last_modified_by) REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE SET NULL,
   PRIMARY KEY (event_id)
-);
+) INHERITS( base.event );
 
-CREATE INDEX event_conference_id_index ON event(conference_id);
-CREATE INDEX event_event_state_index ON event(event_state,event_state_progress);
+CREATE TABLE log.event (
+) INHERITS( base.logging, base.event );
 
