dcbb2b610cf94e1e3f6467f0236861420f66034b
Sven Klemm <sven@c3d2.de>
2007-11-11 23:27:44 +0000

    make table event_rating use table inheritnace

--- sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
+++ sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
@@ -1245,6 +1245,21 @@ CREATE TABLE log.event_image () INHERITS( base.logging, base.event_image );
 INSERT INTO event_image(event_id,mime_type,image) SELECT event_id,mime_type,image FROM old_event_image;
 DROP TABLE event_image CASCADE;
 
+ALTER TABLE event_rating RENAME TO old_event_rating;
+CREATE TABLE base.event_rating ( person_id INTEGER NOT NULL, event_id INTEGER NOT NULL, relevance SMALLINT CHECK (relevance > 0 AND relevance < 6), actuality SMALLINT CHECK (actuality > 0 AND actuality < 6), acceptance SMALLINT CHECK (acceptance > 0 AND acceptance < 6), remark TEXT, eval_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now());
+CREATE TABLE event_rating (
+  FOREIGN KEY (person_id) REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (event_id) REFERENCES event (event_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  PRIMARY KEY (person_id, event_id)
+) INHERITS( base.event_rating );
+CREATE TABLE log.event_rating (
+) INHERITS( base.logging, base.event_rating );
+INSERT INTO event_rating(person_id,event_id,relevance,actuality,acceptance,remark,eval_time) SELECT person_id,event_id,relevance,actuality,acceptance,remark,eval_time FROM old_event_rating;
+DROP TABLE old_event_rating;
+
+
+
+
 
 
 
--- sql/tables/event_rating.sql
+++ sql/tables/event_rating.sql
@@ -1,14 +1,20 @@
 
-CREATE TABLE event_rating (
+CREATE TABLE base.event_rating (
   person_id INTEGER NOT NULL,
   event_id INTEGER NOT NULL,
   relevance SMALLINT CHECK (relevance > 0 AND relevance < 6),
   actuality SMALLINT CHECK (actuality > 0 AND actuality < 6),
   acceptance SMALLINT CHECK (acceptance > 0 AND acceptance < 6),
   remark TEXT,
-  eval_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  eval_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE event_rating (
   FOREIGN KEY (person_id) REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (event_id) REFERENCES event (event_id) ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY (person_id, event_id)
-);
+) INHERITS( base.event_rating );
+
+CREATE TABLE log.event_rating (
+) INHERITS( base.logging, base.event_rating );
 
