9201647e9917107dc98b039f80ee9fc77e9f65ff
Sven Klemm <sven@c3d2.de>
2007-11-11 17:44:07 +0000

    make table person_availability use tabl e inheritance

--- sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
+++ sql/maintenance/upgrade_0.3.5_to_0.3.6.sql
@@ -1095,7 +1095,17 @@ INSERT INTO person_im(person_id,im_type,im_address,rank) SELECT person_id,im_typ
 DROP TABLE old_person_im;
 DROP TABLE old_im_type;
 
-
+ALTER TABLE person_availability RENAME TO old_person_availability;
+CREATE TABLE base.person_availability ( person_availability_id SERIAL, person_id INTEGER NOT NULL, conference_id INTEGER NOT NULL, start_date TIMESTAMP WITH TIME ZONE NOT NULL, duration INTERVAL NOT NULL);
+CREATE TABLE person_availability (
+  FOREIGN KEY (person_id) REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (conference_id) REFERENCES conference (conference_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  PRIMARY KEY (person_availability_id),
+  UNIQUE (person_id, conference_id, start_date)
+) INHERITS( base.person_availability );
+CREATE TABLE log.person_availability () INHERITS( base.logging, base.person_availability );
+INSERT INTO person_availability(person_id,conference_id,start_date,duration) SELECT person_id,conference_id,start_date,duration FROM old_person_availability;
+DROP TABLE old_person_availability;
 
 
 
--- sql/tables/person_availability.sql
+++ sql/tables/person_availability.sql
@@ -1,13 +1,19 @@
 
-CREATE TABLE person_availability (
+CREATE TABLE base.person_availability (
   person_availability_id SERIAL,
   person_id INTEGER NOT NULL,
   conference_id INTEGER NOT NULL,
   start_date TIMESTAMP WITH TIME ZONE NOT NULL,
-  duration INTERVAL NOT NULL,
+  duration INTERVAL NOT NULL
+);
+
+CREATE TABLE person_availability (
   FOREIGN KEY (person_id) REFERENCES person (person_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (conference_id) REFERENCES conference (conference_id) ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY (person_availability_id),
   UNIQUE (person_id, conference_id, start_date)
-);
+) INHERITS( base.person_availability );
+
+CREATE TABLE log.person_availability (
+) INHERITS( base.logging, base.person_availability );
 
