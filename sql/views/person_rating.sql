5a26ac20b0c05255457e7597d43d0e867ce750e3
Sven Klemm <sven@c3d2.de>
2007-11-17 18:45:44 +0000

    only include rows with actual ratings in view_person_rating

--- sql/views/person_rating.sql
+++ sql/views/person_rating.sql
@@ -1,13 +1,20 @@
 
 CREATE OR REPLACE VIEW view_person_rating AS
-  SELECT person_rating.person_id,
-         person_rating.evaluator_id,
-         person_rating.speaker_quality,
-         person_rating.competence,
-         person_rating.remark,
-         person_rating.eval_time,
-         view_person.name
-    FROM person_rating
-         INNER JOIN view_person ON (person_rating.evaluator_id = view_person.person_id)
-    ORDER BY person_rating.eval_time DESC;
+  SELECT
+    person_rating.person_id,
+    person_rating.evaluator_id,
+    person_rating.speaker_quality,
+    person_rating.competence,
+    person_rating.remark,
+    person_rating.eval_time,
+    view_person.name
+  FROM
+    person_rating
+    INNER JOIN view_person ON (person_rating.evaluator_id = view_person.person_id)
+  WHERE
+    person_rating.speaker_quality IS NOT NULL OR
+    person_rating.competence IS NOT NULL OR
+    person_rating.remark IS NOT NULL
+  ORDER BY person_rating.eval_time DESC
+;
 
