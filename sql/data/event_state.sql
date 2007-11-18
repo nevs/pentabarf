81e656cef83cb43aeca0f0c88b8f02f9108ff699
Sven Klemm <sven@c3d2.de>
2007-11-18 14:58:15 +0000

    set ranks for event_state

--- sql/data/event_state.sql
+++ sql/data/event_state.sql
@@ -12,9 +12,9 @@ SET search_path = public, pg_catalog;
 -- Data for Name: event_state; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO event_state (event_state, rank) VALUES ('undecided', NULL);
-INSERT INTO event_state (event_state, rank) VALUES ('accepted', NULL);
-INSERT INTO event_state (event_state, rank) VALUES ('rejected', NULL);
+INSERT INTO event_state (event_state, rank) VALUES ('undecided', 1);
+INSERT INTO event_state (event_state, rank) VALUES ('accepted', 2);
+INSERT INTO event_state (event_state, rank) VALUES ('rejected', 3);
 
 
 --
