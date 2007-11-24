c739e067568b9660fe2c61d578fb0f4f955ee439
Sven Klemm <sven@c3d2.de>
2007-11-24 18:55:12 +0000

    adding csv export of resources report

--- rails/app/controllers/csv_controller.rb
+++ rails/app/controllers/csv_controller.rb
@@ -1,2 +1,29 @@
+
+require 'csv'
+
 class CsvController < ApplicationController
+
+  def resources
+    @current_conference = Conference.select_single(:acronym=>params[:id])
+    resources = View_report_resources.select({:conference_id => @current_conference.conference_id})
+    generate_csv( resources, 'resources.csv' ) do | d |
+      [ [d.title,d.subtitle].join(' '), d.resources ]
+    end
+  end
+
+  protected
+
+  def generate_csv( data, filename )
+    out = ""
+    CSV::Writer.generate( out ) do | csv |
+      data.each do | d |
+        csv << yield( d )
+      end
+    end
+    response.headers['Content-Type'] = 'text/csv'
+    response.headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
+    render( :text => out, :layout => false )
+  end
+
 end
+
