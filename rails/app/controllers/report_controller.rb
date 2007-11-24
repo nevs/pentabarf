9dcae35ec48f6165c56ac386319e10d8272e786c
Sven Klemm <sven@c3d2.de>
2007-11-24 19:19:53 +0000

    minor optimizations for report controller

--- rails/app/controllers/report_controller.rb
+++ rails/app/controllers/report_controller.rb
@@ -6,32 +6,17 @@ class ReportController < ApplicationController
   def index
   end
 
-  def expenses
-    @report_class = View_report_expenses
-  end
-
-  def paper
-  end
-
-  def feedback
-    @rows = View_report_feedback.select({:conference_id=>@current_conference.conference_id})
-  end
-
-  def resources
-    @rows = View_report_resources.select({:conference_id=>@current_conference.conference_id})
-  end
-
-  [].each do | report |
+  REPORTS.each do | report |
     define_method( report ) do
-      @report_class = Object.const_get("View_report_#{report}")
-      render(:action => 'report')
+      @content_title = "#{params[:action].capitalize} Report"
+      @rows = "View_report_#{report}".constantize.select({:conference_id=>@current_conference.conference_id})
     end
   end
 
   protected
 
   def init
-    @content_title = "Reports"
+    @content_title = 'Reports'
     @current_conference = Conference.select_single(:conference_id => POPE.user.current_conference_id)
     @current_language = POPE.user.current_language
   end
