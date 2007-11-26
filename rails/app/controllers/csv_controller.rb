357fe0cddba38bf99cb853e671c647310ce6a948
Sven Klemm <sven@c3d2.de>
2007-11-26 14:24:36 +0000

    adding csv export for expenses, feedback and paper report

--- rails/app/controllers/csv_controller.rb
+++ rails/app/controllers/csv_controller.rb
@@ -3,10 +3,34 @@ require 'csv'
 
 class CsvController < ApplicationController
 
+  def expenses
+    @current_conference = Conference.select_single(:acronym=>params[:id])
+    rows = View_report_expenses.select({:conference_id => @current_conference.conference_id})
+    generate_csv( rows, 'expenses.csv' ) do | d |
+      [ d.name, d.fee, d.fee_currency, d.travel_cost, d.travel_currency, d.accommodation_cost, d.accommodation_currency ]
+    end
+  end
+
+  def feedback
+    @current_conference = Conference.select_single(:acronym=>params[:id])
+    rows = View_report_feedback.select({:conference_id => @current_conference.conference_id})
+    generate_csv( rows, 'feedback.csv' ) do | d |
+      [ [d.title,d.subtitle].join(' '), d.comments, d.votes ]
+    end
+  end
+
+  def paper
+    @current_conference = Conference.select_single(:acronym=>params[:id])
+    rows = View_report_paper.select({:conference_id => @current_conference.conference_id})
+    generate_csv( rows, 'paper.csv' ) do | d |
+      [ [d.title,d.subtitle].join(' '), d.paper_submitted > 0 ? 'yes' : 'no', d.pages ]
+    end
+  end
+
   def resources
     @current_conference = Conference.select_single(:acronym=>params[:id])
-    resources = View_report_resources.select({:conference_id => @current_conference.conference_id})
-    generate_csv( resources, 'resources.csv' ) do | d |
+    rows = View_report_resources.select({:conference_id => @current_conference.conference_id})
+    generate_csv( rows, 'resources.csv' ) do | d |
       [ [d.title,d.subtitle].join(' '), d.resources ]
     end
   end
