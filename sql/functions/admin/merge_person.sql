d776af2923a708c18633ef14220152f53e301bdd
Sven Klemm <sven@c3d2.de>
2007-12-06 11:42:56 +0000

    bugfixes for merge_person procedure

--- sql/functions/admin/merge_person.sql
+++ sql/functions/admin/merge_person.sql
@@ -18,18 +18,15 @@ CREATE OR REPLACE FUNCTION merge_person( primary_person INTEGER, secondary_perso
     UPDATE person_rating SET person_id = primary_person
       WHERE person_id = secondary_person AND
             evaluator_id NOT IN ( SELECT evaluator_id FROM person_rating WHERE person_id = primary_person);
-    UPDATE person_image SET person_id = primary_person WHERE person_id = secondary_person;
+    UPDATE person_image SET person_id = primary_person WHERE person_id = secondary_person AND NOT EXISTS( SELECT 1 FROM person_image WHERE person_id = primary_person);
 
     UPDATE event_transaction SET changed_by = primary_person WHERE changed_by = secondary_person;
     UPDATE event_person SET person_id = primary_person WHERE person_id = secondary_person;
-    UPDATE event_person SET last_modified_by = primary_person WHERE last_modified_by = secondary_person;
-    UPDATE event_attachment SET last_modified_by = primary_person WHERE last_modified_by = secondary_person;
 
     UPDATE conference_transaction SET changed_by = primary_person WHERE changed_by = secondary_person;
     UPDATE conference_person SET person_id = primary_person
       WHERE person_id = secondary_person AND
             conference_id NOT IN ( SELECT conference_id FROM conference_person WHERE person_id = primary_person );
-    UPDATE conference_person SET last_modified_by = primary_person WHERE last_modified_by = secondary_person;
 
     DELETE FROM person WHERE person_id = secondary_person;
     RETURN primary_person;
