cb55d4e42819f71589730cdbfe221d7fddfa078f
Sven Klemm <sven@c3d2.de>
2007-12-15 16:04:28 +0000

    bugfix for xml export of schedule

--- rails/app/controllers/xml_controller.rb
+++ rails/app/controllers/xml_controller.rb
@@ -4,6 +4,7 @@ class XmlController < ApplicationController
 
   def schedule
     @conference = Conference.select_single({:conference_id => params[:id]})
+    @days = Conference_day.select({:conference_id=>@conference.conference_id},{:order=>:conference_day})
     @rooms = Conference_room.select({:conference_id=>@conference.conference_id, :public=>'t'},{:order=>:rank})
     @events = View_schedule_event.select({:conference_id=>@conference.conference_id,:translated=>@current_language})
   end
