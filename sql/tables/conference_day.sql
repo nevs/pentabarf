cd01a45c45d3dad82776aaa7b64d914eb5b122ef
Sven Klemm <sven@c3d2.de>
2007-12-19 20:55:02 +0000

    fix foreign key constraint for table conference_day

--- sql/tables/conference_day.sql
+++ sql/tables/conference_day.sql
@@ -7,7 +7,7 @@ CREATE TABLE base.conference_day (
 );
 
 CREATE TABLE conference_day (
-  FOREIGN KEY( conference_id ) REFERENCES conference( conference_id ),
+  FOREIGN KEY( conference_id ) REFERENCES conference( conference_id ) ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY( conference_id, conference_day)
 ) INHERITS( base.conference_day );
 
