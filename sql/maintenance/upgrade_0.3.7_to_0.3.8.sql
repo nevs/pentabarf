0fc3fb0ad0caa7bbf010940b405ccb921dce20a0
Sven Klemm <sven@c3d2.de>
2007-12-19 21:10:31 +0000

    adding VEF in upgrade script for 0.3.8

--- sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
+++ sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
@@ -4,5 +4,7 @@ BEGIN;
 ALTER TABLE conference_day DROP CONSTRAINT conference_day_conference_id_fkey;
 ALTER TABLE conference_day ADD CONSTRAINT conference_day_conference_id_fkey FOREIGN KEY( conference_id ) REFERENCES conference( conference_id ) ON UPDATE CASCADE ON DELETE CASCADE;
 
+INSERT INTO currency (currency, exchange_rate) VALUES ('VEF', NULL);
+
 COMMIT;
 
