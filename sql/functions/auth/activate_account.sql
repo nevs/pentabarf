d9ae33ead208b6f60ac5c24a93eb007539854896
Sven Klemm <sven@c3d2.de>
2007-12-20 01:57:17 +0000

    set current_conference_id in activate_account

--- sql/functions/auth/activate_account.sql
+++ sql/functions/auth/activate_account.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
       INSERT INTO auth.account_role(account_id, role) VALUES (cur_activation.account_id, 'submitter');
       SELECT INTO cur_person_id nextval(pg_get_serial_sequence('base.person','person_id'));
       INSERT INTO person(person_id,nickname,email) SELECT cur_person_id,login_name,email FROM auth.account WHERE account.account_id = cur_activation.account_id;
-      UPDATE auth.account SET person_id = cur_person_id WHERE account_id = cur_activation.account_id;
+      UPDATE auth.account SET person_id = cur_person_id, current_conference_id = cur_activation.conference_id WHERE account_id = cur_activation.account_id;
       INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_person_id, now(), cur_person_id, 't');
       DELETE FROM auth.account_activation WHERE account_activation.activation_string = activation_string;
     ELSE
