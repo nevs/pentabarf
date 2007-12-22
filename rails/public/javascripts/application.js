748acc71855cb5f12e9cef91889c16544d45222c
Sven Klemm <sven@c3d2.de>
2007-12-22 14:18:02 +0000

    bugfix for event page not showing all persons in opera

--- rails/public/javascripts/application.js
+++ rails/public/javascripts/application.js
@@ -297,7 +297,7 @@ function add_event_person( event_person_id, person_id, event_role, event_role_st
   var index = table_row_counter['event_person'] - 1;
   var select = $('event_person[' + index + '][person_id]');
   var link = document.createElement('a');
-  link.href = document.baseURI.replace( /event\/\d+(#.*)?/, 'person/' + select.value );
+  link.href = document.URL.replace( /event\/\d+(#.*)?/, 'person/' + select.value );
   link.appendChild( document.createTextNode( select.options[select.selectedIndex].text ) );
   select.parentNode.appendChild( link );
   replace_select_with_hidden_field( select );
@@ -308,7 +308,7 @@ function add_person_event( event_person_id, event_id, event_role, event_role_sta
   var index = table_row_counter['event_person'] - 1;
   var select = $('event_person[' + index + '][event_id]');
   var link = document.createElement('a');
-  link.href = document.baseURI.replace( /person\/\d+(#.*)?/, 'event/' + select.value );
+  link.href = document.URL.replace( /person\/\d+(#.*)?/, 'event/' + select.value );
   link.appendChild( document.createTextNode( select.options[select.selectedIndex].text ) );
   select.parentNode.appendChild( link );
   replace_select_with_hidden_field( select );
