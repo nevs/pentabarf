b45776eb7723c3a273214691ecfe759693441ce3
Sven Klemm <sven@c3d2.de>
2007-12-27 15:17:02 +0000

    replace event_rating_public with event_feedback in ui_message

--- sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
+++ sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
@@ -6,5 +6,7 @@ ALTER TABLE conference_day ADD CONSTRAINT conference_day_conference_id_fkey FORE
 
 INSERT INTO currency (currency, exchange_rate) VALUES ('VEF', NULL);
 
+UPDATE ui_message SET ui_message = replace( ui_message, ':event_rating_public:', ':event_feedback:');
+
 COMMIT;
 
