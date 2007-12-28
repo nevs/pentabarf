f09ddf1f355ea674a61472ba7441b1d9ac031b10
Sven Klemm <sven@c3d2.de>
2007-12-28 14:48:00 +0000

    add missing localizations for conflicts in upgrade script

--- sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
+++ sql/maintenance/upgrade_0.3.7_to_0.3.8.sql
@@ -8,5 +8,12 @@ INSERT INTO currency (currency, exchange_rate) VALUES ('VEF', NULL);
 
 UPDATE ui_message SET ui_message = replace( ui_message, ':event_rating_public:', ':event_feedback:');
 
+INSERT INTO conflict.conflict_localized VALUES ('event_paper_unknown','en','Event with unknown paper state.');
+INSERT INTO conflict.conflict_localized VALUES ('event_paper_unknown','de','Veranstaltung mit unklarem Paper-Status.');
+INSERT INTO conflict.conflict_localized VALUES ('event_person_event_time_attendee','en','Attendee with two events at the same time.');
+INSERT INTO conflict.conflict_localized VALUES ('event_person_event_time_attendee','de','Besucher mit zwei gleichzeitigen Veranstaltungen.');
+INSERT INTO conflict.conflict_localized VALUES ('event_slides_unknown','en','Event with unknown slides state.');
+INSERT INTO conflict.conflict_localized VALUES ('event_slides_unknown','de','Veranstaltung mit unklarem Folien-Status.');
+
 COMMIT;
 
