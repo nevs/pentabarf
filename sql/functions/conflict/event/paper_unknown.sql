fdad9a288b5bbb2f370cf5d5aa2d9262cb3a1767
Sven Klemm <sven@c3d2.de>
2007-12-28 15:25:04 +0000

    bugfix for event_paper_unknown conflict function

--- sql/functions/conflict/event/paper_unknown.sql
+++ sql/functions/conflict/event/paper_unknown.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION conflict.conflict_event_paper_unknown( conference_id INTEGER ) RETURNS SETOF conflict.conflict_event AS $$
       SELECT event_id
         FROM event
-       WHERE event.conference_id = conference_id AND
+       WHERE event.conference_id = $1 AND
              event.event_state = 'accepted' AND
              event.paper IS NULL
 $$ LANGUAGE SQL;
