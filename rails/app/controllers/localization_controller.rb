6a60ab239faba1a48c00bc8cd2b021e20624e308
Sven Klemm <sven@c3d2.de>
2008-01-09 18:09:09 +0000

    change localization controller to show all localizable languages

--- rails/app/controllers/localization_controller.rb
+++ rails/app/controllers/localization_controller.rb
@@ -8,14 +8,18 @@ class LocalizationController < ApplicationController
 
   def ui_message
     @content_title = 'Localization ui_message'
-    constraints = {:translated=>@current_language}
+    @languages = Language.select({:localized=>'t'},{:order=>:language})
+    @tags = Ui_message.select({},{:order=>:ui_message})
+    constraints = {:translated=>@languages.map(&:language)}
     constraints[:ui_message] = {:ilike=>"#{params[:id]}%"} if params[:id]
     @messages = Ui_message_localized.select(constraints,{:order=>:ui_message})
   end
 
   def save_ui_message
     params[:ui_message].each do | ui_message, value |
-      write_row( Ui_message_localized, value, {:preset=>{:ui_message=>ui_message,:translated=>params[:translated]}})
+      value.each do | translated, value |
+        write_row( Ui_message_localized, value, {:only=>[:name],:preset=>{:ui_message=>ui_message,:translated=>translated},:ignore_empty=>:name})
+      end
     end
     Localizer.purge(params[:translated])
     redirect_to( :action => :ui_message )
