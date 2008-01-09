4d3031e2cfa40e5c40b69e84d1a8d21a880c1e8a
Sven Klemm <sven@c3d2.de>
2008-01-09 22:31:02 +0000

    add localization of table transport

--- sql/maintenance/upgrade_0.3.8_to_0.3.9.sql
+++ sql/maintenance/upgrade_0.3.8_to_0.3.9.sql
@@ -2,7 +2,7 @@
 BEGIN;
 
 INSERT INTO auth.object_domain(object,domain) VALUES ('attachment_type_localized','localization');
-
+INSERT INTO auth.object_domain(object,domain) VALUES ('transport_localized','localization');
 
 COMMIT;
 
