85953ba29c7d2f95a0285317bac594e15531805e
Sven Klemm <sven@c3d2.de>
2008-01-12 03:37:36 +0000

    remove pages column from slides report

--- sql/views/report/report_slides.sql
+++ sql/views/report/report_slides.sql
@@ -9,12 +9,7 @@ CREATE OR REPLACE VIEW view_report_slides AS
             FROM event_attachment
            WHERE event_id = event.event_id AND
                  attachment_type = 'slides')
-         AS slides_submitted,
-         (SELECT sum(pages)
-           FROM event_attachment
-          WHERE event_id = event.event_id AND
-                attachment_type = 'slides')
-         AS pages
+         AS slides_submitted
     FROM event
    WHERE event.event_state = 'accepted' AND
          event.event_state_progress = 'confirmed' AND
