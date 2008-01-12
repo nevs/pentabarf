ca511ae20102f2451b681e55451170a5be1f6464
Sven Klemm <sven@c3d2.de>
2008-01-12 01:02:58 +0000

    show speakers in feedback report

--- sql/views/report/report_feedback.sql
+++ sql/views/report/report_feedback.sql
@@ -5,6 +5,16 @@ CREATE OR REPLACE VIEW view_report_feedback AS
          title,
          subtitle,
          conference_track,
+         array_to_string(ARRAY(
+           SELECT view_person.name
+             FROM
+               event_person
+               INNER JOIN view_person USING (person_id)
+             WHERE
+               event_person.event_role IN ('speaker','moderator') AND
+               event_person.event_role_state = 'confirmed' AND
+               event_person.event_id = event.event_id
+         ), ', '::text) AS speakers,
          count(event_id) AS votes,
          count(event_feedback.remark) AS comments,
          coalesce( sum((participant_knowledge - 3) * 50 )/ count(participant_knowledge), 0) AS participant_knowledge,
@@ -14,6 +24,6 @@ CREATE OR REPLACE VIEW view_report_feedback AS
          coalesce( sum((audience_involvement - 3) * 50 )/ count(audience_involvement), 0) AS audience_involvement
     FROM event_feedback
          INNER JOIN event USING (event_id)
-   GROUP BY event_id, conference_id, title, subtitle, conference_track
+   GROUP BY event_id, conference_id, title, subtitle, conference_track,speakers
 ;
 
