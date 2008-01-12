c333392191f97436b27b63b06f67d693e9077e74
Sven Klemm <sven@c3d2.de>
2008-01-12 16:45:41 +0000

    drop changed views in upgrade script show real currently selected conference in conference selector

--- sql/maintenance/upgrade_0.3.8_to_0.3.9.sql
+++ sql/maintenance/upgrade_0.3.8_to_0.3.9.sql
@@ -17,6 +17,16 @@ INSERT INTO auth.object_domain(object,domain) VALUES ('language_localized','loca
 INSERT INTO auth.object_domain(object,domain) VALUES ('link_type_localized','localization');
 
 DROP VIEW view_review;
+DROP VIEW view_find_event;
+DROP VIEW view_report_feedback;
+DROP VIEW view_report_paper;
+DROP VIEW view_report_slides;
+DROP VIEW view_report_resources;
+DROP VIEW view_report_review;
+DROP VIEW view_schedule;;
+DROP VIEW view_own_events_coordinator;
+DROP VIEW view_own_events_participant;
+
 
 COMMIT;
 
