7a3b0dfe376dd25e8a19e290752fe04e44434543
Sven Klemm <sven@c3d2.de>
2008-01-12 16:30:00 +0000

    use format_event on paper report

--- sql/views/report/report_paper.sql
+++ sql/views/report/report_paper.sql
@@ -1,24 +1,47 @@
 
 CREATE OR REPLACE VIEW view_report_paper AS
-  SELECT event_id,
-         conference_id,
-         title,
-         subtitle,
-         paper,
-         (SELECT count(event_attachment_id)
-            FROM event_attachment
-           WHERE event_id = event.event_id AND
-                 attachment_type = 'paper')
-         AS paper_submitted,
-         (SELECT sum(pages)
-           FROM event_attachment
-          WHERE event_id = event.event_id AND
-                attachment_type = 'paper')
-         AS pages
-    FROM event
-   WHERE event.event_state = 'accepted' AND
-         event.event_state_progress = 'confirmed' AND
-         event.paper = 't'
-   ORDER BY lower(title), lower(subtitle)
+  SELECT 
+    event_id,
+    conference_id,
+    title,
+    subtitle,
+    array_to_string(ARRAY(
+      SELECT view_person.person_id
+        FROM
+          event_person
+          INNER JOIN view_person USING (person_id)
+        WHERE
+          event_person.event_role IN ('speaker','moderator') AND
+          event_person.event_role_state = 'confirmed' AND
+          event_person.event_id = event.event_id
+        ORDER BY view_person.name, event_person.person_id
+      ), E'\n'::text) AS speaker_ids,
+    array_to_string(ARRAY(
+      SELECT view_person.name
+        FROM
+          event_person
+          INNER JOIN view_person USING (person_id)
+        WHERE
+          event_person.event_role IN ('speaker','moderator') AND
+          event_person.event_role_state = 'confirmed' AND
+          event_person.event_id = event.event_id
+        ORDER BY view_person.name, event_person.person_id
+      ), E'\n'::text) AS speakers,
+    paper,
+    (SELECT count(event_attachment_id)
+       FROM event_attachment
+      WHERE event_id = event.event_id AND
+            attachment_type = 'paper')
+    AS paper_submitted,
+    (SELECT sum(pages)
+      FROM event_attachment
+     WHERE event_id = event.event_id AND
+           attachment_type = 'paper')
+    AS pages
+  FROM event
+  WHERE event.event_state = 'accepted' AND
+    event.event_state_progress = 'confirmed' AND
+    event.paper = 't'
+  ORDER BY lower(title), lower(subtitle)
 ;
 
