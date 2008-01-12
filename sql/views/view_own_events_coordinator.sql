cea7bfdc9a6e244f534e49841a0f498faa848daa
Sven Klemm <sven@c3d2.de>
2008-01-12 16:39:38 +0000

    show speaker and moderator of events on own_events page

--- sql/views/view_own_events_coordinator.sql
+++ sql/views/view_own_events_coordinator.sql
@@ -10,6 +10,28 @@ SELECT
   event_state_progress_localized.name AS event_state_progress_name,
   event.title,
   event.subtitle,
+  array_to_string(ARRAY(
+    SELECT view_person.person_id
+      FROM
+        event_person
+        INNER JOIN view_person USING (person_id)
+      WHERE
+        event_person.event_role IN ('speaker','moderator') AND
+        event_person.event_role_state = 'confirmed' AND
+        event_person.event_id = event.event_id
+      ORDER BY view_person.name, event_person.person_id
+    ), E'\n'::text) AS speaker_ids,
+  array_to_string(ARRAY(
+    SELECT view_person.name
+      FROM
+        event_person
+        INNER JOIN view_person USING (person_id)
+      WHERE
+        event_person.event_role IN ('speaker','moderator') AND
+        event_person.event_role_state = 'confirmed' AND
+        event_person.event_id = event.event_id
+      ORDER BY view_person.name, event_person.person_id
+    ), E'\n'::text) AS speakers,
   event_role_localized.event_role AS event_role,
   event_role_localized.name AS event_role_name,
   ''::text AS event_role_state,
