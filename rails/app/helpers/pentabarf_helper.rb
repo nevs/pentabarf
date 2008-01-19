dbf6639ae619a72e731d8cb7d9dbf3eafd51c06a
Sven Klemm <sven@c3d2.de>
2008-01-19 23:39:30 +0000

    added function change_url

--- rails/app/helpers/pentabarf_helper.rb
+++ rails/app/helpers/pentabarf_helper.rb
@@ -17,5 +17,30 @@ module PentabarfHelper
     xml
   end
 
+  def change_url( change )
+    klass = change.class.table
+    if klass.columns.key?(:conference_person_id)
+      cperson = Conference_person.select_single({:conference_person_id=>change.conference_person_id})
+      person = View_person.select_single({:person_id=>cperson.person_id})
+      link_title = person.name
+      link = url_for({:action=>:person,:id=>person.person_id})
+    elsif klass.columns.key?(:person_id)
+      person = View_person.select_single({:person_id=>change.person_id})
+      link_title = person.name
+      link = url_for({:action=>:person,:id=>person.person_id})
+    elsif klass.columns.key?(:event_id)
+      event = Event.select_single({:event_id=>change.event_id})
+      link_title = "#{event.title} #{event.subtitle}"
+      link = url_for({:action=>:event,:id=>event.event_id})
+    elsif klass.columns.key?(:conference_id)
+      conf = Conference.select_single({:conference_id=>change.conference_id})
+      link_title = conf.title
+      link = url_for({:action=>:conference,:id=>conf.conference_id})
+    else
+      raise "Could not determince change class for #{klass.table_name}"
+    end
+    [link,link_title]
+  end
+
 end
 
