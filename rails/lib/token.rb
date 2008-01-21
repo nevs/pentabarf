f5fee80626cdd86c846fdc5bb4a1d6cbfba4c3fb
Sven Klemm <sven@c3d2.de>
2008-01-21 01:30:16 +0000

    require digest/sha1 in Token class

--- rails/lib/token.rb
+++ rails/lib/token.rb
@@ -1,10 +1,12 @@
+
+require 'digest/sha1'
+
 # class that handles token generation
 class Token
   class << self
 
     def generate( url )
       salt = POPE.user.password[0..15] rescue ""
-#      puts "Token: URL: #{url} salt: '#{salt}'"
       Digest::SHA1.hexdigest( url + Digest::SHA1.hexdigest( salt ) )
     end
 
