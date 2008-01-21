fbdb58291bca12b9fe91f3b44e4645b04a195c7a
Sven Klemm <sven@c3d2.de>
2008-01-21 09:33:40 +0000

    minor changes to recent changes

--- rails/app/helpers/pentabarf_helper.rb
+++ rails/app/helpers/pentabarf_helper.rb
@@ -21,17 +21,21 @@ module PentabarfHelper
     klass = change.class.table
     if klass.columns.key?(:conference_person_id)
       cperson = Conference_person.select_single({:conference_person_id=>change.conference_person_id})
-      person = View_person.select_single({:person_id=>cperson.person_id})
-      link_title = person.name
-      link = url_for({:action=>:person,:id=>person.person_id})
-    elsif klass.columns.key?(:person_id)
-      person = View_person.select_single({:person_id=>change.person_id})
+      person = View_person.select_single({:person_id=>cperson.person_id.to_i})
       link_title = person.name
       link = url_for({:action=>:person,:id=>person.person_id})
     elsif klass.columns.key?(:event_id)
       event = Event.select_single({:event_id=>change.event_id})
       link_title = "#{event.title} #{event.subtitle}"
       link = url_for({:action=>:event,:id=>event.event_id})
+    elsif klass.columns.key?(:person_id)
+      begin
+        person = View_person.select_single({:person_id=>change.person_id.to_i})
+        link_title = person.name
+      rescue
+        link_title = change.public_name
+      end
+      link = url_for({:action=>:person,:id=>change.person_id})
     elsif klass.columns.key?(:conference_id)
       conf = Conference.select_single({:conference_id=>change.conference_id})
       link_title = conf.title
