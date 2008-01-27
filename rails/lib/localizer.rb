1a2af6a62f8511cbe529f79a15d72cc928b19d07
Sven Klemm <sven@c3d2.de>
2008-01-27 13:39:29 +0000

    automagically create new ui_message tags when missing

--- rails/lib/localizer.rb
+++ rails/lib/localizer.rb
@@ -15,6 +15,10 @@ class Localizer
           local = Ui_message_localized.select_single(:translated=>language,:ui_message=>tag).name
           @cache[language][tag] = local
         rescue Momomoto::Nothing_found
+          if POPE.permission?( :modify_localization )
+            ui_msg = Ui_message.select_or_new( :ui_message => tag )
+            ui_msg.write if ui_msg.new_record?
+          end
           # warn( "unlocalized tag: `#{tag}` for language #{language}" )
           local = tag.gsub('_',' ').capitalize
         end
