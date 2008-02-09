f0ff35ba983730a509d45fa64e77d94b332febcd
Sven Klemm <sven@c3d2.de>
2008-02-09 21:34:56 +0000

    add submission_visible and submission_settable to custom_fields interface

--- rails/app/controllers/admin_controller.rb
+++ rails/app/controllers/admin_controller.rb
@@ -29,7 +29,7 @@ class AdminController < ApplicationController
   end
 
   def save_custom_fields
-    write_rows( Custom_fields, params[:custom_fields], {:ignore_empty=>:field_name})
+    write_rows( Custom_fields, params[:custom_fields], {:ignore_empty=>:field_name,:always=>[:submission_visible,:submission_settable]})
     redirect_to( :action => :custom_fields )
   end
 
