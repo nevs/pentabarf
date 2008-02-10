646e79a9b1550497beb7e0bf4908c23dec8dd2c4
Sven Klemm <sven@c3d2.de>
2008-02-10 00:46:53 +0000

    add custom_fields_trigger function in upgrade script

--- sql/maintenance/upgrade_0.3.9_to_0.3.10.sql
+++ sql/maintenance/upgrade_0.3.9_to_0.3.10.sql
@@ -110,6 +110,41 @@ INSERT INTO auth.object_domain VALUES ('custom_person','person');
 INSERT INTO auth.object_domain VALUES ('ui_message','localization');
 INSERT INTO auth.object_domain VALUES ('conference_room_role', 'conference');
 
+CREATE OR REPLACE FUNCTION custom_field_trigger() RETURNS trigger AS $$
+  BEGIN
+    IF TG_OP = 'DELETE' THEN
+      IF OLD.field_name !~* '^[a-z_0-9]+$' OR OLD.field_name = 'conference_person_id' THEN
+        RAISE EXCEPTION 'Invalid field_name: %', OLD.field_name;
+      END IF;
+      PERFORM 1 FROM information_schema.columns WHERE table_name = 'custom_' || OLD.table_name AND table_schema = 'base' AND column_name = OLD.field_name;
+      IF FOUND THEN
+        EXECUTE 'ALTER TABLE base.' || quote_ident( 'custom_' || OLD.table_name ) || ' DROP COLUMN ' || quote_ident( OLD.field_name );
+      END IF;
+      RETURN OLD;
+    ELSIF TG_OP = 'INSERT' THEN
+      PERFORM 1 FROM information_schema.columns WHERE table_name = 'custom_' || NEW.table_name AND table_schema = 'base' AND column_name = NEW.field_name;
+      IF FOUND THEN
+        -- the fields seems to exist already
+        RETURN NEW;
+      END IF;
+      IF NEW.field_name !~* '^[a-z_0-9]+$' THEN
+        RAISE EXCEPTION 'Invalid field_name: %', NEW.field_name;
+      END IF;
+      IF NEW.field_type NOT IN ('text','boolean') THEN
+        RAISE EXCEPTION 'Invalid field_type: %', NEW.field_type;
+      END IF;
+      EXECUTE 'ALTER TABLE base.' || quote_ident( 'custom_' || NEW.table_name ) || ' ADD COLUMN ' || quote_ident( NEW.field_name ) || ' ' || NEW.field_type;
+    ELSIF TG_OP = 'UPDATE' THEN
+      IF OLD.table_name <> NEW.table_name THEN RAISE EXCEPTION 'Changing table is not allowed.'; END IF;
+      IF OLD.field_name <> NEW.field_name THEN RAISE EXCEPTION 'Changing field name is not allowed.'; END IF;
+      IF OLD.field_type <> NEW.field_type THEN RAISE EXCEPTION 'Changing field type is not allowed.'; END IF;
+    END IF;
+
+    PERFORM log.activate_logging();
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER custom_fields_trigger BEFORE INSERT OR UPDATE OR DELETE ON custom.custom_fields FOR EACH ROW EXECUTE PROCEDURE custom_field_trigger();
 
 ALTER TABLE base.conference_person ADD COLUMN reconfirmed BOOLEAN NOT NULL DEFAULT FALSE;
