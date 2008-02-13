3eb84800a0fb5742ce4e96c6780df45fc672e2fe
Sven Klemm <sven@c3d2.de>
2008-02-13 11:45:58 +0000

    bugfix for find person conference search

--- sql/views/find/find_person.sql
+++ sql/views/find/find_person.sql
@@ -14,24 +14,34 @@ CREATE OR REPLACE VIEW view_find_person AS
     mime_type.file_extension,
     conference.conference_id,
     conference_person.arrived,
-    ARRAY( 
-      SELECT event_person.event_role 
+    ARRAY(
+      SELECT event_person.event_role
       FROM event_person INNER JOIN event USING( event_id )
-      WHERE 
+      WHERE
         event_person.person_id = view_person.person_id AND
         event.conference_id = conference.conference_id
     ) AS event_role,
-    ARRAY( 
-      SELECT account_role.role 
-      FROM auth.account INNER JOIN auth.account_role USING(account_id) 
-      WHERE 
-        account.person_id = view_person.person_id 
+    ARRAY(
+      SELECT account_role.role
+      FROM auth.account INNER JOIN auth.account_role USING(account_id)
+      WHERE
+        account.person_id = view_person.person_id
     ) AS role
   FROM view_person
     CROSS JOIN conference
     LEFT OUTER JOIN conference_person USING (person_id,conference_id)
     LEFT OUTER JOIN person_image USING (person_id)
     LEFT OUTER JOIN mime_type USING (mime_type)
+  WHERE
+    conference_person.person_id IS NOT NULL OR
+    EXISTS(
+      SELECT 1 FROM
+        event_person
+        INNER JOIN event USING(event_id)
+      WHERE
+        event.conference_id = conference.conference_id AND
+        event_person.person_id = view_person.person_id
+    )
   ORDER BY view_person.name, view_person.person_id
 ;
 
