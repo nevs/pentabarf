9f780d5543cb26dd6f2632f08f598b837b49de44
Sven Klemm <sven@c3d2.de>
2008-02-13 22:38:02 +0000

    adjust activate_account to account_settings changes

--- sql/functions/auth/activate_account.sql
+++ sql/functions/auth/activate_account.sql
@@ -11,7 +11,8 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
       INSERT INTO auth.account_role(account_id, role) VALUES (cur_activation.account_id, 'submitter');
       SELECT INTO cur_person_id nextval(pg_get_serial_sequence('base.person','person_id'));
       INSERT INTO person(person_id,nickname,email) SELECT cur_person_id,login_name,email FROM auth.account WHERE account.account_id = cur_activation.account_id;
-      UPDATE auth.account SET person_id = cur_person_id, current_conference_id = cur_activation.conference_id WHERE account_id = cur_activation.account_id;
+      UPDATE auth.account SET person_id = cur_person_id WHERE account_id = cur_activation.account_id;
+      INSERT INTO auth.account_settings(account_id,current_conference_id) VALUES (cur_activation.account_id, cur_activation.conference_id);
       INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_person_id, now(), cur_person_id, 't');
       DELETE FROM auth.account_activation WHERE account_activation.activation_string = activation_string OR account_creation < now() + activation_interval;
     ELSE
