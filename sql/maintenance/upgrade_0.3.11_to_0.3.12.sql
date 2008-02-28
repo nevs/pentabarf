59b20a3e8df464f23e91d985c9efdcb682cb92e0
Sven Klemm <sven@c3d2.de>
2008-02-28 00:15:44 +0000

    enable f_submission_writable for conference where submission is enabled in upgrade script

--- sql/maintenance/upgrade_0.3.11_to_0.3.12.sql
+++ sql/maintenance/upgrade_0.3.11_to_0.3.12.sql
@@ -8,6 +8,7 @@ DELETE FROM log.log_transaction_involved_tables where table_name = 'ui_message';
 CREATE INDEX account_person_id_index ON auth.account(person_id);
 
 ALTER TABLE base.conference ADD COLUMN f_submission_writable BOOL NOT NULL DEFAULT FALSE;
+UPDATE conference SET f_submission_writable = TRUE WHERE f_submission_enabled = TRUE;
 
 SELECT log.activate_logging();
 
