b395d4eed1a8c44bc1c71a6f9419002568d73d7e
Sven Klemm <sven@c3d2.de>
2008-02-28 00:18:45 +0000

    add f_submission_writable to conference page in pentabarf controller

--- rails/app/controllers/pentabarf_controller.rb
+++ rails/app/controllers/pentabarf_controller.rb
@@ -71,7 +71,7 @@ class PentabarfController < ApplicationController
 
   def save_conference
     params[:conference][:conference_id] = params[:id] if params[:id].to_i > 0
-    conf = write_row( Conference, params[:conference], {:except=>[:conference_id],:always=>[:f_submission_enabled,:f_visitor_enabled,:f_feedback_enabled,:f_reconfirmation_enabled]} )
+    conf = write_row( Conference, params[:conference], {:except=>[:conference_id],:always=>[:f_submission_enabled,:f_submission_writable,:f_visitor_enabled,:f_feedback_enabled,:f_reconfirmation_enabled]} )
     custom_bools = Custom_fields.select({:table_name=>:conference,:field_type=>:boolean}).map(&:field_name)
     write_row( Custom_conference, params[:custom_conference], {:preset=>{:conference_id=>conf.conference_id},:always=>custom_bools})
     write_rows( Conference_day, params[:conference_day], {:preset=>{:conference_id => conf.conference_id},:always=>[:public],:ignore_empty=>:conference_day})
