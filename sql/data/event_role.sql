709b7ab05f11d89d2307e4c4c673cfe63b4077e2
Sven Klemm <sven@c3d2.de>
2008-03-29 21:35:25 +0000

    update initial data for event_role

--- sql/data/event_role.sql
+++ sql/data/event_role.sql
@@ -2,11 +2,11 @@
 SET client_encoding = 'UTF8';
 SET search_path = public, pg_catalog;
 
-INSERT INTO event_role (event_role, rank) VALUES ('attendee', NULL);
-INSERT INTO event_role (event_role, rank) VALUES ('coordinator', 3);
-INSERT INTO event_role (event_role, rank) VALUES ('moderator', 2);
-INSERT INTO event_role (event_role, rank) VALUES ('reporter', NULL);
-INSERT INTO event_role (event_role, rank) VALUES ('reviewer', NULL);
-INSERT INTO event_role (event_role, rank) VALUES ('speaker', 1);
-INSERT INTO event_role (event_role, rank) VALUES ('submitter', NULL);
-INSERT INTO event_role (event_role, rank) VALUES ('visitor', 4);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('attendee', NULL, true);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('coordinator', 3, false);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('moderator', 2, false);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('reporter', NULL, true);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('reviewer', NULL, false);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('speaker', 1, true);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('submitter', NULL, false);
+INSERT INTO event_role (event_role, rank, participant) VALUES ('visitor', 4, true);
