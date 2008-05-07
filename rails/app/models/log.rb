59d4484664d9aedd064efb9f4ab3cfdce1e5fc53
Sven Klemm <sven@c3d2.de>
2008-05-07 21:04:55 +0000

    copy custom methods from base class to log class

--- rails/app/models/log.rb
+++ rails/app/models/log.rb
@@ -4,6 +4,12 @@ module Log
   def self.const_missing( table )
     klass = Class.new( Momomoto::Table )
     klass.schema_name = "log"
+
+    # copy custom methods from base class to log class if available
+    if "::#{table}".constantize.const_defined?('Methods')
+      klass.const_set(:Methods, "::#{table}::Methods".constantize)
+    end
+
     Log.const_set(table, klass)
   end
 
