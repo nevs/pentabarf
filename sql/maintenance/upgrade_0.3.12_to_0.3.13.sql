6db58a7fa42a56fab1bd2480b765d648e376abca
Sven Klemm <sven@c3d2.de>
2008-05-07 21:40:13 +0000

    remove logging for person_availability

--- sql/maintenance/upgrade_0.3.12_to_0.3.13.sql
+++ sql/maintenance/upgrade_0.3.12_to_0.3.13.sql
@@ -4,5 +4,8 @@ BEGIN;
 ALTER TABLE base.event_role ADD COLUMN participant BOOL NOT NULL DEFAULT FALSE;
 UPDATE event_role set participant = TRUE WHERE event_role IN ('speaker','visitor','reporter','attendee');
 
+DROP TABLE log.person_availability;
+SELECT log.activate_logging();
+
 COMMIT;
 
