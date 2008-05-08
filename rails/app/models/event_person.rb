d4b858fc9360bdda7886fd8d72c8c6deec06a30c
Sven Klemm <sven@c3d2.de>
2008-05-08 08:50:32 +0000

    beautify logging for event_person

--- rails/app/models/event_person.rb
+++ rails/app/models/event_person.rb
@@ -4,6 +4,16 @@ class Event_person < Momomoto::Table
     columns.keys - [:event_person_id]
   end
 
+  def self.log_change_title( change )
+    conf = Conference.select_single(:conference_id=>change.conference_id).acronym rescue nil
+    title = Event.select_single({:event_id=>change.event_id}).title rescue ""
+    if conf
+      "#{conf}: #{title}"
+    else
+      "#{title}"
+    end
+  end
+
   def self.log_change_url( change )
     {:controller=>'pentabarf',:action=>:event,:id=>change.event_id}
   end
