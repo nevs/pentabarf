64e9840131700009c915f4e37f9d996b5de3996d
Sven Klemm <sven@c3d2.de>
2008-05-08 18:59:45 +0000

    nicer recent changes for event_attachment

--- rails/app/models/event_attachment.rb
+++ rails/app/models/event_attachment.rb
@@ -1,7 +1,19 @@
 class Event_attachment < Momomoto::Table
 
+  def self.log_content_columns
+    columns.keys - [:event_id]
+  end
+
   def self.log_hidden_columns
     [:data]
+  end
+
+  def self.log_change_url( change )
+    {:controller=>'pentabarf',:action=>:event,:id=>change.event_id}
+  end
+
+  def self.log_change_title( change )
+    Event.select_single({:event_id=>change.event_id}).title rescue ""
   end
 
 end
