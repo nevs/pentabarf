1fe8752f529e2d38cbae1dec66aaa144f7bd910e
Sven Klemm <sven@c3d2.de>
2008-05-08 08:27:08 +0000

    nicer logging for ui_message_localized

--- rails/app/models/ui_message_localized.rb
+++ rails/app/models/ui_message_localized.rb
@@ -1,3 +1,16 @@
 class Ui_message_localized < Momomoto::Table
+
+  def self.log_change_title( change )
+    change.ui_message
+  end
+
+  def self.log_change_url( change )
+    {:controller=>'localization',:action=>:ui_message,:id=>change.ui_message}
+  end
+
+  def self.log_content_columns
+    columns.keys - [:ui_message]
+  end
+
 end
 
