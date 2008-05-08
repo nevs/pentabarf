1a15388659df087b175fa27e1dc1d5bb63dc232a
Sven Klemm <sven@c3d2.de>
2008-05-08 10:26:17 +0000

    drop log trigger for person_availability in upgrade script

--- sql/maintenance/upgrade_0.3.12_to_0.3.13.sql
+++ sql/maintenance/upgrade_0.3.12_to_0.3.13.sql
@@ -5,7 +5,7 @@ ALTER TABLE base.event_role ADD COLUMN participant BOOL NOT NULL DEFAULT FALSE;
 UPDATE event_role set participant = TRUE WHERE event_role IN ('speaker','visitor','reporter','attendee');
 
 DROP TABLE log.person_availability;
-SELECT log.activate_logging();
+DROP TRIGGER person_availability_log_trigger ON person_availability;
 
 COMMIT;
 
