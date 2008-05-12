09aca6216945253542c2681db5c21952e20a3907
Sven Klemm <sven@c3d2.de>
2008-05-12 15:04:30 +0000

    nicer log display for account_role

--- rails/app/models/account_role.rb
+++ rails/app/models/account_role.rb
@@ -1,4 +1,28 @@
 class Account_role < Momomoto::Table
   schema_name "auth"
+
+  def self.log_content_columns
+    columns.keys - [:account_id]
+  end
+
+  def self.log_change_url( change )
+    account = Account.select_single({:account_id=>change.account_id})
+    {:controller=>'pentabarf',:action=>:person,:id=>account.person_id}
+   rescue
+    {}
+  end
+
+  def self.log_change_title( change )
+    account = Account.select_single({:account_id=>change.account_id})
+    begin
+      person = Person.select_single({:person_id=>account.person_id})
+      person.name
+    rescue
+      account.login_name
+    end
+   rescue
+    change.account_id
+  end
+
 end
 
