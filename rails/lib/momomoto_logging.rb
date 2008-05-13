a05cefa23dd0f5ab575c770d6c6f7fc1d81ecb0f
Sven Klemm <sven@c3d2.de>
2008-05-13 12:04:30 +0000

    better default for default url of log entries

--- rails/lib/momomoto_logging.rb
+++ rails/lib/momomoto_logging.rb
@@ -18,7 +18,17 @@ module Momomoto
       end
 
       def log_change_url( change )
-        {}
+        if change.respond_to?(:conference_id)
+          {:controller=>'pentabarf',:action=>:conference,:id=>change.conference_id}
+        elsif change.respond_to?(:event_id)
+          {:controller=>'pentabarf',:action=>:event,:id=>change.event_id}
+        elsif change.respond_to?(:person_id)
+          {:controller=>'pentabarf',:action=>:person,:id=>change.person_id}
+        elsif change.class.table.table_name.match(/_localized$/)
+          {:controller=>'localization',:action=>change.class.table.table_name.gsub(/_localized$/,'')}
+        else
+          {}
+        end
       end
 
     end
