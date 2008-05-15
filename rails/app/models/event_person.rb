47e784f79c90519d5bfdd71e489d6ff52fd4d57a
Sven Klemm <sven@c3d2.de>
2008-05-15 19:14:32 +0000

    change log format for event_person

--- rails/app/models/event_person.rb
+++ rails/app/models/event_person.rb
@@ -5,7 +5,7 @@ class Event_person < Momomoto::Table
   end
 
   def self.log_change_title( change )
-    Event.log_change_title( Event.select_single({:event_id=>change.event_id}))
+    "#{Event.log_change_title( Event.select_single({:event_id=>change.event_id}))}: #{Person.log_change_title( Person.select_single(:person_id=>change.person_id))}"
    rescue
     ""
   end
