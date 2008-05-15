6d3de674ebcfdc66630cf43c74434f598e80a842
Sven Klemm <sven@c3d2.de>
2008-05-15 10:39:39 +0000

    add nicer urls and titles for person_language to recent changes

--- rails/app/models/person_language.rb
+++ rails/app/models/person_language.rb
@@ -1,3 +1,19 @@
 class Person_language < Momomoto::Table
+
+  def self.log_content_columns
+    columns.keys - [:person_id]
+  end
+
+  def self.log_change_title( change )
+    person = Person.select_single({:person_id=>change.person_id})
+    person.name
+   rescue
+    ""
+  end
+
+  def self.log_change_url( change )
+    {:controller=>'pentabarf',:action=>:person,:id=>change.person_id}
+  end
+
 end
 
