5a243238b325bc06c2cc58670c676a65291f9e55
Sven Klemm <sven@c3d2.de>
2008-08-25 13:06:49 +0000

    add unique key to conference_track in upgrade script

--- sql/maintenance/upgrade_0.3.14_to_0.3.15.sql
+++ sql/maintenance/upgrade_0.3.14_to_0.3.15.sql
@@ -12,6 +12,7 @@ ALTER TABLE public.event DROP CONSTRAINT event_conference_track_fkey;
 ALTER TABLE public.conference_track DROP CONSTRAINT conference_track_pkey1;
 
 ALTER TABLE public.conference_track ADD CONSTRAINT conference_track_pkey1 PRIMARY KEY( conference_track_id );
+ALTER TABLE public.conference_track ADD CONSTRAINT conference_track_uniq UNIQUE( conference_id, conference_track );
 ALTER TABLE public.event ADD CONSTRAINT event_conference_track_id_fkey FOREIGN KEY (conference_track_id) REFERENCES conference_track (conference_track_id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 ALTER TABLE base.event DROP COLUMN conference_track CASCADE;
