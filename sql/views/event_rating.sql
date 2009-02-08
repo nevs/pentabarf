5081106111f62da649b049060aba8c8688274465
Sven Klemm <sven@c3d2.de>
2009-02-08 22:25:59 +0000

    bugfix for view_event_rating

--- sql/views/event_rating.sql
+++ sql/views/event_rating.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE VIEW view_event_rating AS
   SELECT
     event_rating_remark.event_id,
     event_rating_remark.person_id,
+    view_person.name,
     event_rating_remark.remark,
     event_rating_remark.eval_time
   FROM
@@ -11,14 +12,16 @@ CREATE OR REPLACE VIEW view_event_rating AS
   UNION SELECT
     event_rating.event_id,
     event_rating.person_id,
+    view_person.name,
     NULL::text AS remark,
     max(event_rating.eval_time) AS eval_time
   FROM
     event_rating
+    INNER JOIN view_person USING (person_id)
   WHERE
     NOT EXISTS( SELECT 1 FROM event_rating_remark WHERE event_rating_remark.event_id = event_rating.event_id ) AND
     NOT EXISTS( SELECT 1 FROM event_rating_remark WHERE event_rating_remark.person_id = event_rating.person_id )
-  GROUP BY event_id,person_id
+  GROUP BY event_id,person_id,name
   ORDER BY eval_time DESC
 ;
 
