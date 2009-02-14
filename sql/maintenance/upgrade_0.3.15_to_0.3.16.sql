ad37d832583b79b3b2bd84024b0a8930d8fcfc27
Sven Klemm <sven@c3d2.de>
2009-02-14 14:13:26 +0000

    add upgrade script for 0.3.16

---
+++ sql/maintenance/upgrade_0.3.15_to_0.3.16.sql
@@ -0,0 +1,29 @@
+
+CREATE SCHEMA release;
+
+CREATE TABLE base.release (
+  conference_release_id INTEGER
+);
+
+CREATE TABLE base.conference_release (
+  conference_release_id SERIAL,
+  conference_id INTEGER NOT NULL,
+  conference_release TEXT NOT NULL
+);
+
+CREATE TABLE conference_release (
+  FOREIGN KEY( conference_id ) REFERENCES conference( conference_id ) ON UPDATE CASCADE ON DELETE CASCADE,
+  PRIMARY KEY( conference_release_id )
+  UNIQUE( conference_id, conference_release )
+) INHERITS( base.conference_release );
+
+CREATE TABLE log.conference_release (
+) INHERITS( base.logging, base.conference_release );
+
+SELECT log.activate_logging();
+
+CREATE TABLE release.conference (
+  FOREIGN KEY (conference_release_id) REFERENCES conference_release ON UPDATE CASCADE ON DELETE CASCADE,
+  PRIMARY KEY (conference_release_id,conference_id)
+) INHERITS( base.release, base.conference );
+
