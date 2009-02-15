e291cb08f1342471dbb8046e2d312d2a63d5e0d6
Sven Klemm <sven@c3d2.de>
2009-02-15 19:45:37 +0000

    add trigger that copies release relevant data into release tables

--- sql/activate_trigger.sql
+++ sql/activate_trigger.sql
@@ -3,3 +3,5 @@ CREATE TRIGGER related_event_after_insert AFTER INSERT ON event_related FOR STAT
 
 CREATE TRIGGER related_event_after_delete AFTER DELETE ON event_related FOR STATEMENT EXECUTE PROCEDURE event_related_trigger_delete();
 
+CREATE TRIGGER conference_release_after_insert AFTER INSERT ON conference_release FOR EACH ROW EXECUTE PROCEDURE conference_release_trigger_insert();
+
--- sql/trigger.sql
+++ sql/trigger.sql
@@ -35,3 +35,30 @@ CREATE OR REPLACE FUNCTION event_related_trigger_delete() RETURNS TRIGGER AS $$
    END;
 $$ LANGUAGE 'plpgsql';
 
+-- trigger to copy all release relevant data when a new release is done
+CREATE OR REPLACE FUNCTION conference_release_trigger_insert() RETURNS TRIGGER AS $$
+   BEGIN
+      -- conference tables
+      INSERT INTO release.conference SELECT NEW.conference_release_id, conference.* FROM conference WHERE conference.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_image SELECT NEW.conference_release_id, conference_image.* FROM conference_image WHERE conference_image.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_link SELECT NEW.conference_release_id, conference_link.* FROM conference_link WHERE conference_link.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_room SELECT NEW.conference_release_id, conference_room.* FROM conference_room WHERE conference_room.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_track SELECT NEW.conference_release_id, conference_track.* FROM conference_track WHERE conference_track.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_day SELECT NEW.conference_release_id, conference_day.* FROM conference_day WHERE conference_day.conference_id = NEW.conference_id;
+      -- event tables
+      INSERT INTO release.event SELECT NEW.conference_release_id, event.* FROM event WHERE event.conference_id = NEW.conference_id;
+      INSERT INTO release.event_attachment SELECT NEW.conference_release_id, event_attachment.* FROM event_attachment WHERE event_attachment.event_id IN (SELECT event_id FROM release.event WHERE event.conference_release_id = NEW.conference_release_id );
+      INSERT INTO release.event_image SELECT NEW.conference_release_id, event_image.* FROM event_image WHERE event_image.event_id IN (SELECT event_id FROM release.event WHERE event.conference_release_id = NEW.conference_release_id );
+      INSERT INTO release.event_link SELECT NEW.conference_release_id, event_link.* FROM event_link WHERE event_link.event_id IN (SELECT event_id FROM release.event WHERE event.conference_release_id = NEW.conference_release_id );
+      INSERT INTO release.event_person SELECT NEW.conference_release_id, event_person.* FROM event_person WHERE event_person.event_id IN (SELECT event_id FROM release.event WHERE event.conference_release_id = NEW.conference_release_id );
+
+      -- person tables
+      INSERT INTO release.person SELECT NEW.conference_release_id, person.* FROM person WHERE person.person_id IN (SELECT person_id FROM release.event_person WHERE event_person.conference_release_id = NEW.conference_release_id );
+      INSERT INTO release.conference_person SELECT NEW.conference_release_id, conference_person.* FROM conference_person WHERE person_id IN (SELECT person_id FROM release.person WHERE person.conference_release_id = NEW.conference_release_id) AND conference_person.conference_id = NEW.conference_id;
+      INSERT INTO release.conference_person_link SELECT NEW.conference_release_id, conference_person_link.* FROM conference_person_link WHERE conference_person_id IN( SELECT conference_person_id FROM release.conference_person WHERE conference_person.conference_release_id = NEW.conference_release_id );
+      INSERT INTO release.person_image SELECT NEW.conference_release_id, person_image.* FROM person_image WHERE person_id IN (SELECT person_id FROM release.person WHERE person.conference_release_id = NEW.conference_release_id);
+
+      RETURN NEW;
+   END;
+$$ LANGUAGE 'plpgsql';
+
