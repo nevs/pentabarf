b64a1eb7f5587ae512d70664f0b288816e6fed0a
Sven Klemm <sven@c3d2.de>
2009-02-23 12:07:53 +0000

    add days, rooms and tracks helper methods to conference model

--- rails/app/models/conference.rb
+++ rails/app/models/conference.rb
@@ -1,6 +1,22 @@
 class Conference < Momomoto::Table
   default_order( Momomoto.lower(:acronym) )
 
+  module Methods
+
+    def days( conditions = {}, options = {} )
+      Conference_day.select( conditions.merge( {:conference_id=>conference_id} ), options )
+    end
+
+    def rooms( conditions = {}, options = {} )
+      Conference_room.select( conditions.merge( {:conference_id=>conference_id} ), options )
+    end
+
+    def tracks( conditions = {}, options = {} )
+      Conference_track.select( conditions.merge( {:conference_id=>conference_id} ), options )
+    end
+
+  end
+
   def self.log_content_columns
     columns.keys - [:conference_id]
   end
