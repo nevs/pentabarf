cedccd009eb518fbec6118618984f5cf9f0a4710
Sven Klemm <sven@c3d2.de>
2009-03-06 19:59:40 +0000

    port the conference_room_role definition fix to trunk and 0.3.15

--- sql/tables/conference_room_role.sql
+++ sql/tables/conference_room_role.sql
@@ -1,14 +1,16 @@
 
 CREATE TABLE base.conference_room_role (
+  conference_id INTEGER NOT NULL,
   conference_room_id INTEGER NOT NULL,
   event_role TEXT NOT NULL,
   amount INTEGER NOT NULL DEFAULT 1
 );
 
 CREATE TABLE conference_room_role (
+  FOREIGN KEY (conference_id) REFERENCES conference (conference_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (conference_room_id) REFERENCES conference_room (conference_room_id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (event_role) REFERENCES event_role (event_role) ON UPDATE CASCADE ON DELETE CASCADE,
-  PRIMARY KEY (conference_id, conference_room, event_role)
+  PRIMARY KEY (conference_id, conference_room_id, event_role)
 ) INHERITS( base.conference_room_role );
 
 CREATE TABLE log.conference_room_role (
