ad2e9e65b260d7ecb80d424aec190886bc606a2c
Sven Klemm <sven@c3d2.de>
2009-03-14 21:07:14 +0000

    make release model try to require missing files

--- rails/app/models/release.rb
+++ rails/app/models/release.rb
@@ -2,15 +2,24 @@
 module Release
 
   def self.const_missing( table )
-    klass = Class.new( Momomoto::Table )
-    klass.schema_name = "release"
+    table_name = table.to_s.downcase
+    raise "Invalid class name" unless table_name.match(/^[a-z_]+$/)
+    begin
+      raise LoadError
+      require "release/#{table_name}"
+      Release.const_get(table)
+    rescue LoadError
+      klass = Class.new( Momomoto::Table )
+      klass.schema_name = "release"
+      klass.table_name = table_name
 
-    # copy custom methods from base class to release class if available
-    if "::#{table}".constantize.const_defined?('Methods')
-      klass.const_set(:Methods, "::#{table}::Methods".constantize)
+      # copy custom methods from base class to release class if available
+      # if "::#{table}".constantize.const_defined?('Methods')
+      #   klass.const_set(:Methods, "::#{table}::Methods".constantize)
+      # end
+
+      Release.const_set(table, klass)
     end
-
-    Release.const_set(table, klass)
   end
 
 end
