6b013bb41b379fa04361d593e0b723fdb0af667f
Sven Klemm <sven@c3d2.de>
2009-03-15 10:40:21 +0000

    disable autorequire for Release module because it doesn't work in development mode

--- rails/app/models/release.rb
+++ rails/app/models/release.rb
@@ -1,26 +1,27 @@
 
 module Release
 
-  def self.const_missing( table )
-    table_name = table.to_s.downcase
-    raise "Invalid class name" unless table_name.match(/^[a-z_]+$/)
-    begin
-      require "release/#{table_name}"
-      raise LoadError if not local_constants.member?( table.to_s )
-      Release.const_get(table)
-    rescue LoadError
-      klass = Class.new( Momomoto::Table )
-      klass.schema_name = "release"
-      klass.table_name = table_name
-
-      # copy custom methods from base class to release class if available
-      # if "::#{table}".constantize.const_defined?('Methods')
-      #   klass.const_set(:Methods, "::#{table}::Methods".constantize)
-      # end
-
-      Release.const_set(table, klass)
-    end
-  end
+#FIXME: disabled for now because it doesn't work in development mode
+#  def self.const_missing( table )
+#    table_name = table.to_s.downcase
+#    raise "Invalid class name" unless table_name.match(/^[a-z_]+$/)
+#    begin
+#      require "release/#{table_name}"
+#      raise LoadError if not local_constants.member?( table.to_s )
+#      Release.const_get(table)
+#    rescue LoadError
+#      klass = Class.new( Momomoto::Table )
+#      klass.schema_name = "release"
+#      klass.table_name = table_name
+#
+#      # copy custom methods from base class to release class if available
+#      # if "::#{table}".constantize.const_defined?('Methods')
+#      #   klass.const_set(:Methods, "::#{table}::Methods".constantize)
+#      # end
+#
+#      Release.const_set(table, klass)
+#    end
+#  end
 
 end
 
