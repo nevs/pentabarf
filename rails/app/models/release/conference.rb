f245cf3b62c01b4a2d8e81acc844f3a046d88b0e
Sven Klemm <sven@c3d2.de>
2009-03-17 22:32:48 +0000

    add persons helper function to conference rename speakers helper function to persons on Release::View_schedule_event

--- rails/app/models/release/conference.rb
+++ rails/app/models/release/conference.rb
@@ -5,6 +5,7 @@ class Release::Conference < Momomoto::Table
   fk_helper_single :release, Conference_release, [:conference_id,:conference_release_id]
 
   fk_helper_multiple :events, Release::View_schedule_event, [:conference_id,:conference_release_id]
+  fk_helper_multiple :persons, Release::View_schedule_person, [:conference_id,:conference_release_id]
   fk_helper_multiple :days, Release::View_schedule_day, [:conference_id,:conference_release_id]
   fk_helper_multiple :rooms, Release::View_schedule_room, [:conference_id,:conference_release_id]
   fk_helper_multiple :tracks, Release::View_schedule_track, [:conference_id,:conference_release_id]
--- rails/app/models/release/view_schedule_event.rb
+++ rails/app/models/release/view_schedule_event.rb
@@ -2,7 +2,7 @@ class Release::View_schedule_event < Momomoto::Table
 
   schema_name "release"
 
-  fk_helper_multiple :speakers, Release::View_schedule_event_person, [:conference_release_id,:event_id,:translated]
+  fk_helper_multiple :persons, Release::View_schedule_event_person, [:conference_release_id,:event_id,:translated]
 
 end
 
