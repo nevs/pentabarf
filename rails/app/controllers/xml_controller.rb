47915f3d4b0b183889dd836430cb1f417f7170f1
Sven Klemm <sven@c3d2.de>
2009-03-18 11:07:35 +0000

    adjust xml controller to schedule changes

--- rails/app/controllers/xml_controller.rb
+++ rails/app/controllers/xml_controller.rb
@@ -3,9 +3,20 @@ class XmlController < ApplicationController
   before_filter :init
 
   def schedule
-    @conference = Conference.select_single({:acronym => params[:conference]})
-    @rooms = Conference_room.select({:conference_id=>@conference.conference_id, :public=>'t'},{:order=>:rank})
-    @events = View_schedule_event.select({:conference_id=>@conference.conference_id,:translated=>@current_language})
+    if params[:preview]
+      # if preview is specified we work on live data
+      @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+    elsif params[:release]
+      # if a specific release is selected we show that one
+      @conference = Release::Conference.select_single({:acronym=>params[:conference],:conference_release_id=>params[:release]})
+    else
+      # otherwise we show the latest release with fallback to live data if nothing has been released yet
+      begin
+        @conference = Release::Conference.select_single({:acronym=>params[:conference]},{:limit=>1,:order=>Momomoto.desc(:conference_release_id)})
+      rescue Momomoto::Nothing_found
+        @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+      end
+    end
   end
 
   protected
