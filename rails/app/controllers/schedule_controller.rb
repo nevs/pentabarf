d9dc6709f5b28d4a43e7a3eeb00db6a7f4889bb8
Sven Klemm <sven@c3d2.de>
2009-03-18 11:06:46 +0000

    use latest release in schedule controller with fallback to live preview if nothing has been released yet

--- rails/app/controllers/schedule_controller.rb
+++ rails/app/controllers/schedule_controller.rb
@@ -63,7 +63,20 @@ class ScheduleController < ApplicationController
   protected
 
   def init
-    @conference = Release::Conference.select_single({:acronym=>params[:conference]},{:limit=>1,:order=>Momomoto.desc(:conference_release_id)})
+    if params[:preview]
+      # if preview is specified we work on live data
+      @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+    elsif params[:release]
+      # if a specific release is selected we show that one
+      @conference = Release::Conference.select_single({:acronym=>params[:conference],:conference_release_id=>params[:release]})
+    else
+      # otherwise we show the latest release with fallback to live data if nothing has been released yet
+      begin
+        @conference = Release::Conference.select_single({:acronym=>params[:conference]},{:limit=>1,:order=>Momomoto.desc(:conference_release_id)})
+      rescue Momomoto::Nothing_found
+        @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+      end
+    end
     @current_language = params[:language] || 'en'
   end
 
