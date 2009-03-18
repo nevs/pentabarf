06972ca61ed95d7d5a18e40f93538aa39119ac08
Sven Klemm <sven@c3d2.de>
2009-03-18 11:09:36 +0000

    add links helper method to View_schedule_event and Release::View_schedule_event

--- rails/app/models/release/view_schedule_event.rb
+++ rails/app/models/release/view_schedule_event.rb
@@ -2,6 +2,7 @@ class Release::View_schedule_event < Momomoto::Table
 
   schema_name "release"
 
+  fk_helper_multiple :links, Release::Event_link, [:conference_release_id,:event_id]
   fk_helper_multiple :persons, Release::View_schedule_event_person, [:conference_release_id,:event_id,:translated]
 
 end
--- rails/app/models/view_schedule_event.rb
+++ rails/app/models/view_schedule_event.rb
@@ -1,6 +1,7 @@
 class View_schedule_event < Momomoto::Table
   default_order( Momomoto::lower(:title,:subtitle))
 
+  fk_helper_multiple :links, Event_link, [:event_id]
   fk_helper_multiple :persons, View_schedule_event_person, [:event_id,:translated]
 
 end
