839385ce8458a0c750df0c0be2d86c20c1cbb4af
Sven Klemm <sven@c3d2.de>
2009-03-24 11:55:30 +0000

    activate custom_field_trigger on install

--- sql/activate_trigger.sql
+++ sql/activate_trigger.sql
@@ -5,3 +5,5 @@ CREATE TRIGGER related_event_after_delete AFTER DELETE ON event_related FOR STAT
 
 CREATE TRIGGER conference_release_after_insert AFTER INSERT ON conference_release FOR EACH ROW EXECUTE PROCEDURE conference_release_trigger_insert();
 
+CREATE TRIGGER custom_fields_trigger BEFORE INSERT OR UPDATE OR DELETE ON custom.custom_fields FOR EACH ROW EXECUTE PROCEDURE custom_field_trigger();
+
