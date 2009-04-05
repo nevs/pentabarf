a9fd42651ecb20abb9295578832ff6c13579953b
Sven Klemm <sven@c3d2.de>
2009-04-05 16:52:07 +0000

    bugfix for view_schedule_person

--- sql/views/schedule/view_schedule_person.sql
+++ sql/views/schedule/view_schedule_person.sql
@@ -3,23 +3,29 @@ CREATE OR REPLACE VIEW view_schedule_person AS
     person.person_id,
     coalesce(person.public_name, coalesce(person.first_name || ' ', '') || person.last_name, person.nickname) AS name,
     person.email,
-    conference_person.conference_id,
+    conference.conference_id,
     conference_person.conference_person_id,
     conference_person.abstract,
     conference_person.description
   FROM
     person
-    INNER JOIN conference_person USING(person_id)
+    CROSS JOIN conference
+    LEFT OUTER JOIN conference_person USING(conference_id,person_id)
   WHERE
     EXISTS (
       SELECT 1
       FROM
         event_person
-        INNER JOIN event ON( event.public = TRUE AND event.event_state = 'accepted' AND event.event_state_progress = 'reconfirmed' )
+        INNER JOIN event ON( 
+          event.event_id = event_person.event_id AND
+          event.public = TRUE AND
+          event.event_state = 'accepted' AND 
+          event.event_state_progress = 'reconfirmed' )
         INNER JOIN view_schedule_day USING(conference_day_id)
         INNER JOIN view_schedule_room USING(conference_room_id)
       WHERE
         event_person.person_id = person.person_id AND
+        event.conference_id = conference.conference_id AND
         event_person.event_role IN ('speaker','moderator') AND
         event_person.event_role_state = 'confirmed'
     )
