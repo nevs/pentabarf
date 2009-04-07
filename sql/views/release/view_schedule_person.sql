6f52a74076520ce55f2517c72fa97f2bd1e6a4cd
Sven Klemm <sven@c3d2.de>
2009-04-07 13:05:03 +0000

    bugfix for release.view_schedule_person

--- sql/views/release/view_schedule_person.sql
+++ sql/views/release/view_schedule_person.sql
@@ -10,7 +10,8 @@ CREATE OR REPLACE VIEW release.view_schedule_person AS
     conference_person.description
   FROM
     release.person
-    INNER JOIN release.conference_person USING(conference_release_id,person_id)
+    INNER JOIN release.conference USING(conference_release_id)
+    LEFT OUTER JOIN release.conference_person USING(conference_release_id,person_id)
   WHERE
     EXISTS (
       SELECT 1
@@ -25,7 +26,7 @@ CREATE OR REPLACE VIEW release.view_schedule_person AS
         event_person.event_role IN ('speaker','moderator') AND
         event_person.event_role_state = 'confirmed' AND
         event.public = TRUE AND
-        event.event_state = 'accepted' AND 
+        event.event_state = 'accepted' AND
         event.event_state_progress = 'reconfirmed'
     )
   ORDER BY coalesce(person.public_name, coalesce(person.first_name || ' ', '') || person.last_name, person.nickname)
