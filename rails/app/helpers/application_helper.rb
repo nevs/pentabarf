487795161a6730c0c696a5035623b7b215e373f0
Sven Klemm <sven@c3d2.de>
2009-04-19 10:02:44 +0000

    set pentabarf version to 0.3.18

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   TableRowColors = ['khaki', 'plum', 'lightgreen', 'skyblue', 'silver', 'moccasin', 'rosybrown', 'salmon', 'sandybrown']
 
   def pentabarf_version
-    "0.3.17"
+    "0.3.18"
   end
 
   # tries to read the current revision of pentabarf from subversion meta data
