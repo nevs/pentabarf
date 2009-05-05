333ebce6747a8e71eac0c5f9167af3ef4c6de843
Sven Klemm <sven@c3d2.de>
2009-05-05 19:35:34 +0000

    change account_conference_permissions to return setof auth.conference_permission

--- sql/functions/auth/account_conference_permissions.sql
+++ sql/functions/auth/account_conference_permissions.sql
@@ -1,12 +1,11 @@
 
 -- returns list of permissions an account has in a certain conference
 
-CREATE OR REPLACE FUNCTION auth.account_conference_permissions( account_id INTEGER, conference_id INTEGER ) RETURNS SETOF TEXT AS $$
-  SELECT DISTINCT permission
+CREATE OR REPLACE FUNCTION auth.account_conference_permissions( account_id INTEGER ) RETURNS SETOF auth.conference_permission AS $$
+  SELECT DISTINCT conference_id, permission
   FROM auth.account_conference_role
     INNER JOIN auth.conference_role_permission USING( conference_role )
   WHERE
-    account_conference_role.account_id = $1 AND 
-    account_conference_role.conference_id = $2;
+    account_conference_role.account_id = $1;
 $$ LANGUAGE SQL;
 
