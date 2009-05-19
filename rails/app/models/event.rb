d2cd675a375af5c0623ce0b40a91cd1a0cb286ae
Sven Klemm <sven@c3d2.de>
2009-05-19 20:35:19 +0000

    link to event controller in recent changes

--- rails/app/models/event.rb
+++ rails/app/models/event.rb
@@ -14,7 +14,7 @@ class Event < Momomoto::Table
   end
 
   def self.log_change_url( change )
-    {:controller=>'pentabarf',:action=>:event,:id=>change.event_id}
+    {:controller=>'event',:action=>:edit,:event_id=>change.event_id}
   end
 
   def self.log_change_title( change )
