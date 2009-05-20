b24f1a195734b4c3789257a15dc182906c7d7baf
Sven Klemm <sven@c3d2.de>
2009-05-20 13:09:19 +0000

    remove event and save_event method from pentabarf_controller

--- rails/app/controllers/pentabarf_controller.rb
+++ rails/app/controllers/pentabarf_controller.rb
@@ -83,53 +83,6 @@ class PentabarfController < ApplicationController
     redirect_to( :action => :conference, :id => conf.conference_id)
   end
 
-  def event
-    begin
-      @event = Event.select_single( :event_id => params[:id] )
-      @content_title = @event.title
-      @content_subtitle = @event.subtitle
-    rescue
-      raise "Not allowed to create event." if not POPE.permission?( 'event::create' )
-      return redirect_to(:action=>:event,:id=>'new') if params[:id] != 'new'
-      @content_title = "New Event"
-      @event = Event.new(:event_id=>0,:conference_id=>@current_conference.conference_id)
-    end
-    @event_rating_remark = Event_rating_remark.select_or_new({:event_id=>@event.event_id,:person_id=>POPE.user.person_id})
-    @conference = Conference.select_single( :conference_id => @event.conference_id )
-    @current_conference = @conference
-    @attachments = View_event_attachment.select({:event_id=>@event.event_id,:translated=>@current_language})
-    @transaction = Event_transaction.select_or_new({:event_id=>@event.event_id},{:limit=>1})
-  end
-
-  def save_event
-    params[:event][:event_id] = params[:id] if params[:id].to_i > 0
-    event = write_row( Event, params[:event], {:except=>[:event_id,:conference_id],:init=>{:conference_id=>@current_conference.conference_id},:always=>[:public]} )
-    custom_bools = Custom_fields.select({:table_name=>:event,:field_type=>:boolean}).map(&:field_name)
-    write_row( Custom_event, params[:custom_event], {:preset=>{:event_id=>event.event_id},:always=>custom_bools})
-    if params[:event_rating] then
-      params[:event_rating].each do | k,v | 
-        v[:event_rating_category_id]=k 
-        if v[:rating] == "remove"
-          v[:rating] == "0"
-          v[:remove] = true
-        end
-      end
-      write_rows( Event_rating, params[:event_rating], {:preset=>{:event_id => event.event_id,:person_id=>POPE.user.person_id}})
-    end
-    params[:event_rating_remark][:remove] = true if params[:event_rating_remark][:remark].to_s.strip == ""
-    write_row( Event_rating_remark, params[:event_rating_remark], {:only=>[:remark],:remove=>true,:preset=>{:event_id => event.event_id,:person_id=>POPE.user.person_id}})
-    write_rows( Event_person, params[:event_person], {:preset=>{:event_id => event.event_id}})
-    write_rows( Event_link, params[:event_link], {:preset=>{:event_id => event.event_id},:ignore_empty=>:url})
-    write_rows( Event_link_internal, params[:event_link_internal], {:preset=>{:event_id => event.event_id},:ignore_empty=>:url})
-    write_file_row( Event_image, params[:event_image], {:preset=>{:event_id => event.event_id},:image=>true})
-    write_rows( Event_attachment, params[:event_attachment], {:always=>[:public]} )
-    write_file_rows( Event_attachment, params[:attachment_upload], {:preset=>{:event_id=>event.event_id}})
-
-    Event_transaction.new({:event_id=>event.event_id,:changed_by=>POPE.user.person_id}).write
-
-    redirect_to( :action => :event, :id => event.event_id )
-  end
-
   def person
     begin
       @person = Person.select_single( :person_id => params[:id] )
