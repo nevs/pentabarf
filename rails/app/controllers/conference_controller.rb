63142edf4c016c471af6b4840eea11daa640cbdc
Sven Klemm <sven@c3d2.de>
2009-05-29 09:56:14 +0000

    move conference methods to conference controller remove person methods from pentabarf controller

--- rails/app/controllers/conference_controller.rb
+++ rails/app/controllers/conference_controller.rb
@@ -1,3 +1,45 @@
 class ConferenceController < ApplicationController
 
+
+  def edit
+    begin
+      @conference = Conference.select_single( :conference_id => params[:conference_id] )
+      @content_title = @conference.title
+      @current_conference = @conference
+    rescue
+      raise "Not allowed to create conference." if not POPE.permission?( 'conference::create' )
+      @content_title = "New Conference"
+      return redirect_to(:action=>:conference,:id=>'new') if params[:id] != 'new'
+      @conference = Conference.new(:conference_id=>0)
+      @conference.feedback_base_url ||= url_for(:controller=>:pentabarf,:action=>:index,:only_path=>false)
+    end
+    @transaction = Conference_transaction.select_or_new({:conference_id=>@conference.conference_id},{:limit=>1})
+  end
+
+  def save
+    params[:conference][:conference_id] = nil if params[:id].to_i == 0
+    conf = write_row( Conference, params[:conference], {:except=>[:conference_id],:always=>[:f_submission_enabled,:f_submission_new_events,:f_submission_writable,:f_visitor_enabled,:f_feedback_enabled,:f_reconfirmation_enabled]} )
+    custom_bools = Custom_fields.select({:table_name=>:conference,:field_type=>:boolean}).map(&:field_name)
+    write_row( Custom_conference, params[:custom_conference], {:preset=>{:conference_id=>conf.conference_id},:always=>custom_bools})
+    write_rows( Conference_link, params[:conference_link], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:url})
+    write_rows( Conference_day, params[:conference_day], {:preset=>{:conference_id => conf.conference_id},:always=>[:public],:ignore_empty=>:conference_day})
+    write_rows( Conference_language, params[:conference_language], {:preset=>{:conference_id => conf.conference_id}})
+    write_rows( Conference_team, params[:conference_team], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_team})
+    write_rows( Conference_track, params[:conference_track], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_track})
+    write_rows( Conference_release, params[:conference_release], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_release})
+    write_rows( Conference_room, params[:conference_room], {:preset=>{:conference_id => conf.conference_id},:always=>[:public],:ignore_empty=>:conference_room})
+    write_rows( Event_rating_category, params[:event_rating_category], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:event_rating_category})
+    write_rows( Conference_room_role, params[:conference_room_role] )
+    write_file_row( Conference_image, params[:conference_image], {:preset=>{:conference_id => conf.conference_id},:image=>true})
+    Conference_transaction.new({:conference_id=>conf.conference_id,:changed_by=>POPE.user.person_id}).write
+
+    redirect_to( :action => :conference, :id => conf.conference_id)
+  end
+
+  protected
+
+  def check_permission
+    return POPE.conference_permission?('pentabarf::login',params[:conference_id])
+  end
+
 end
--- rails/app/controllers/pentabarf_controller.rb
+++ rails/app/controllers/pentabarf_controller.rb
@@ -21,14 +21,6 @@ class PentabarfController < ApplicationController
     end
   end
 
-  def conflict_person
-    @conflicts = []
-    @conflicts += View_conflict_person.call({:conference_id => @current_conference.conference_id},{:person_id=>params[:id],:translated=>@current_language})
-    @conflicts += View_conflict_event_person.call({:conference_id => @current_conference.conference_id},{:person_id=>params[:id],:translated=>@current_language})
-    @conflicts += View_conflict_event_person_event.call({:conference_id => @current_conference.conference_id},{:person_id=>params[:id],:translated=>@current_language})
-    @conflicts.length > 0 ? render( :partial => 'conflict_person' ) : render( :text => '' )
-  end
-
   def index
     @content_title = "Overview"
   end
@@ -46,102 +38,6 @@ class PentabarfController < ApplicationController
       row.delete
       redirect_to(:action=>:index)
     end
-  end
-
-  def conference
-    begin
-      @conference = Conference.select_single( :conference_id => params[:id] )
-      @content_title = @conference.title
-      @current_conference = @conference
-    rescue
-      raise "Not allowed to create conference." if not POPE.permission?( 'conference::create' )
-      @content_title = "New Conference"
-      return redirect_to(:action=>:conference,:id=>'new') if params[:id] != 'new'
-      @conference = Conference.new(:conference_id=>0)
-      @conference.feedback_base_url ||= url_for(:controller=>:pentabarf,:action=>:index,:only_path=>false)
-    end
-    @transaction = Conference_transaction.select_or_new({:conference_id=>@conference.conference_id},{:limit=>1})
-  end
-
-  def save_conference
-    params[:conference][:conference_id] = nil if params[:id].to_i == 0
-    conf = write_row( Conference, params[:conference], {:except=>[:conference_id],:always=>[:f_submission_enabled,:f_submission_new_events,:f_submission_writable,:f_visitor_enabled,:f_feedback_enabled,:f_reconfirmation_enabled]} )
-    custom_bools = Custom_fields.select({:table_name=>:conference,:field_type=>:boolean}).map(&:field_name)
-    write_row( Custom_conference, params[:custom_conference], {:preset=>{:conference_id=>conf.conference_id},:always=>custom_bools})
-    write_rows( Conference_link, params[:conference_link], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:url})
-    write_rows( Conference_day, params[:conference_day], {:preset=>{:conference_id => conf.conference_id},:always=>[:public],:ignore_empty=>:conference_day})
-    write_rows( Conference_language, params[:conference_language], {:preset=>{:conference_id => conf.conference_id}})
-    write_rows( Conference_team, params[:conference_team], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_team})
-    write_rows( Conference_track, params[:conference_track], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_track})
-    write_rows( Conference_release, params[:conference_release], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:conference_release})
-    write_rows( Conference_room, params[:conference_room], {:preset=>{:conference_id => conf.conference_id},:always=>[:public],:ignore_empty=>:conference_room})
-    write_rows( Event_rating_category, params[:event_rating_category], {:preset=>{:conference_id => conf.conference_id},:ignore_empty=>:event_rating_category})
-    write_rows( Conference_room_role, params[:conference_room_role] )
-    write_file_row( Conference_image, params[:conference_image], {:preset=>{:conference_id => conf.conference_id},:image=>true})
-    Conference_transaction.new({:conference_id=>conf.conference_id,:changed_by=>POPE.user.person_id}).write
-
-    redirect_to( :action => :conference, :id => conf.conference_id)
-  end
-
-  def person
-    begin
-      @person = Person.select_single( :person_id => params[:id] )
-      @content_title = @person.name
-    rescue
-      raise "Not allowed to create person." if not POPE.permission?( 'person::create' )
-      return redirect_to(:action=>:person,:id=>'new') if params[:id] != 'new'
-      @content_title = "New Person"
-      @person = Person.new(:person_id=>0)
-    end
-    @conference = @current_conference
-    @conference_person = Conference_person.select_or_new({:conference_id=>@conference.conference_id, :person_id=>@person.person_id})
-    @conference_person_travel = Conference_person_travel.select_or_new({:conference_person_id=>@conference_person.conference_person_id.to_i})
-    @person_rating = Person_rating.select_or_new({:person_id=>@person.person_id,:evaluator_id=>POPE.user.person_id})
-    @person_image = Person_image.select_or_new({:person_id=>@person.person_id})
-    @account = Account.select_or_new(:person_id=>@person.person_id)
-    @settings = Account_settings.select_or_new(:account_id=>@account.account_id.to_i)
-    @account_roles = @account.new_record? ? [] : Account_role.select(:account_id=>@account.account_id)
-    @transaction = Person_transaction.select_or_new({:person_id=>@person.person_id},{:limit=>1})
-  end
-
-  def save_person
-    params[:person][:person_id] = params[:id] if params[:id].to_i > 0
-    person = write_row( Person, params[:person], {:except=>[:person_id],:always=>[:spam]} )
-    if params[:account]
-      params[:account][:account_id] = Account.select_single(:person_id=>person.person_id).account_id rescue nil
-      account = write_row( Account, params[:account], {:except=>[:account_id,:salt,:edit_token,:password,:password2],:preset=>{:person_id=>person.person_id},:ignore_empty=>:login_name} ) do | row |
-        if params[:account][:password].to_s != "" && ( row.account_id == POPE.user.account_id || POPE.permission?( 'account::update' ) )
-          raise "Passwords do not match" if params[:account][:password] != params[:account][:password2]
-          row.password = params[:account][:password]
-        end
-      end
-      write_row( Account_settings, params[:account_settings], {:preset=>{:account_id=>account.account_id}}) unless account.new_record?
-    end
-    conference_person = write_row( Conference_person, params[:conference_person], {:always=>[:arrived,:reconfirmed],:preset=>{:person_id => person.person_id,:conference_id=>@current_conference.conference_id}})
-    POPE.refresh if not POPE.own_conference_person?( conference_person.conference_person_id )
-    custom_bools = Custom_fields.select({:table_name=>:person,:field_type=>:boolean}).map(&:field_name)
-    write_row( Custom_person, params[:custom_person], {:preset=>{:person_id=>person.person_id},:always=>custom_bools})
-    custom_bools = Custom_fields.select({:table_name=>:conference_person,:field_type=>:boolean}).map(&:field_name)
-    write_row( Custom_conference_person, params[:custom_conference_person], {:preset=>{:person_id=>person.person_id,:conference_id=>conference_person.conference_id},:always=>custom_bools})
-    write_row( Conference_person_travel, params[:conference_person_travel], {:preset=>{:conference_person_id => conference_person.conference_person_id},:always=>[:need_travel_cost,:need_accommodation,:need_accommodation_cost,:arrival_pickup,:departure_pickup]})
-    write_row( Person_rating, params[:person_rating], {:preset=>{:person_id => person.person_id,:evaluator_id=>POPE.user.person_id}})
-    write_rows( Person_language, params[:person_language], {:preset=>{:person_id => person.person_id}})
-    write_rows( Conference_person_link, params[:conference_person_link], {:preset=>{:conference_person_id => conference_person.conference_person_id},:ignore_empty=>:url})
-    write_rows( Conference_person_link_internal, params[:conference_person_link_internal], {:preset=>{:conference_person_id => conference_person.conference_person_id},:ignore_empty=>:url})
-    write_rows( Person_im, params[:person_im], {:preset=>{:person_id => person.person_id},:ignore_empty=>:im_address})
-    write_rows( Person_phone, params[:person_phone], {:preset=>{:person_id => person.person_id},:ignore_empty=>:phone_number})
-    write_rows( Event_person, params[:event_person], {:preset=>{:person_id => person.person_id}})
-
-    write_file_row( Person_image, params[:person_image], {:preset=>{:person_id => person.person_id},:always=>[:public],:image=>true})
-    write_person_availability( @current_conference, person, params[:person_availability])
-
-    if POPE.permission?( 'account::update' )
-      params[:account_role].each do | k,v | v[:remove] = true if not v[:set] end
-      write_rows( Account_role, params[:account_role], {:preset=>{:account_id=>account.account_id},:except=>[:set]})
-    end
-    Person_transaction.new({:person_id=>person.person_id,:changed_by=>POPE.user.person_id}).write
-
-    redirect_to( :action => :person, :id => person.person_id )
   end
 
   def activity
