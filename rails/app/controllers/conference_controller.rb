cb0e9a0a935b1b926d13d914d8112172a55f49ce
Sven Klemm <sven@c3d2.de>
2009-06-03 08:39:19 +0000

    add new method to conference controller

--- rails/app/controllers/conference_controller.rb
+++ rails/app/controllers/conference_controller.rb
@@ -1,23 +1,33 @@
 class ConferenceController < ApplicationController
 
+  before_filter :init
   before_filter :check_transaction, :only => :save
 
+  def new
+    raise "Not allowed to create conference." if not POPE.permission?( 'conference::create' )
+    @content_title = "New Conference"
+    @conference = Conference.new(:conference_id=>0)
+    @conference.feedback_base_url = url_for(:controller=>:pentabarf,:action=>:index,:only_path=>false)
+
+    @transaction = Conference_transaction.new({:conference_id=>@conference.conference_id})
+    render(:action=>'edit')
+  end
+
   def edit
-    begin
-      @conference = Conference.select_single( :conference_id => params[:conference_id] )
-      @content_title = @conference.title
-      @current_conference = @conference
-    rescue
-      raise "Not allowed to create conference." if not POPE.permission?( 'conference::create' )
-      @content_title = "New Conference"
-      return redirect_to(:action=>:conference,:id=>'new') if params[:id] != 'new'
-      @conference = Conference.new(:conference_id=>0)
-      @conference.feedback_base_url ||= url_for(:controller=>:pentabarf,:action=>:index,:only_path=>false)
-    end
+    @conference = Conference.select_single( :conference_id => params[:conference_id] )
+    @content_title = @conference.title
+    @current_conference = @conference
     @transaction = Conference_transaction.select_or_new({:conference_id=>@conference.conference_id},{:limit=>1})
   end
 
   def save
+    if params[:transaction].to_i != 0
+      transaction = Conference_transaction.select_single({:conference_id=>params[:conference_id]},{:limit=>1})
+      if transaction.conference_transaction_id != params[:transaction].to_i
+        raise "Simultanious edit"
+      end
+    end
+
     params[:conference][:conference_id] = nil if params[:id].to_i == 0
     conf = write_row( Conference, params[:conference], {:except=>[:conference_id],:always=>[:f_submission_enabled,:f_submission_new_events,:f_submission_writable,:f_visitor_enabled,:f_feedback_enabled,:f_reconfirmation_enabled]} )
     custom_bools = Custom_fields.select({:table_name=>:conference,:field_type=>:boolean}).map(&:field_name)
@@ -38,6 +48,10 @@ class ConferenceController < ApplicationController
   end
 
   protected
+
+  def init
+    @current_language = POPE.user.current_language
+  end
 
   def check_permission
     return POPE.conference_permission?('pentabarf::login',params[:conference_id])
