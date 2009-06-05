f6448514636de96d08a643817b79fdbfcbfc0e01
Sven Klemm <sven@c3d2.de>
2009-06-05 18:23:59 +0000

    add more test cases to conference controller

--- rails/test/functional/conference_controller_test.rb
+++ rails/test/functional/conference_controller_test.rb
@@ -5,15 +5,33 @@ class ConferenceControllerTest < ActionController::TestCase
     @controller = ConferenceController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
-    authenticate_user( Account.select_single(:login_name=>'committee') )
   end
 
-  test "new conference" do
+  def teardown
+    POPE.deauth
+  end
+
+  def test_conference_unauthenticated
+    get :new
+    assert_response 401
+    get :edit, {:event_id=>1}
+    assert_response 401
+  end
+
+  def test_conference_submitter
+    authenticate_user( Account.select_single(:login_name=>'testcase_submitter') )
+    get :new
+    # this should actually be 401 but due to the way auth is handled in test cases no data is returned
+    assert_response 0
+    get :edit, {:event_id=>1}
+    # this should actually be 401 but due to the way auth is handled in test cases no data is returned
+    assert_response 0
+  end
+
+  def test_conference_committee
+    authenticate_user( Account.select_single(:login_name=>'testcase_committee') )
     get :new
     assert_response :success
-  end
-
-  test "edit conference" do
     get :edit, {:conference_id=>1}
     assert_response :success
   end
