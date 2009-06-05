7adce84c28c2fac67817cfd142338f98b54d2218
Sven Klemm <sven@c3d2.de>
2009-06-05 18:04:16 +0000

    add more tests to event controller

--- rails/test/functional/event_controller_test.rb
+++ rails/test/functional/event_controller_test.rb
@@ -6,11 +6,34 @@ class EventControllerTest < ActionController::TestCase
     @controller = EventController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
-    authenticate_user( Account.select_single(:login_name=>'committee') )
   end
 
-  def test_event
+  def teardown
+    POPE.deauth
+  end
+
+  def test_event_unauthenticated
     get :new
+    assert_response 401
+    get :edit, {:event_id=>1}
+    assert_response 401
+  end
+
+  def test_event_submitter
+    authenticate_user( Account.select_single(:login_name=>'testcase_submitter') )
+    get :new
+    # this should actually be 401 but due to the way auth is handled in test cases no data is returned
+    assert_response 0
+    get :edit, {:event_id=>1}
+    # this should actually be 401 but due to the way auth is handled in test cases no data is returned
+    assert_response 0
+  end
+
+  def test_event_committee
+    authenticate_user( Account.select_single(:login_name=>'testcase_committee') )
+    get :new
+    assert_response :success
+    get :edit, {:event_id=>1}
     assert_response :success
   end
 
