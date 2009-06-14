194d4e7fc3759f67729447146109a8c1ccd09d9b
Sven Klemm <sven@c3d2.de>
2009-06-14 16:55:02 +0000

    test conference permissions for conference controller

--- rails/test/functional/conference_controller_test.rb
+++ rails/test/functional/conference_controller_test.rb
@@ -15,26 +15,46 @@ class ConferenceControllerTest < ActionController::TestCase
     get :new
     assert_response 401
 
-    get :edit, {:event_id=>1}
+    get :edit, {:conference_id=>1}
     assert_response 401
   end
 
-  def test_conference_submitter
+  def test_submitter
     authenticate_user( Account.select_single(:login_name=>'testcase_submitter') )
     get :new
     assert_response 401
 
     authenticate_user( Account.select_single(:login_name=>'testcase_submitter') )
-    get :edit, {:event_id=>1}
+    get :edit, {:conference_id=>1}
     assert_response 401
   end
 
-  def test_conference_committee
+  def test_committee
     authenticate_user( Account.select_single(:login_name=>'testcase_committee') )
     get :new
     assert_response :success
 
     authenticate_user( Account.select_single(:login_name=>'testcase_committee') )
+    get :edit, {:conference_id=>1}
+    assert_response :success
+  end
+
+  def test_conference_reviewer
+    authenticate_user( Account.select_single(:login_name=>'testcase_conference_reviewer') )
+    get :new
+    assert_response 401
+
+    authenticate_user( Account.select_single(:login_name=>'testcase_conference_reviewer') )
+    get :edit, {:conference_id=>1}
+    assert_response :success
+  end
+
+  def test_conference_committee
+    authenticate_user( Account.select_single(:login_name=>'testcase_conference_committee') )
+    get :new
+    assert_response 401
+
+    authenticate_user( Account.select_single(:login_name=>'testcase_conference_committee') )
     get :edit, {:conference_id=>1}
     assert_response :success
   end
