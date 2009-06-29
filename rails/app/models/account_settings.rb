b91edfc0c80496d131aa285b9b36367ffd58da12
sven <sven@c3d2.de>
2009-06-29 13:00:31 +0200

    remove default current_conference_id from Account_settings model

--- rails/app/models/account_settings.rb
+++ rails/app/models/account_settings.rb
@@ -3,10 +3,6 @@ class Account_settings < Momomoto::Table
   schema_name "auth"
   module Methods
 
-    def current_conference_id
-      get_column(:current_conference_id) || 1
-    end
-
     def current_language
       get_column(:current_language) || 'en'
     end
