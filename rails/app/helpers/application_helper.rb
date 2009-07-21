7bca182721cbd9290282c96cfb2f5ce8b5cf2001
sven <sven@c3d2.de>
2009-07-21 18:59:36 +0200

    remove pentabarf_revision since it was svn specific

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -12,27 +12,6 @@ module ApplicationHelper
     "0.4.0"
   end
 
-  # tries to read the current revision of pentabarf from subversion meta data
-  def pentabarf_revision
-    if not self.class.class_variables.member?( '@@revision' )
-      revision_file = File.join( RAILS_ROOT, '..', '.svn', 'entries' )
-      begin
-        content = File.open( revision_file, 'r').read
-        if content[0..1] == '<?'    # new svn xml file
-          d = REXML::Document.new( content )
-          rev = d.elements['//entry[@name=""]/@revision'].to_s
-        else                        # simple txt file
-          rev = content.split("\n")[3]
-        end
-        rev = rev.to_i
-      rescue
-        rev = 0
-      end
-      @@revision = rev
-    end
-    @@revision.to_s
-  end
-
   def local( tag, arguments = {} )
     Localizer.lookup( tag.to_s, @current_language, arguments )
   end
