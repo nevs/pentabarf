3047285e65428d6df718e776ccecc90058185069
sven <sven@c3d2.de>
2009-07-21 19:18:15 +0200

    bugfix for time conflicts using overlaps. Closes GH-6

--- sql/functions/conflict/event_person_event/time_visitor_visitor.sql
+++ sql/functions/conflict/event_person_event/time_visitor_visitor.sql
@@ -17,7 +17,6 @@ CREATE OR REPLACE FUNCTION conflict.conflict_event_person_event_time_visitor_vis
     INNER JOIN event_person AS ep2 ON (
       ep2.person_id = ep1.person_id AND
       ep2.event_role = 'visitor' AND
-      ep2.event_role_state = 'confirmed' AND
       ep2.event_id <> ep1.event_id
     )
     INNER JOIN event AS e2 ON (
@@ -32,7 +31,6 @@ CREATE OR REPLACE FUNCTION conflict.conflict_event_person_event_time_visitor_vis
     )
   WHERE
     ep1.event_role = 'visitor' AND
-    ep1.event_role_state = 'confirmed' AND
     (cd1.conference_day + e1.start_time, e1.duration) OVERLAPS (cd2.conference_day + e2.start_time, e2.duration) AND
     -- OVERLAPS also returns true when the end of interval 1 matches the start of interval 2
     -- thats why we exclude those cases explicitly
