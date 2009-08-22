69575724d413a8b04fe016200e73ee068e520380
pentabarf <pentabarf@republica.pentabarf.org>
2009-08-22 13:20:12 +0200

    fix xml controller

--- rails/app/controllers/xml_controller.rb
+++ rails/app/controllers/xml_controller.rb
@@ -5,16 +5,16 @@ class XmlController < ApplicationController
   def schedule
     if params[:preview]
       # if preview is specified we work on live data
-      @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+      @conference = Release_preview::Conference.select_single({:conference_id=>params[:conference_id]})
     elsif params[:release]
       # if a specific release is selected we show that one
-      @conference = Release::Conference.select_single({:acronym=>params[:conference],:conference_release_id=>params[:release]})
+      @conference = Release::Conference.select_single({:conference_id=>params[:conference_id],:conference_release_id=>params[:release]})
     else
       # otherwise we show the latest release with fallback to live data if nothing has been released yet
       begin
-        @conference = Release::Conference.select_single({:acronym=>params[:conference]},{:limit=>1,:order=>Momomoto.desc(:conference_release_id)})
+        @conference = Release::Conference.select_single({:conference_id=>params[:conference_id]},{:limit=>1,:order=>Momomoto.desc(:conference_release_id)})
       rescue Momomoto::Nothing_found
-        @conference = Release_preview::Conference.select_single({:acronym=>params[:conference]})
+        @conference = Release_preview::Conference.select_single({:conference_id=>params[:conference_id]})
       end
     end
   end
