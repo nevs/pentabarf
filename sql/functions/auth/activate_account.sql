08c43203d2e20b64a3930410dd2efa0069998987
sven <sven@c3d2.de>
2009-10-15 10:11:36 +0200

    increase activation interval to 1 week and delete unactivated accounts after that period

--- sql/functions/auth/activate_account.sql
+++ sql/functions/auth/activate_account.sql
@@ -5,8 +5,9 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
     cur_person_id INTEGER;
     activation_interval INTERVAL;
   BEGIN
-    activation_interval = '-1 day';
-    SELECT INTO cur_activation account_id, conference_id FROM auth.account_activation WHERE account_activation.activation_string = activation_string AND account_creation > now() + activation_interval;
+    activation_interval = '-7 day';
+    DELETE FROM auth.account WHERE account_id IN ( SELECT account_id FROM auth.account_activation WHERE account_creation < now() + activation_interval );
+    SELECT INTO cur_activation account_id, conference_id FROM auth.account_activation WHERE account_activation.activation_string = activation_string;
     IF FOUND THEN
       INSERT INTO auth.account_role(account_id, role) VALUES (cur_activation.account_id, 'submitter');
       SELECT INTO cur_person_id nextval(pg_get_serial_sequence('base.person','person_id'));
@@ -14,7 +15,6 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
       UPDATE auth.account SET person_id = cur_person_id WHERE account_id = cur_activation.account_id;
       INSERT INTO auth.account_settings(account_id,current_conference_id) VALUES (cur_activation.account_id, cur_activation.conference_id);
       INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_person_id, now(), cur_person_id, 't');
-      DELETE FROM auth.account_activation WHERE account_activation.activation_string = activation_string OR account_creation < now() + activation_interval;
     ELSE
       PERFORM 1 FROM log.account_activation WHERE account_activation.activation_string = activation_string AND account_activation.log_operation = 'D';
       IF FOUND THEN
