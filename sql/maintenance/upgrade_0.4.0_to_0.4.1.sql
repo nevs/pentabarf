a72d3c33cc7bc378ccf04401541d56868c263e87
sven <sven@c3d2.de>
2009-10-16 16:32:38 +0200

    bugfix for upgrade script from 0.4.0 to 0.4.1

--- sql/maintenance/upgrade_0.4.0_to_0.4.1.sql
+++ sql/maintenance/upgrade_0.4.0_to_0.4.1.sql
@@ -1,7 +1,7 @@
 
 BEGIN;
 
-INSERT INTO conference_role_permission (conference_role, permission) VALUES ('reviewer', 'review::modify');
+INSERT INTO auth.conference_role_permission (conference_role, permission) VALUES ('reviewer', 'review::modify');
 
 ALTER TABLE event_role ADD COLUMN public BOOL NOT NULL DEFAULT false;
 UPDATE event_role SET public = true WHERE event_role IN ('moderator','speaker');
