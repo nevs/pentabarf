e6861953ead067eab9273ee06010a0f25f1ee143
sven <sven@c3d2.de>
2009-12-16 23:52:04 +0100

    remove references to person_transaction and event_transaction

--- sql/functions/auth/activate_account.sql
+++ sql/functions/auth/activate_account.sql
@@ -14,7 +14,6 @@ CREATE OR REPLACE FUNCTION auth.activate_account( activation_string char(64)) RE
       INSERT INTO person(person_id,nickname,email) SELECT cur_person_id,login_name,email FROM auth.account WHERE account.account_id = cur_activation.account_id;
       UPDATE auth.account SET person_id = cur_person_id WHERE account_id = cur_activation.account_id;
       INSERT INTO auth.account_settings(account_id,current_conference_id) VALUES (cur_activation.account_id, cur_activation.conference_id);
-      INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_person_id, now(), cur_person_id, 't');
     ELSE
       PERFORM 1 FROM log.account_activation WHERE account_activation.activation_string = activation_string AND account_activation.log_operation = 'D';
       IF FOUND THEN
--- sql/functions/auth/activate_account_id.sql
+++ sql/functions/auth/activate_account_id.sql
@@ -15,7 +15,6 @@ CREATE OR REPLACE FUNCTION auth.activate_account_id( cur_account_id INTEGER ) RE
     SELECT INTO cur_person_id nextval(pg_get_serial_sequence('base.person','person_id'));
     INSERT INTO person(person_id,nickname,email) SELECT cur_person_id,login_name,email FROM auth.account WHERE account.account_id = cur_account_id;
     UPDATE auth.account SET person_id = cur_person_id WHERE account.account_id = cur_account_id;
-    INSERT INTO person_transaction( person_id, changed_when, changed_by, f_create ) VALUES (cur_person_id, now(), cur_person_id, 't');
 
     RETURN;
   END;
