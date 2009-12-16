25a8794bf34ad16b401936e0283a2115f5d50571
sven <sven@c3d2.de>
2009-12-16 23:52:04 +0100

    remove references to person_transaction and event_transaction

--- sql/functions/admin/merge_person.sql
+++ sql/functions/admin/merge_person.sql
@@ -39,9 +39,6 @@ CREATE OR REPLACE FUNCTION merge_person( primary_person INTEGER, secondary_perso
             language NOT IN ( SELECT language FROM person_language WHERE person_id = primary_person );
     DELETE FROM person_language WHERE person_id = secondary_person;
 
-    UPDATE person_transaction SET person_id = primary_person WHERE person_id = secondary_person;
-    UPDATE person_transaction SET changed_by = primary_person WHERE changed_by = secondary_person;
-
     UPDATE person_availability SET person_id = primary_person WHERE person_id = secondary_person;
     UPDATE person_phone SET person_id = primary_person WHERE person_id = secondary_person;
     UPDATE person_im SET person_id = primary_person WHERE person_id = secondary_person;
@@ -50,10 +47,8 @@ CREATE OR REPLACE FUNCTION merge_person( primary_person INTEGER, secondary_perso
             evaluator_id NOT IN ( SELECT evaluator_id FROM person_rating WHERE person_id = primary_person);
     UPDATE person_image SET person_id = primary_person WHERE person_id = secondary_person AND NOT EXISTS( SELECT 1 FROM person_image WHERE person_id = primary_person);
 
-    UPDATE event_transaction SET changed_by = primary_person WHERE changed_by = secondary_person;
     UPDATE event_person SET person_id = primary_person WHERE person_id = secondary_person;
 
-    UPDATE conference_transaction SET changed_by = primary_person WHERE changed_by = secondary_person;
     UPDATE conference_person SET person_id = primary_person
       WHERE person_id = secondary_person AND
             conference_id NOT IN ( SELECT conference_id FROM conference_person WHERE person_id = primary_person );
