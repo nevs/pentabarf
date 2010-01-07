a2d93608e1d3063133c36663309005f066e1a669
sven <sven@c3d2.de>
2010-01-07 11:26:08 +0100

    add event_attachment action to schedule controller

--- rails/app/models/release/view_schedule_event_attachment.rb
+++ rails/app/models/release/view_schedule_event_attachment.rb
@@ -2,5 +2,7 @@ class Release::View_schedule_event_attachment < Momomoto::Table
 
   schema_name "release"
 
+  fk_helper_single :data, Release::Event_attachment, [:conference_release_id,:event_attachment_id]
+
 end
 
--- rails/app/models/view_schedule_event_attachment.rb
+++ rails/app/models/view_schedule_event_attachment.rb
@@ -1,3 +1,6 @@
 class View_schedule_event_attachment < Momomoto::Table
+
+  fk_helper_single :data, Event_attachment, [:event_attachment_id]
+
 end
 
