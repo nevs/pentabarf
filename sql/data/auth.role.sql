055088638105043aa3e4c678168a3e4645d22d1c
sven <sven@c3d2.de>
2010-01-15 22:11:19 +0100

    update auth.role initial data

--- sql/data/auth.role.sql
+++ sql/data/auth.role.sql
@@ -2,8 +2,8 @@
 SET client_encoding = 'UTF8';
 SET search_path = auth, pg_catalog;
 
-INSERT INTO "role" ("role", rank) VALUES ('admin', 2);
-INSERT INTO "role" ("role", rank) VALUES ('committee', 3);
-INSERT INTO "role" ("role", rank) VALUES ('developer', 1);
-INSERT INTO "role" ("role", rank) VALUES ('reviewer', 4);
-INSERT INTO "role" ("role", rank) VALUES ('submitter', 7);
+INSERT INTO role (role, rank) VALUES ('admin', 2);
+INSERT INTO role (role, rank) VALUES ('committee', 3);
+INSERT INTO role (role, rank) VALUES ('developer', 1);
+INSERT INTO role (role, rank) VALUES ('reviewer', 4);
+INSERT INTO role (role, rank) VALUES ('submitter', 7);
