5e218b813b66651ca61a796846e28a2919c2d726
sven <sven@c3d2.de>
2010-01-15 22:11:51 +0100

    update auth.role_localized initial data

--- sql/data/auth.role_localized.sql
+++ sql/data/auth.role_localized.sql
@@ -2,13 +2,13 @@
 SET client_encoding = 'UTF8';
 SET search_path = auth, pg_catalog;
 
-INSERT INTO role_localized ("role", translated, name) VALUES ('admin', 'de', 'Administrator');
-INSERT INTO role_localized ("role", translated, name) VALUES ('admin', 'en', 'Administrator');
-INSERT INTO role_localized ("role", translated, name) VALUES ('committee', 'de', 'Komitee');
-INSERT INTO role_localized ("role", translated, name) VALUES ('committee', 'en', 'Committee');
-INSERT INTO role_localized ("role", translated, name) VALUES ('developer', 'de', 'Entwickler');
-INSERT INTO role_localized ("role", translated, name) VALUES ('developer', 'en', 'Developer');
-INSERT INTO role_localized ("role", translated, name) VALUES ('reviewer', 'de', 'Reviewer');
-INSERT INTO role_localized ("role", translated, name) VALUES ('reviewer', 'en', 'Reviewer');
-INSERT INTO role_localized ("role", translated, name) VALUES ('submitter', 'de', 'Submitter');
-INSERT INTO role_localized ("role", translated, name) VALUES ('submitter', 'en', 'Submitter');
+INSERT INTO role_localized (role, translated, name) VALUES ('admin', 'de', 'Administrator');
+INSERT INTO role_localized (role, translated, name) VALUES ('admin', 'en', 'Administrator');
+INSERT INTO role_localized (role, translated, name) VALUES ('committee', 'de', 'Komitee');
+INSERT INTO role_localized (role, translated, name) VALUES ('committee', 'en', 'Committee');
+INSERT INTO role_localized (role, translated, name) VALUES ('developer', 'de', 'Entwickler');
+INSERT INTO role_localized (role, translated, name) VALUES ('developer', 'en', 'Developer');
+INSERT INTO role_localized (role, translated, name) VALUES ('reviewer', 'de', 'Reviewer');
+INSERT INTO role_localized (role, translated, name) VALUES ('reviewer', 'en', 'Reviewer');
+INSERT INTO role_localized (role, translated, name) VALUES ('submitter', 'de', 'Submitter');
+INSERT INTO role_localized (role, translated, name) VALUES ('submitter', 'en', 'Submitter');
