1db1205d9fb356de6249dfc2b95b227fb3844765
sven <sven@c3d2.de>
2010-01-15 22:14:21 +0100

    update link_type initial data

--- sql/data/link_type.sql
+++ sql/data/link_type.sql
@@ -2,7 +2,7 @@
 SET client_encoding = 'UTF8';
 SET search_path = public, pg_catalog;
 
-INSERT INTO link_type (link_type, "template", rank) VALUES ('orga wiki', 'https://22c3.cccv.de/wiki/', NULL);
-INSERT INTO link_type (link_type, "template", rank) VALUES ('rt cccv', 'https://rt.cccv.de/Ticket/Display.html?id=', NULL);
-INSERT INTO link_type (link_type, "template", rank) VALUES ('rt entheovision', 'https://rt.entheovision.de/Ticket/Display.html?id=', NULL);
-INSERT INTO link_type (link_type, "template", rank) VALUES ('url', NULL, 1);
+INSERT INTO link_type (link_type, template, rank) VALUES ('orga wiki', 'https://22c3.cccv.de/wiki/', NULL);
+INSERT INTO link_type (link_type, template, rank) VALUES ('rt cccv', 'https://rt.cccv.de/Ticket/Display.html?id=', NULL);
+INSERT INTO link_type (link_type, template, rank) VALUES ('rt entheovision', 'https://rt.entheovision.de/Ticket/Display.html?id=', NULL);
+INSERT INTO link_type (link_type, template, rank) VALUES ('url', NULL, 1);
