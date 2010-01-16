945c6d2def640338667b68572cd33cd5d03e43da
sven <sven@c3d2.de>
2010-01-16 16:26:14 +0100

    fix order in upgrade script

--- sql/maintenance/upgrade_0.4.3_to_0.4.4.sql
+++ sql/maintenance/upgrade_0.4.3_to_0.4.4.sql
@@ -2,6 +2,14 @@
 BEGIN;
 
 -- register event_without_break_after and event_without_break_before
+INSERT INTO conflict.conflict (conflict, conflict_type) VALUES ('event_without_break_after', 'event_event');
+INSERT INTO conflict.conflict (conflict, conflict_type) VALUES ('event_without_break_before', 'event_event');
+
+INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_after', 'de', 'Veranstaltung ohne Pause danach');
+INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_after', 'en', 'Event without break after');
+INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_before', 'de', 'Veranstaltultung ohne Pause davor');
+INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_before', 'en', 'Event without break before');
+
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (176, 'chaos', 'event_without_break_after', 'error');
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (177, 'discord', 'event_without_break_after', 'error');
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (178, 'bureaucracy', 'event_without_break_after', 'error');
@@ -12,11 +20,4 @@ INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, co
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (183, 'bureaucracy', 'event_without_break_before', 'error');
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (184, 'confusion', 'event_without_break_before', 'error');
 INSERT INTO conflict.conference_phase_conflict (conference_phase_conflict_id, conference_phase, conflict, conflict_level) VALUES (185, 'aftermath', 'event_without_break_before', 'silent');
-INSERT INTO conflict.conflict (conflict, conflict_type) VALUES ('event_without_break_after', 'event_event');
-INSERT INTO conflict.conflict (conflict, conflict_type) VALUES ('event_without_break_before', 'event_event');
-INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_after', 'de', 'Veranstaltung ohne Pause danach');
-INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_after', 'en', 'Event without break after');
-INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_before', 'de', 'Veranstaltultung ohne Pause davor');
-INSERT INTO conflict.conflict_localized (conflict, translated, name) VALUES ('event_without_break_before', 'en', 'Event without break before');
-
 COMMIT;
