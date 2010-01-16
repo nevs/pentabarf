37f8606eba6e7f1268fe583626c78217b4f47970
sven <sven@c3d2.de>
2010-01-16 16:49:08 +0100

    fix conflict function names

--- sql/functions/conflict/event_event/event_without_break_after.sql
+++ sql/functions/conflict/event_event/event_without_break_after.sql
@@ -1,6 +1,6 @@
 
 -- returns events followed by another event without a break in between
-CREATE OR REPLACE FUNCTION conflict.conflict_event_event_no_break_after( INTEGER ) RETURNS SETOF conflict.conflict_event_event AS $$
+CREATE OR REPLACE FUNCTION conflict.conflict_event_no_break_after( INTEGER ) RETURNS SETOF conflict.conflict_event_event AS $$
 -- Loop through all events
   SELECT
     e2.event_id AS event_id1,
