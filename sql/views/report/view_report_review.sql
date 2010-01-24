f599a1ca84947da072d8d4500572dda6b175b5fc
sven <sven@c3d2.de>
2010-01-24 13:43:22 +0100

    show all public roles in report review no matter their state

--- sql/views/report/view_report_review.sql
+++ sql/views/report/view_report_review.sql
@@ -15,10 +15,9 @@ CREATE OR REPLACE VIEW view_report_review AS
       SELECT view_person.person_id
         FROM
           event_person
+          INNER JOIN event_role ON (event_role.event_role = event_person.event_role AND event_role.public = TRUE)
           INNER JOIN view_person USING (person_id)
         WHERE
-          event_person.event_role IN ('speaker','moderator') AND
-          event_person.event_role_state = 'confirmed' AND
           event_person.event_id = event.event_id
         ORDER BY view_person.name, event_person.person_id
       ), E'\n'::text) AS speaker_ids,
@@ -26,16 +25,15 @@ CREATE OR REPLACE VIEW view_report_review AS
       SELECT view_person.name
         FROM
           event_person
+          INNER JOIN event_role ON (event_role.event_role = event_person.event_role AND event_role.public = TRUE)
           INNER JOIN view_person USING (person_id)
         WHERE
-          event_person.event_role IN ('speaker','moderator') AND
-          event_person.event_role_state = 'confirmed' AND
           event_person.event_id = event.event_id
         ORDER BY view_person.name, event_person.person_id
       ), E'\n'::text) AS speakers,
     coalesce( ( SELECT COUNT(person_id) FROM (SELECT person_id FROM event_rating WHERE event_id = event.event_id GROUP BY person_id) AS unique_rater ), 0 ) AS raters,
     ( SELECT coalesce( SUM( (rating - 3) * 50 ) / COUNT( rating ), 0 ) FROM event_rating WHERE event_id = event.event_id ) AS rating
-  FROM 
+  FROM
     event
     LEFT OUTER JOIN event_state_localized USING (event_state)
     LEFT OUTER JOIN conference_track USING (conference_track_id)
